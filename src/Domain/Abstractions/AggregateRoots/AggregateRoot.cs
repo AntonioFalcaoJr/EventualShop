e242aca54f77b3aa6a8e7efcc37150a07931b647
Antônio Falcão Jr <antonio.falcao@xpi.com.br>
2021-06-28 14:24:47 -0300

    Adding events support in AggregateRoot abstraction

---
+++ src/Domain/Abstractions/AggregateRoots/AggregateRoot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Domain.Abstractions.Entities;
+using Domain.Abstractions.Events;
+
+namespace Domain.Abstractions.AggregateRoots
+{
+    public abstract class AggregateRoot<TId> : Entity<TId>, IAggregateRoot
+        where TId : struct
+    {
+        private readonly List<IEvent> _events = new();
+
+        protected void Load(IEnumerable<IEvent> events)
+            => events.ToList().ForEach(ApplyChanges);
+
+        protected abstract void ApplyChanges(IEvent @event);
+
+        protected void AddEvent(IEvent @event)
+            => _events.Add(@event);
+
+        protected List<IEvent> GetEvents()
+            => _events;
+    }
+}
