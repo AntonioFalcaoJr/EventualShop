ecf139c3ca4024d5fce3736b4dc84da64583bc2e
Antônio Falcão Jr <antonio.falcao@xpi.com.br>
2021-07-09 12:18:48 -0300

    Adding CustomersController

---
+++ src/WebAPI/Controllers/CustomersController.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Application.Customers.Commands.Delete;
+using Application.Customers.Commands.Register;
+using Application.Customers.Commands.Update;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Abstractions;
+
+namespace WebAPI.Controllers
+{
+    public class CustomersController : ApplicationController
+    {
+        public CustomersController(IMediator mediator) 
+            : base(mediator) { }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(RegisterCustomerCommand command)
+            => await SendCommand(command);
+
+
+        [HttpPut]
+        public async Task<IActionResult> Put(UpdateCustomerCommand command)
+            => await SendCommand(command);
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(DeleteCustomerCommand command)
+            => await SendCommand(command);
+    }
+}
