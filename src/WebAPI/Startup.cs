f4dfd599a6f3a8f1e3c3ba217d68ed6e986a9b98
Antônio Falcão Jr <antonio.falcao@xpi.com.br>
2021-07-09 12:19:15 -0300

    Configuring project startup

---
+++ src/WebAPI/Startup.cs
@@ -0,0 +1,51 @@
+using Application.DependencyInjection.Extensions;
+using Infrastructure.DependencyInjection.Extensions;
+using Infrastructure.DependencyInjection.Options;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace WebAPI
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "WebAPI", Version = "v1"}); });
+
+            services.AddMediatR();
+            services.AddEventStore();
+            services.AddRepositories();
+            services.AddEventStoreDbContext();
+
+            services.ConfigureSqlServerRetryingOptions(
+                _configuration.GetSection(nameof(SqlServerRetryingOptions)));
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1"));
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+    }
+}
