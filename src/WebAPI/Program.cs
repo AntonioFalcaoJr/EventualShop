7fa90737f26b5a007fc3c87fcc421dd89a3c3e85
Antônio Falcão Jr <antonio.falcao@xpi.com.br>
2021-07-09 12:20:11 -0300

    Configure serilog in Program.cs

---
+++ src/WebAPI/Program.cs
@@ -0,0 +1,70 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace WebAPI
+{
+public static class Program
+    {
+        private static IHostBuilder CreateHostBuilder(string[] args)
+            => Host.CreateDefaultBuilder(args)
+                .UseSerilog()
+                .ConfigureWebHostDefaults(webBuilder 
+                    => webBuilder.UseStartup<Startup>())
+                .UseDefaultServiceProvider(
+                    (context, options) =>
+                        {
+                            options.ValidateScopes = context.HostingEnvironment.IsDevelopment();
+                            options.ValidateOnBuild = true;
+                        });
+        
+        public static async Task Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(
+                    path: "appsettings.json", 
+                    optional: false, 
+                    reloadOnChange: true)
+                .AddJsonFile(
+                    path: $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", 
+                    optional: true, 
+                    reloadOnChange: true)
+                .Build();
+            
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
+            try
+            {
+                var host = CreateHostBuilder(args).Build();
+                await host.MigrateDatabaseAsync();
+                await host.RunAsync();
+                Log.Information("Stopped cleanly");
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "An unhandled exception occured during bootstrapping");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        private static async Task MigrateDatabaseAsync(this IHost host)
+        {
+            await using var scope =  host.Services.CreateAsyncScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<DbContext>();
+            await dbContext.Database.MigrateAsync();
+            await dbContext.Database.EnsureCreatedAsync();
+        }
+    }
+}
