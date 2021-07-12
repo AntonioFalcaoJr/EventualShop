c4f95f49101deff6bb0f6269721ca1e29e6bf841
Antônio Falcão Jr <antonio.falcao@xpi.com.br>
2021-07-12 12:01:17 -0300

    Adding event store dbcontext

---
+++ src/Infrastructure/Contexts/EventStoreDbContext.cs
@@ -0,0 +1,78 @@
+using Infrastructure.DependencyInjection.Options;
+using Infrastructure.StoreEvents;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Infrastructure.Contexts
+{
+    public class EventStoreDbContext : DbContext
+    {
+        private const string SqlLatin1GeneralCp1CsAs = "SQL_Latin1_General_CP1_CS_AS";
+        private readonly IConfiguration _configuration;
+        private readonly ILoggerFactory _loggerFactory;
+        private readonly SqlServerRetryingOptions _options;
+
+        public EventStoreDbContext(
+            DbContextOptions options,
+            ILoggerFactory loggerFactory,
+            IConfiguration configuration,
+            IOptionsSnapshot<SqlServerRetryingOptions> optionsSnapshot)
+            : base(options)
+        {
+            _loggerFactory = loggerFactory;
+            _configuration = configuration;
+            _options = optionsSnapshot.Value;
+        }
+
+        public DbSet<CustomerStoreEvent> CustomerStoreEvents { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseCollation(SqlLatin1GeneralCp1CsAs);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(EventStoreDbContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.IsConfigured) return;
+
+            optionsBuilder
+                .EnableDetailedErrors()
+                .EnableSensitiveDataLogging()
+                .UseSqlServer(
+                    connectionString: _configuration.GetConnectionString("DefaultConnection"),
+                    sqlServerOptionsAction: SqlServerOptionsAction)
+                .UseLoggerFactory(_loggerFactory);
+        }
+
+        private void SqlServerOptionsAction(SqlServerDbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder
+                .ExecutionStrategy(
+                    dependencies => new SqlServerRetryingExecutionStrategy(
+                        dependencies: dependencies,
+                        maxRetryCount: _options.MaxRetryCount,
+                        maxRetryDelay: _options.MaxRetryDelay,
+                        errorNumbersToAdd: _options.ErrorNumbersToAdd))
+                .MigrationsAssembly(assemblyName: typeof(EventStoreDbContext).Assembly.GetName().Name);
+
+        // public override int SaveChanges()
+        // {
+        //     if (base.SaveChanges() <= 0) return default;
+        //     
+        //         var domainEntities = ChangeTracker
+        //             .Entries<CustomerStoreEvent>()
+        //             .Where(x => x.Entity.DomainEvent is not null);
+        //
+        //         var domainEvents = domainEntities
+        //             .SelectMany(x => x.Entity.DomainEvents)
+        //             .ToList();
+        //
+        //         var tasks = domainEvents
+        //             .Select(async (domainEvent) => { await mediator.PublishEvent(domainEvent); });
+        // }
+    }
+}
