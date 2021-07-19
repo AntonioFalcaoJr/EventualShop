a375c6d6075ef4d61159a474292925f236134da1
Antônio Falcão Jr <antonio.falcao@xpi.com.br>
2021-07-19 20:26:14 -0300

    Adding a better design for commands and handlers

---
+++ src/Application/UseCases/Customers/Commands/DeleteCustomer/DeleteCustomerCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using Application.Abstractions.Commands;
+using MassTransit;
+
+namespace Application.UseCases.Customers.Commands.DeleteCustomer
+{
+    public record DeleteCustomerCommand(Guid Id) : ICommand;
+    
+    public class DeleteCustomerHandler : IConsumer<DeleteCustomerCommand>
+    {
+        public Task Consume(ConsumeContext<DeleteCustomerCommand> context)
+            => throw new System.NotImplementedException();
+    }
+}
---
+++ src/Application/UseCases/Customers/Commands/UpdateCustomer/UpdateCustomerCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Application.Abstractions.Commands;
+using Application.Interfaces;
+using MassTransit;
+
+namespace Application.UseCases.Customers.Commands.UpdateCustomer
+{
+    public record UpdateCustomerCommand(Guid Id, string Name, int Age) : ICommand;
+
+    public class UpdateCustomerHandler : IConsumer<UpdateCustomerCommand>
+    {
+        private readonly ICustomerEventStoreService _eventStoreService;
+
+        public UpdateCustomerHandler(ICustomerEventStoreService eventStoreService)
+        {
+            _eventStoreService = eventStoreService;
+        }
+
+        public async Task Consume(ConsumeContext<UpdateCustomerCommand> context)
+        {
+            var (aggregateId, name, age) = context.Message;
+
+            var customer = await _eventStoreService.LoadAggregateFromStreamAsync(aggregateId, context.CancellationToken);
+
+            customer.ChangeAge(age);
+            customer.ChangeName(name);
+
+            await _eventStoreService.AppendEventsToStreamAsync(customer, context.CancellationToken);
+        }
+    }
+}
