3930cdc0a1a5580fdbaf37e90631330a352d1bc6
Antônio Falcão Jr <antonio.falcao@xpi.com.br>
2021-08-05 01:40:47 -0300

    Moving

--- src/Infrastructure/EventSourcing/EventStore/Customers/CustomerSnapshot.cs
+++
@@ -1,8 +0,0 @@
-using System;
-using Domain.Entities.Customers;
-using Infrastructure.Abstractions.EventSourcing.EventStore;
-
-namespace Infrastructure.EventSourcing.EventStore.Customers
-{
-    public record CustomerSnapshot : Snapshot<Customer, Guid>;
-}
--- src/Infrastructure/EventSourcing/EventStore/Customers/CustomerStoreEvent.cs
+++
@@ -1,8 +0,0 @@
-using System;
-using Domain.Entities.Customers;
-using Infrastructure.Abstractions.EventSourcing.EventStore;
-
-namespace Infrastructure.EventSourcing.EventStore.Customers
-{
-    public record CustomerStoreEvent : StoreEvent<Customer, Guid>;
-}
