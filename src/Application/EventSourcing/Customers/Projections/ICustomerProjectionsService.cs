ae794cc3fdd17b91c3878186597c37e617074a49
Antônio Falcão Jr <antonio.falcao@xpi.com.br>
2021-08-06 13:57:23 -0300

    Adding pagination and adjusting projections description

--- src/Application/EventSourcing/Customers/Projections/ICustomerProjectionsService.cs
+++ src/Application/EventSourcing/Customers/Projections/ICustomerProjectionsService.cs
@@ -1,17 +1,17 @@
 using System;
-using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Threading;
 using System.Threading.Tasks;
-using Application.UseCases.Customers.Queries.CustomerDetails;
+using Application.Abstractions.EventSourcing.Projections.Pagination;
+using Application.UseCases.Customers.Queries.GetCustomerDetails;
 
 namespace Application.EventSourcing.Customers.Projections
 {
     public interface ICustomerProjectionsService 
     {
-        Task<List<CustomerDetailsModel>> GetCustomersAsync(Expression<Func<CustomerDetailsModel, bool>> predicate, CancellationToken cancellationToken);
-        Task ProjectNewCustomerDetailsAsync(CustomerDetailsModel customerDetails, CancellationToken cancellationToken);
-        Task ProjectCustomerListAsync(CustomerDetailsModel customerDetails, CancellationToken cancellationToken);
-        Task<CustomerDetailsModel> GetCustomerDetailsAsync(Guid customerId, CancellationToken cancellationToken);
+        Task<IPagedResult<CustomerDetailsProjection>> GetCustomersDetailsWithPaginationAsync(IPaging paging, Expression<Func<CustomerDetailsProjection, bool>> predicate, CancellationToken cancellationToken);
+        Task ProjectNewCustomerDetailsAsync(CustomerDetailsProjection customerDetails, CancellationToken cancellationToken);
+        Task ProjectCustomerListAsync(CustomerDetailsProjection customerDetails, CancellationToken cancellationToken);
+        Task<CustomerDetailsProjection> GetCustomerDetailsAsync(Guid customerId, CancellationToken cancellationToken);
     }
 }
