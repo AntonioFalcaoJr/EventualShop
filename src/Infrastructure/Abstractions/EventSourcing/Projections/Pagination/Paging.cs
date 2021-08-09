dbd11e5cd41ae67a2e7d7b5f4cfbcaee65b7ffa8
Antônio Falcão Jr <antonio.falcao@xpi.com.br>
2021-08-09 17:15:22 -0300

    Adding paging control

--- src/Infrastructure/Abstractions/EventSourcing/Projections/Pagination/Paging.cs
+++ src/Infrastructure/Abstractions/EventSourcing/Projections/Pagination/Paging.cs
@@ -4,13 +4,12 @@ namespace Infrastructure.Abstractions.EventSourcing.Projections.Pagination
 {
     public record Paging : IPaging
     {
-        private const int UpperLimit = 50;
         private const int DefaultLimit = 10;
         private readonly int _limit;
 
         public int Limit
         {
-            get => _limit > UpperLimit ? UpperLimit : _limit <= 0 ? DefaultLimit : _limit;
+            get => _limit > 0 ? _limit : DefaultLimit;
             init => _limit = value;
         }
 
