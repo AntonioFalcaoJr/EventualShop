89261f213403e37c7b8c8b2151087a421fa56c5b
Antônio Falcão Jr <antonio.falcao@xpi.com.br>
2021-08-10 23:58:59 -0300

    Adding RabbitMQ options

---
+++ src/Infrastructure/DependencyInjection/Options/RabbitMQOptions.cs
@@ -0,0 +1,9 @@
+namespace Infrastructure.DependencyInjection.Options
+{
+    public record RabbitMqOptions
+    {
+        public string Host { get; init; }
+        public string Username { get; init; }
+        public string Password { get; init; }
+    }
+}
