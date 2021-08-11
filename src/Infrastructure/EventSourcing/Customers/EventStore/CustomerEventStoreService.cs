13acacc02f244a48f7ef2b88cdf112ea33018028
Antônio Falcão Jr <antonio.falcao@xpi.com.br>
2021-08-10 22:22:22 -0300

    Improving options pattern lifetime with Monitor

--- src/Infrastructure/EventSourcing/Customers/EventStore/CustomerEventStoreService.cs
+++ src/Infrastructure/EventSourcing/Customers/EventStore/CustomerEventStoreService.cs
@@ -1,17 +1,17 @@
 using System;
-using Application.DependencyInjection.Options;
 using Application.EventSourcing.Customers.EventStore;
 using Application.EventSourcing.Customers.EventStore.Events;
 using Domain.Entities.Customers;
 using Infrastructure.Abstractions.EventSourcing.EventStore;
-using MassTransit.Mediator;
+using Infrastructure.DependencyInjection.Options;
+using MassTransit;
 using Microsoft.Extensions.Options;
 
 namespace Infrastructure.EventSourcing.Customers.EventStore
 {
     public class CustomerEventStoreService : EventStoreService<Customer, CustomerStoreEvent, CustomerSnapshot, Guid>, ICustomerEventStoreService
     {
-        public CustomerEventStoreService(IOptionsSnapshot<EventStoreOptions> options, ICustomerEventStoreRepository repository, IMediator mediator)
-            : base(options, repository, mediator) { }
+        public CustomerEventStoreService(IOptionsMonitor<EventStoreOptions> optionsMonitor, ICustomerEventStoreRepository repository, IBus bus)
+            : base(optionsMonitor, repository, bus) { }
     }
 }
