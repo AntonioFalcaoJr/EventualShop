603a9ab73f9bf1ddb8e39017c566249a7d4114a5
Antônio Falcão Jr <antonio.falcao@xpi.com.br>
2021-08-30 19:40:38 -0300

    Adding topology exclude treatment

--- src/Domain/Abstractions/Events/DomainEvent.cs
+++ src/Domain/Abstractions/Events/DomainEvent.cs
@@ -1,7 +1,9 @@
 using System;
+using MassTransit.Topology;
 
 namespace Domain.Abstractions.Events
 {
+    [ExcludeFromTopology]
     public abstract record DomainEvent : IDomainEvent
     {
         public DateTimeOffset Timestamp { get; private set; } = DateTimeOffset.Now;
--- src/Domain/Abstractions/Events/IDomainEvent.cs
+++ src/Domain/Abstractions/Events/IDomainEvent.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using MassTransit.Topology;
 
 namespace Domain.Abstractions.Events
 {
+    [ExcludeFromTopology]
     public interface IDomainEvent
     {
         DateTimeOffset Timestamp { get; }
