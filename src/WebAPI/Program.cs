99c46e6a2fcc0785003f0c5337ccc32fafa95474
Antônio Falcão Jr <antonio.falcao@xpi.com.br>
2021-08-31 17:23:27 -0300

    Removing startup

--- src/WebAPI/Program.cs
+++ src/WebAPI/Program.cs
@@ -1,64 +1,84 @@
 using System;
-using System.IO;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
+using Infrastructure.DependencyInjection.Extensions;
+using Infrastructure.DependencyInjection.Options;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Serilog;
 
-namespace WebAPI
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddLogging(loggingBuilder =>
 {
-    public static class Program
-    {
-        private static IHostBuilder CreateHostBuilder(string[] args)
-            => Host.CreateDefaultBuilder(args)
-                .UseSerilog()
-                .ConfigureWebHostDefaults(webBuilder
-                    => webBuilder.UseStartup<Startup>())
-                .UseDefaultServiceProvider(
-                    (context, options) =>
-                    {
-                        options.ValidateScopes = context.HostingEnvironment.IsDevelopment();
-                        options.ValidateOnBuild = true;
-                    });
+    Log.Logger = new LoggerConfiguration().ReadFrom
+        .Configuration(builder.Configuration)
+        .CreateLogger();
+    
+    loggingBuilder.ClearProviders();
+    loggingBuilder.AddSerilog();
+});
 
-        public static async Task Main(string[] args)
+builder.Services.AddSwaggerGen(options
+    => options.SwaggerDoc(
+        name: "v1",
+        info: new()
         {
-            var configuration = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", false, true)
-                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", true, true)
-                .Build();
+            Title = "WebAPI",
+            Version = "v1"
+        }));
 
-            Log.Logger = new LoggerConfiguration()
-                .ReadFrom.Configuration(configuration)
-                .CreateLogger();
+builder.Services.AddApplicationServices();
 
-            try
-            {
-                var host = CreateHostBuilder(args).Build();
-                await host.MigrateDatabaseAsync();
-                await host.RunAsync();
-                Log.Information("Stopped cleanly");
-            }
-            catch (Exception ex)
-            {
-                Log.Fatal(ex, "An unhandled exception occured during bootstrapping");
-            }
-            finally
-            {
-                Log.CloseAndFlush();
-            }
-        }
+builder.Services.AddEventStoreRepositories();
+builder.Services.AddProjectionsRepositories();
 
-        private static async Task MigrateDatabaseAsync(this IHost host)
-        {
-            await using var scope = host.Services.CreateAsyncScope();
-            var dbContext = scope.ServiceProvider.GetRequiredService<DbContext>();
-            await dbContext.Database.MigrateAsync();
-            await dbContext.Database.EnsureCreatedAsync();
-        }
-    }
+builder.Services.AddEventStoreDbContext();
+builder.Services.AddProjectionsDbContext();
+
+builder.Services.AddMassTransitWithRabbitMq(options
+    => builder.Configuration.Bind(nameof(RabbitMqOptions), options));
+
+builder.Services.ConfigureEventStoreOptions(
+    builder.Configuration.GetSection(nameof(EventStoreOptions)));
+
+builder.Services.ConfigureMongoDbOptions(
+    builder.Configuration.GetSection(nameof(MongoDbOptions)));
+
+builder.Services.ConfigureSqlServerRetryingOptions(
+    builder.Configuration.GetSection(nameof(SqlServerRetryingOptions)));
+
+var app = builder.Build();
+
+if (builder.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(options
+        => options.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1"));
+}
+
+app.UseRouting();
+app.UseEndpoints(endpoints => endpoints.MapControllers());
+
+try
+{
+    await using var scope = app.Services.CreateAsyncScope();
+    var dbContext = scope.ServiceProvider.GetRequiredService<DbContext>();
+    await dbContext.Database.MigrateAsync();
+    await dbContext.Database.EnsureCreatedAsync();
+    await app.RunAsync();
+    Log.Information("Stopped cleanly");
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "An unhandled exception occured during bootstrapping");
+}
+finally
+{
+    Log.CloseAndFlush();
 }
