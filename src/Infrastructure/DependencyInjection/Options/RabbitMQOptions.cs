136bf7676cc8ca3ab3deb2ec4d95d1d603d9d7b8
Antônio Falcão Jr <antonio.falcao@xpi.com.br>
2021-08-31 10:03:28 -0300

    Adding Virtual Host to RabbitMQOptions

--- src/Infrastructure/DependencyInjection/Options/RabbitMQOptions.cs
+++ src/Infrastructure/DependencyInjection/Options/RabbitMQOptions.cs
@@ -5,5 +5,6 @@ namespace Infrastructure.DependencyInjection.Options
         public string Host { get; init; }
         public string Username { get; init; }
         public string Password { get; init; }
+        public string VirtualHost { get; set; }
     }
 }
