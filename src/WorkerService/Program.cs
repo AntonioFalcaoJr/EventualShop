dfdf278d899cc06be9a6a782e5e203ef4d7022f9
Antônio Falcão Jr <antonio.falcao@xpi.com.br>
2021-08-31 22:17:24 -0300

    Adjusting Program.cs

--- src/WorkerService/Program.cs
+++ src/WorkerService/Program.cs
@@ -1,33 +1,35 @@
+using System;
 using Infrastructure.DependencyInjection.Extensions;
 using Infrastructure.DependencyInjection.Options;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using WorkerService;
 
-var host = Host.CreateDefaultBuilder(args);
+var builder = Host.CreateDefaultBuilder(args);
 
-host.UseDefaultServiceProvider((context, options) =>
+builder.UseDefaultServiceProvider((context, options) =>
 {
     options.ValidateScopes = context.HostingEnvironment.IsDevelopment();
     options.ValidateOnBuild = true;
 });
 
-host.ConfigureLogging((context, builder) =>
+builder.ConfigureLogging((context, loggingBuilder) =>
 {
     Log.Logger = new LoggerConfiguration().ReadFrom
         .Configuration(context.Configuration)
         .CreateLogger();
 
-    builder.AddSerilog();
+    loggingBuilder.AddSerilog();
 });
 
-host.UseSerilog();
+builder.UseSerilog();
 
-host.ConfigureServices((context, services) =>
+builder.ConfigureServices((context, services) =>
 {
-    services.AddHostedService<WorkerService.Worker>();
+    services.AddHostedService<Worker>();
 
     services.AddApplicationServices();
 
@@ -50,4 +52,22 @@ host.ConfigureServices((context, services) =>
         context.Configuration.GetSection(nameof(SqlServerRetryingOptions)));
 });
 
-await host.Build().RunAsync();
+var host = builder.Build();
+
+try
+{
+    await using var scope = host.Services.CreateAsyncScope();
+    var dbContext = scope.ServiceProvider.GetRequiredService<DbContext>();
+    await dbContext.Database.MigrateAsync();
+    await dbContext.Database.EnsureCreatedAsync();
+    await host.RunAsync();
+    Log.Information("Stopped cleanly");
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "An unhandled exception occured during bootstrapping");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
