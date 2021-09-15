761341e9ff2cfd43a66d873921066e0d7ad6aa7b
Antônio Falcão Jr <antonio.falcao@xpi.com.br>
2021-09-15 09:52:41 -0300

    Adding UpdateCatalogItem contract

---
+++ src/Contracts/Messages/Catalogs/Commands/UpdateCatalogItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Messages.Catalogs.Commands
+{
+    public interface UpdateCatalogItem
+    {
+        Guid CatalogId { get; }
+        Guid CatalogItemId { get; }
+        string Name { get; }
+        string Description { get; }
+        decimal Price { get; }
+        string PictureUri { get; }
+    }
+}
