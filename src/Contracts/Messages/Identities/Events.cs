64547e14057db960574585848c15ed72331351d7
Antônio Falcão Jr <antonio.falcao@xpi.com.br>
2021-09-27 21:42:43 -0300

    Fixing command

--- src/Contracts/Messages/Identities/Events.cs
+++ src/Contracts/Messages/Identities/Events.cs
@@ -7,7 +7,7 @@ namespace Messages.Identities
     {
         public record UserDeleted(Guid UserId) : Message<Guid>(UserId), IEvent;
 
-        public record UserRegistered(Guid UserId, string Email, string FirstName, string PasswordConfirmation, string Login) : Message<Guid>(UserId), IEvent;
+        public record UserRegistered(Guid UserId, string Email, string FirstName, string Password, string PasswordConfirmation) : Message<Guid>(UserId), IEvent;
 
         public record UserPasswordChanged(Guid UserId, string NewPassword, string NewPasswordConfirmation) : Message<Guid>(UserId), IEvent;
     }
