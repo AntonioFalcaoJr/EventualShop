84fdeda99141955c92c4c81c2acd50ce31292a40
Antônio Falcão Jr <antonio.falcao@xpi.com.br>
2021-10-03 23:13:20 -0300

    CleanUp

--- src/Services/Catalog/Application/EventSourcing/Projections/CatalogItemProjection.cs
+++ src/Services/Catalog/Application/EventSourcing/Projections/CatalogItemProjection.cs
@@ -5,12 +5,11 @@ namespace Application.EventSourcing.Projections
 {
     public record CatalogItemProjection : IProjection
     {
-        public Guid AggregateId { get; init; }
         public string Name { get; init; }
         public string Description { get; init; }
         public decimal Price { get; init; }
         public string PictureUri { get; init; }
-        
+        public Guid AggregateId { get; init; }
         public bool IsDeleted { get; init; }
     }
 }
--- src/Services/Catalog/Application/EventSourcing/Projections/CatalogProjection.cs
+++ src/Services/Catalog/Application/EventSourcing/Projections/CatalogProjection.cs
@@ -6,10 +6,10 @@ namespace Application.EventSourcing.Projections
 {
     public record CatalogProjection : IProjection
     {
-        public Guid AggregateId { get; init; }
         public string Title { get; init; }
-        public bool IsDeleted { get; init; }
         public bool IsActive { get; init; }
-        public IEnumerable<CatalogItemProjection> Items { get; init; } 
+        public IEnumerable<CatalogItemProjection> Items { get; init; }
+        public Guid AggregateId { get; init; }
+        public bool IsDeleted { get; init; }
     }
 }
