6936c239e7d37ae0c916f6a8e16cc03609db8eec
Antônio Falcão Jr <antonio.falcao@xpi.com.br>
2021-10-09 14:54:49 -0300

    Adding Address and CreditCard value objects and respective validators

---
+++ src/Services/ShoppingCart/Domain/ValueObjects/CreditCards/CreditCard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Domain.Abstractions.ValueObjects;
+
+namespace Domain.ValueObjects.CreditCards
+{
+    public record CreditCard : ValueObject
+    {
+        public DateOnly Expiration { get; init; }
+        public string HolderName { get; init; }
+        public string Number { get; init; }
+        public string SecurityNumber { get; init; }
+
+        protected override bool Validate()
+            => OnValidate<CreditCardValidator, CreditCard>();
+    }
+}
