cbd8056ec13ba4827a32fc387172f2db7104e98f
Antônio Falcão Jr <antonio.falcao@xpi.com.br>
2021-10-26 20:37:26 -0300

    Adding payment contracts

---
+++ src/Contracts/Messages/Payments/Queries.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Messages.Abstractions.Queries;
+
+namespace Messages.Payments
+{
+    public static class Queries
+    {
+        public record GetPaymentDetails(Guid PaymentId) : Query;
+    }
+}
---
+++ src/Contracts/Messages/Payments/Responses.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Messages.Abstractions.Queries.Responses;
+
+namespace Messages.Payments
+{
+    public static class Responses
+    {
+        public record PaymentDetails : Response
+        {
+            public Guid Id { get; init; }
+            public Guid OrderId { get; init; }
+            public decimal Amount { get; init; }
+            public string Status { get; init; }
+            public bool IsDeleted { get; init; }
+        }
+    }
+}
