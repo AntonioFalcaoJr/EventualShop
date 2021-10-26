8110891d71f4d757da7c167e168be83ede85d38f
Antônio Falcão Jr <antonio.falcao@xpi.com.br>
2021-10-26 20:40:35 -0300

    Adding WorkerService project

---
+++ src/Services/Payment/WorkerService/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Reflection;
+using Infrastructure.DependencyInjection.Extensions;
+using Infrastructure.DependencyInjection.Options;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+var builder = Host.CreateDefaultBuilder(args);
+
+builder.UseDefaultServiceProvider((context, options) =>
+{
+    options.ValidateScopes = context.HostingEnvironment.IsDevelopment();
+    options.ValidateOnBuild = true;
+});
+
+builder.ConfigureAppConfiguration(configurationBuilder =>
+{
+    configurationBuilder
+        .AddUserSecrets(Assembly.GetExecutingAssembly())
+        .AddEnvironmentVariables();
+});
+
+builder.ConfigureLogging((context, loggingBuilder) =>
+{
+    Log.Logger = new LoggerConfiguration().ReadFrom
+        .Configuration(context.Configuration)
+        .CreateLogger();
+
+    loggingBuilder.ClearProviders();
+    loggingBuilder.AddSerilog();
+});
+
+builder.UseSerilog();
+
+builder.ConfigureServices((context, services) =>
+{
+    services.AddLogging(loggingBuilder
+        => loggingBuilder.AddSerilog());
+
+    services.AddApplicationServices();
+
+    services.AddEventStoreRepositories();
+    services.AddProjectionsRepositories();
+
+    services.AddEventStoreDbContext();
+    services.AddProjectionsDbContext();
+
+    services.AddMessageFluentValidation();
+
+    services.AddMassTransitWithRabbitMq(options
+        => context.Configuration.Bind(nameof(RabbitMqOptions), options));
+
+    services.ConfigureEventStoreOptions(
+        context.Configuration.GetSection(nameof(EventStoreOptions)));
+
+    services.ConfigureSqlServerRetryingOptions(
+        context.Configuration.GetSection(nameof(SqlServerRetryingOptions)));
+});
+
+var host = builder.Build();
+
+try
+{
+    await using var scope = host.Services.CreateAsyncScope();
+    var dbContext = scope.ServiceProvider.GetRequiredService<DbContext>();
+    await dbContext.Database.MigrateAsync();
+    await dbContext.Database.EnsureCreatedAsync();
+    await host.RunAsync();
+    Log.Information("Stopped cleanly");
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "An unhandled exception occured during bootstrapping");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
