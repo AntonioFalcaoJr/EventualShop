7808b12a571e0f6ad38f2b9b479b25b1c842f43d
Antônio Falcão Jr <antonio.falcao@xpi.com.br>
2022-01-18 12:13:22 -0300

    Adding contract validator filter

---
+++ src/Services/Account/Infrastructure/DependencyInjection/Filters/ContractValidatorFilter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using ECommerce.Abstractions.Validations;
+using FluentValidation;
+using GreenPipes;
+using MassTransit;
+using MassTransit.Definition;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using ValidationResult = FluentValidation.Results.ValidationResult;
+
+namespace Infrastructure.DependencyInjection.Filters;
+
+public class ContractValidatorFilter<T> : IFilter<ConsumeContext<T>>
+    where T : class
+{
+    private readonly IValidator<T> _validator;
+    private ValidationResult _validationResult = new();
+
+    public ContractValidatorFilter(IServiceProvider serviceProvider)
+    {
+        _validator = serviceProvider.GetService<IValidator<T>>();
+    }
+
+    public async Task Send(ConsumeContext<T> context, IPipe<ConsumeContext<T>> next)
+    {
+        if (_validator is not null)
+            _validationResult = await _validator.ValidateAsync(context.Message, context.CancellationToken);
+
+        if (_validationResult.IsValid is false)
+        {
+            Log.Error("Message validation errors: {Errors}", _validationResult.Errors);
+
+            await context.Send(
+                destinationAddress: new($"queue:account-cart.{KebabCaseEndpointNameFormatter.Instance.SanitizeName(typeof(T).Name)}.validation-errors"),
+                message: new ValidationResultMessage<T>(context.Message, _validationResult.Errors.Select(failure => failure.ErrorMessage)));
+
+            return;
+        }
+
+        await next.Send(context);
+    }
+
+    public void Probe(ProbeContext context)
+        => context.CreateFilterScope("Contract validation");
+}
