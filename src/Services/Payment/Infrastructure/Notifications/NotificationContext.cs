10fff128a0a155cd4cf24f35c9bf9fd636b7a441
Antônio Falcão Jr <antonio.falcao@xpi.com.br>
2022-01-18 12:37:24 -0300

    Implementing Notification Context

---
+++ src/Services/Payment/Infrastructure/Notifications/NotificationContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Application.Abstractions;
+using Application.Abstractions.Notifications;
+using FluentValidation.Results;
+
+namespace Infrastructure.Notifications;
+
+public class NotificationContext : INotificationContext
+{
+    private readonly List<string> _notifications = new();
+
+    public IEnumerable<string> Errors
+        => _notifications;
+
+    public bool HasNotifications
+        => Errors.Any();
+
+    public void AddNotification(string message)
+        => _notifications.Add(message);
+
+    public void AddErrors(IEnumerable<ValidationFailure> validationFailures)
+        => _notifications.AddRange(validationFailures.Select(failure => failure.ErrorMessage));
+}
