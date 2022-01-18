82ac0c463be84cddcc233a68594730dd2399cf65
Antônio Falcão Jr <antonio.falcao@xpi.com.br>
2022-01-18 12:37:04 -0300

    Defining notification context interface

---
+++ src/Services/ShoppingCart/Application/Abstractions/Notifications/INotificationContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using FluentValidation.Results;
+
+namespace Application.Abstractions.Notifications;
+
+public interface INotificationContext
+{
+    bool HasNotifications { get; }
+    IEnumerable<string> Errors { get; }
+    void AddErrors(IEnumerable<ValidationFailure> validationFailures);
+}
