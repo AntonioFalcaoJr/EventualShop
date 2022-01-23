9094918614783e60bf1599443dff8f7b8b7e5d32
Antônio Falcão Jr <antonio.falcao@xpi.com.br>
2022-01-23 11:09:03 -0300

    Validations result message

--- src/Contracts/ECommerce/Abstractions/Validations/ValidationResultMessage.cs
+++ src/Contracts/ECommerce/Abstractions/Validations/ValidationResultMessage.cs
@@ -1,5 +1,11 @@
-﻿using FluentValidation.Results;
+﻿using System.Collections.Generic;
+using MassTransit.Topology;
 
 namespace ECommerce.Abstractions.Validations;
 
-public record ValidationResultMessage<TMessage>(TMessage Message, ValidationResult ValidationResult);
+[ExcludeFromTopology]
+public abstract record ValidationResultMessage<TMessage>(TMessage Message, IEnumerable<string> Errors);
+
+public record BusinessValidationResult<TMessage>(TMessage Message, IEnumerable<string> Errors) : ValidationResultMessage<TMessage>(Message, Errors);
+
+public record ContractValidationResult<TMessage>(TMessage Message, IEnumerable<string> Errors) : ValidationResultMessage<TMessage>(Message, Errors);
