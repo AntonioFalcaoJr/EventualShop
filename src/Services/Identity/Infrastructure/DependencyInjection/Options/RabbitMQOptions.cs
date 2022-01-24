910ec846a753d4cf944816b2dd49971d15c4fbc7
Antônio Falcão Jr <antonio.falcao@xpi.com.br>
2022-01-23 23:06:08 -0300

    Improving RabbitMQOptions to support retry strategy

--- src/Services/Identity/Infrastructure/DependencyInjection/Options/RabbitMQOptions.cs
+++ src/Services/Identity/Infrastructure/DependencyInjection/Options/RabbitMQOptions.cs
@@ -21,4 +21,16 @@ public record RabbitMqOptions
 
     [Required, MinLength(5)]
     public string SchedulerQueueName { get; init; }
+
+    [Required, Range(1, 10)]
+    public int RetryLimit { get; init; }
+
+    [Required, Range(1, 10)]
+    public int MinimumInterval { get; init; }
+
+    [Required, Range(2, 100)]
+    public int MaximumInterval { get; init; }
+
+    [Required, Range(0, 10)]
+    public int IntervalDelta { get; init; }
 }
