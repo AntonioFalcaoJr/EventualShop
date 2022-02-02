1f91bcb38e940fc6221bad6a34becf4131f3fafc
Antônio Falcão Jr <antonio.falcao@xpi.com.br>
2022-02-01 23:42:01 -0300

    Defining CorrelationId

--- src/Contracts/ECommerce/Contracts/Catalog/Commands.cs
+++ src/Contracts/ECommerce/Contracts/Catalog/Commands.cs
@@ -7,17 +7,17 @@ public static class Commands
 {
     public record CreateCatalog(string Title) : Command;
 
-    public record DeleteCatalog(Guid CatalogId) : Command;
+    public record DeleteCatalog(Guid CatalogId) : Command(CatalogId);
 
-    public record UpdateCatalog(Guid CatalogId, string Title) : Command;
+    public record UpdateCatalog(Guid CatalogId, string Title) : Command(CatalogId);
 
-    public record ActivateCatalog(Guid CatalogId) : Command;
+    public record ActivateCatalog(Guid CatalogId) : Command(CatalogId);
 
-    public record DeactivateCatalog(Guid CatalogId) : Command;
+    public record DeactivateCatalog(Guid CatalogId) : Command(CatalogId);
 
-    public record RemoveCatalogItem(Guid CatalogId, Guid CatalogItemId) : Command;
+    public record RemoveCatalogItem(Guid CatalogId, Guid CatalogItemId) : Command(CatalogId);
 
-    public record AddCatalogItem(Guid CatalogId, string Name, string Description, decimal Price, string PictureUri) : Command;
+    public record AddCatalogItem(Guid CatalogId, string Name, string Description, decimal Price, string PictureUri) : Command(CatalogId);
 
-    public record UpdateCatalogItem(Guid CatalogId, Guid CatalogItemId, string Name, string Description, decimal Price, string PictureUri) : Command;
+    public record UpdateCatalogItem(Guid CatalogId, Guid CatalogItemId, string Name, string Description, decimal Price, string PictureUri) : Command(CatalogId);
 }
