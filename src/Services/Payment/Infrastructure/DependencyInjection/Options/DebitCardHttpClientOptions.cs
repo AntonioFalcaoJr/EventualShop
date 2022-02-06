a03a21eced73e929be6e3ef0fa20062f09562f0a
Antônio Falcão Jr <antonio.falcao@xpi.com.br>
2022-02-06 11:36:09 -0300

    Adding resilience options

--- src/Services/Payment/Infrastructure/DependencyInjection/Options/DebitCardHttpClientOptions.cs
+++ src/Services/Payment/Infrastructure/DependencyInjection/Options/DebitCardHttpClientOptions.cs
@@ -18,4 +18,22 @@ public record DebitCardHttpClientOptions
     
     [Required, MinLength(5)]
     public string CaptureEndpoint { get; init; }
+        
+    [Required, Range(5, 200)]
+    public int OverallTimeout { get; init; }
+
+    [Required, Range(2, 20)]
+    public int RetryCount { get; init; }
+    
+    [Required, Range(2, 20)]
+    public int SleepDurationPower { get; init; }
+    
+    [Required, Range(2, 20)]
+    public int EachRetryTimeout { get; init; }
+
+    [Required, Range(2, 20)]
+    public int CircuitBreaking { get; init; }
+
+    [Required, Range(2, 20)]
+    public int DurationOfBreak { get; init; }
 }
