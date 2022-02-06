1af6de8b225c74f18618d79c09c4a88c6e25a4d6
Antônio Falcão Jr <antonio.falcao@xpi.com.br>
2022-02-06 11:37:34 -0300

    Adding HTTP Policies

---
+++ src/Services/Payment/Infrastructure/DependencyInjection/HttpPolicies/HttpPolicy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Polly;
+using Polly.CircuitBreaker;
+using Polly.Extensions.Http;
+using Polly.Timeout;
+using Serilog;
+
+namespace Infrastructure.DependencyInjection.HttpPolicies;
+
+public static class HttpPolicy
+{
+    public static IAsyncPolicy<HttpResponseMessage> GetRetryPolicyAsync(int retryCount, int sleepDurationPower, int eachRetryTimeout)
+        => HttpPolicyExtensions
+            .HandleTransientHttpError()
+            .Or<TimeoutRejectedException>()
+            .WaitAndRetryAsync(
+                retryCount: retryCount,
+                sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(Math.Pow(sleepDurationPower, retryAttempt)),
+                onRetry: (result, waitingTime, retryAttempt, _)
+                    => Log.Warning(@"Retrying in {WaitingTime}s. Attempt: {RetryAttempt}/{RetryCount}. Status Code: {StatusCode}. Message: {Message}",
+                        waitingTime.TotalSeconds, retryAttempt, retryCount, result?.Result?.StatusCode ?? default, result?.Exception?.Message ?? "No message"))
+            .WrapAsync(
+                Policy.TimeoutAsync<HttpResponseMessage>(
+                    seconds: eachRetryTimeout,
+                    timeoutStrategy: TimeoutStrategy.Optimistic,
+                    onTimeoutAsync: async (_, timeout, _) => { await Task.Yield(); Log.Warning("Timeout applied after {AwaitedTime}s", timeout); }));
+
+    public static IAsyncPolicy<HttpResponseMessage> GetCircuitBreakerPolicyAsync(int circuitBreaking, int durationOfBreak)
+        => HttpPolicyExtensions
+            .HandleTransientHttpError()
+            .Or<TimeoutRejectedException>()
+            .CircuitBreakerAsync(
+                handledEventsAllowedBeforeBreaking: circuitBreaking,
+                durationOfBreak: TimeSpan.FromSeconds(durationOfBreak),
+                onBreak: (_, state, breakingTime, _) => { Log.Warning("Circuit breaking! State: {State}. Break time: {BreakingTime}s", state, breakingTime.TotalSeconds); },
+                onReset: _ => { Log.Warning("Circuit resetting!"); },
+                onHalfOpen: () => { Log.Warning("Circuit transitioning to {State}", CircuitState.HalfOpen); });
+}
