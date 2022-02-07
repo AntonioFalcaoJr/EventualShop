9959e706f9708fd73813ab0250e9b61ced937ef0
Antônio Falcão Jr <antonio.falcao@xpi.com.br>
2022-02-07 09:42:34 -0300

    Using IPagedInfo from contracts

--- src/Services/Payment/Application/Abstractions/EventSourcing/Projections/Pagination/IPageInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Application.Abstractions.EventSourcing.Projections.Pagination;
-
-public interface IPageInfo
-{
-    int Current { get; }
-    int Size { get; }
-    bool HasPrevious { get; }
-    bool HasNext { get; }
-}
