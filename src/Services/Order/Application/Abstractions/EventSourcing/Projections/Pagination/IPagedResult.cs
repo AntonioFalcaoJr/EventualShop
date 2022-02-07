d6296e15dde77bae519886e7dc7a46a8c925c352
Antônio Falcão Jr <antonio.falcao@xpi.com.br>
2022-02-07 09:42:16 -0300

    Using IPagedResult from contracts

--- src/Services/Order/Application/Abstractions/EventSourcing/Projections/Pagination/IPagedResult.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Application.Abstractions.EventSourcing.Projections.Pagination;
-
-public interface IPagedResult<out T>
-{
-    IEnumerable<T> Items { get; }
-    IPageInfo PageInfo { get; }
-}
