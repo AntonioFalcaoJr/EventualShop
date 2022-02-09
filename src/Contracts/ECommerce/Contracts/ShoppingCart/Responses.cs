094da6e2ea6906ddd014afee71e5b8050543e27c
Antônio Falcão Jr <antonio.falcao@xpi.com.br>
2022-02-08 23:50:05 -0300

    Removing unnecessary annotation

--- src/Contracts/ECommerce/Contracts/ShoppingCart/Responses.cs
+++ src/Contracts/ECommerce/Contracts/ShoppingCart/Responses.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using ECommerce.Abstractions.Messages.Queries.Responses;
 using ECommerce.Contracts.Common;
-using Newtonsoft.Json;
 
 namespace ECommerce.Contracts.ShoppingCart;
 
@@ -13,10 +12,7 @@ public static class Responses
     public record CartDetails : Response
     {
         public IEnumerable<Models.Item> CartItems { get; init; }
-
-        [JsonProperty(TypeNameHandling = TypeNameHandling.Objects)]
         public IEnumerable<Models.IPaymentMethod> PaymentMethods { get; init; }
-
         public Guid CustomerId { get; init; }
         public decimal Total { get; init; }
         public Guid Id { get; init; }
