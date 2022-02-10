936a95b70d7c57fd2c2a95b1acdb73e98387337c
Antônio Falcão Jr <antonio.falcao@xpi.com.br>
2022-02-10 00:00:35 -0300

    Request validator

---
+++ src/Web/WebAPI/ECommerce.WebAPI/DataTransferObjects/ShoppingCarts/Validators/CreateCartValidator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using FluentValidation;
+
+namespace ECommerce.WebAPI.DataTransferObjects.ShoppingCarts.Validators;
+
+public class CreateCartValidator : AbstractValidator<Requests.CreateCart>
+{
+    public CreateCartValidator()
+    {
+        RuleFor(cart => cart.CustomerId)
+            .NotEqual(Guid.Empty);
+    }
+}
