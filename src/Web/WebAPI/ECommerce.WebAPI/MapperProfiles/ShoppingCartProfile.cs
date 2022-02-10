12ec6f610a3f2cedaa284a158ae65e3321badf59
Antônio Falcão Jr <antonio.falcao@xpi.com.br>
2022-02-10 00:04:51 -0300

    Adding mapping profile

---
+++ src/Web/WebAPI/ECommerce.WebAPI/MapperProfiles/ShoppingCartProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using ECommerce.Contracts.ShoppingCart;
+
+namespace ECommerce.WebAPI.MapperProfiles;
+
+public class ShoppingCartProfile : Profile
+{
+    public ShoppingCartProfile()
+    {
+        CreateMap<Responses.CartDetails, DataTransferObjects.ShoppingCarts.Outputs.CartDetails>();
+    }
+}
