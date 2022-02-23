170cd9d62b101994164ba0a1be088a93adb52bae
Antônio Falcão Jr <antonio.falcao@xpi.com.br>
2022-02-23 10:36:58 -0300

    Removing resources from LoggingConsumeObserver

--- src/Services/ShoppingCart/Infrastructure/DependencyInjection/PipeObservers/LoggingConsumeObserver.cs
+++ src/Services/ShoppingCart/Infrastructure/DependencyInjection/PipeObservers/LoggingConsumeObserver.cs
@@ -10,24 +10,16 @@ public class LoggingConsumeObserver : IConsumeObserver
     public async Task PreConsume<T>(ConsumeContext<T> context)
         where T : class
     {
+        await Task.Yield();
         var messageType = context.Message.GetType();
         Log.Information("Consuming {Message} message from {Namespace}, CorrelationId: {CorrelationId}", messageType.Name, messageType.Namespace, context.CorrelationId);
-        await Task.CompletedTask;
     }
 
-    public async Task PostConsume<T>(ConsumeContext<T> context)
+    public Task PostConsume<T>(ConsumeContext<T> context)
         where T : class
-    {
-        var messageType = context.Message.GetType();
-        Log.Information("{Message} message from {Namespace} was consumed, CorrelationId: {CorrelationId}", messageType.Name, messageType.Namespace, context.CorrelationId);
-        await Task.CompletedTask;
-    }
+        => Task.CompletedTask;
 
-    public async Task ConsumeFault<T>(ConsumeContext<T> context, Exception exception)
+    public Task ConsumeFault<T>(ConsumeContext<T> context, Exception exception)
         where T : class
-    {
-        var messageType = context.Message.GetType();
-        Log.Warning("Fault on consuming message {Message} from {Namespace}, CorrelationId: {CorrelationId}", messageType.Name, messageType.Namespace, context.CorrelationId);
-        await Task.CompletedTask;
-    }
+        => Task.CompletedTask;
 }
--- src/Web/WebAPI/ECommerce.WebAPI/DependencyInjection/PipeObservers/LoggingConsumeObserver.cs
+++ src/Web/WebAPI/ECommerce.WebAPI/DependencyInjection/PipeObservers/LoggingConsumeObserver.cs
@@ -15,19 +15,11 @@ public class LoggingConsumeObserver : IConsumeObserver
         Log.Information("Consuming {Message} message from {Namespace}, CorrelationId: {CorrelationId}", messageType.Name, messageType.Namespace, context.CorrelationId);
     }
 
-    public async Task PostConsume<T>(ConsumeContext<T> context)
+    public Task PostConsume<T>(ConsumeContext<T> context)
         where T : class
-    {
-        await Task.Yield();
-        var messageType = context.Message.GetType();
-        Log.Information("{Message} message from {Namespace} was consumed, CorrelationId: {CorrelationId}", messageType.Name, messageType.Namespace, context.CorrelationId);
-    }
+        => Task.CompletedTask;
 
-    public async Task ConsumeFault<T>(ConsumeContext<T> context, Exception exception)
+    public Task ConsumeFault<T>(ConsumeContext<T> context, Exception exception)
         where T : class
-    {
-        await Task.Yield();
-        var messageType = context.Message.GetType();
-        Log.Warning("Fault on consuming message {Message} from {Namespace}, Error {Error}, CorrelationId: {CorrelationId}", messageType.Name, messageType.Namespace, exception.Message, context.CorrelationId);
-    }
+        => Task.CompletedTask;
 }
