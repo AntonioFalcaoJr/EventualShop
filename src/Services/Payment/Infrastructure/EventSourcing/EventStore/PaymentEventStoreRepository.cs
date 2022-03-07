f82fddaf04a9779d2e6063801754eac7ed081fb3
Antônio Falcão Jr <antonio.falcao@xpi.com.br>
2022-03-07 12:44:16 -0300

    Making parameter DbContext

--- src/Services/Payment/Infrastructure/EventSourcing/EventStore/PaymentEventStoreRepository.cs
+++ src/Services/Payment/Infrastructure/EventSourcing/EventStore/PaymentEventStoreRepository.cs
@@ -4,11 +4,12 @@ using Application.EventSourcing.EventStore.Events;
 using Domain.Aggregates;
 using Infrastructure.Abstractions.EventSourcing.EventStore;
 using Infrastructure.EventSourcing.EventStore.Contexts;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.EventSourcing.EventStore;
 
 public class PaymentEventStoreRepository : EventStoreRepository<Payment, PaymentStoreEvent, PaymentSnapshot, Guid>, IPaymentEventStoreRepository
 {
-    public PaymentEventStoreRepository(EventStoreDbContext dbContext) 
+    public PaymentEventStoreRepository(DbContext dbContext) 
         : base(dbContext) { }
 }
