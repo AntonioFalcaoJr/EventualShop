917c190609c382c97ab3943174674856fcb18f16
Antônio Falcão Jr <antonio.falcao@xpi.com.br>
2022-03-07 19:35:51 -0300

    Adding GreaterThan rule

--- src/Services/ShoppingCart/Domain/Entities/CartItems/CartItemValidator.cs
+++ src/Services/ShoppingCart/Domain/Entities/CartItems/CartItemValidator.cs
@@ -1,6 +1,14 @@
 ﻿using System;
 using Domain.Abstractions.Validators;
+using FluentValidation;
 
 namespace Domain.Entities.CartItems;
 
-public class CartItemValidator : EntityValidator<CartItem, Guid> { }
+public class CartItemValidator : EntityValidator<CartItem, Guid>
+{
+    public CartItemValidator()
+    {
+        RuleFor(item => item.Quantity)
+            .GreaterThan(0);
+    }
+}
