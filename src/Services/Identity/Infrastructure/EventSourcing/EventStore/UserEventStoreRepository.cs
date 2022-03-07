f89a2c9d57a4f0a9c27ad013d8c5dca8cc0cfd06
Antônio Falcão Jr <antonio.falcao@xpi.com.br>
2022-03-07 12:54:51 -0300

    Making parameter DbContext

--- src/Services/Identity/Infrastructure/EventSourcing/EventStore/UserEventStoreRepository.cs
+++ src/Services/Identity/Infrastructure/EventSourcing/EventStore/UserEventStoreRepository.cs
@@ -3,12 +3,12 @@ using Application.EventSourcing.EventStore;
 using Application.EventSourcing.EventStore.Events;
 using Domain.Aggregates;
 using Infrastructure.Abstractions.EventSourcing.EventStore;
-using Infrastructure.EventSourcing.EventStore.Contexts;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.EventSourcing.EventStore;
 
 public class UserEventStoreRepository : EventStoreRepository<User, UserStoreEvent, UserSnapshot, Guid>, IUserEventStoreRepository
 {
-    public UserEventStoreRepository(EventStoreDbContext dbContext)
+    public UserEventStoreRepository(DbContext dbContext)
         : base(dbContext) { }
 }
--- src/Services/Warehouse/Infrastructure/EventSourcing/EventStore/WarehouseEventStoreRepository.cs
+++ src/Services/Warehouse/Infrastructure/EventSourcing/EventStore/WarehouseEventStoreRepository.cs
@@ -3,12 +3,12 @@ using Application.EventSourcing.EventStore;
 using Application.EventSourcing.EventStore.Events;
 using Domain.Aggregates;
 using Infrastructure.Abstractions.EventSourcing.EventStore;
-using Infrastructure.EventSourcing.EventStore.Contexts;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.EventSourcing.EventStore;
 
 public class WarehouseEventStoreRepository : EventStoreRepository<InventoryItem, WarehouseStoreEvent, WarehouseSnapshot, Guid>, IWarehouseEventStoreRepository
 {
-    public WarehouseEventStoreRepository(EventStoreDbContext dbContext) 
+    public WarehouseEventStoreRepository(DbContext dbContext) 
         : base(dbContext) { }
 }
