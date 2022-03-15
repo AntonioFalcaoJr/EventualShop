173081f98b653a82980ee58913b7450b9e30598d
Antônio Falcão <antonio.falcao@xpi.com.br>
2022-03-15 19:44:17 -0300

    Adding test

---
+++ test/Load/test.js
@@ -0,0 +1,8 @@
+import http from 'k6/http';
+export const options = {
+  vus: 600,
+  duration: '60s',
+};
+export default function () {
+  http.get('http://nginx:4000/api/v1/shopping-carts/81f21228-4450-44bc-9ca9-47e13f8f14aa');
+}
