b2bbbd52408cc356734d02f71b4f4202e57a75b0
Antônio Falcão <antonio.falcao@xpi.com.br>
2022-03-18 15:51:49 -0300

    Moving capabilities to MessageBus project

---
+++ src/Services/Account/Infrastructure.MessageBus/DependencyInjection/Extensions/RabbitMqBusFactoryConfiguratorExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Application.UseCases.Events.Integrations;
+using Application.UseCases.Events.Projections;
+using ECommerce.Abstractions.Messages.Events;
+using ECommerce.Contracts.Account;
+using MassTransit;
+
+namespace Infrastructure.MessageBus.DependencyInjection.Extensions;
+
+internal static class RabbitMqBusFactoryConfiguratorExtensions
+{
+    public static void ConfigureEventReceiveEndpoints(this IRabbitMqBusFactoryConfigurator cfg, IRegistrationContext registration)
+    {
+        cfg.ConfigureEventReceiveEndpoint<CreateAccountWhenUserRegisteredConsumer, ECommerce.Contracts.Identity.DomainEvents.UserRegistered>(registration);
+        cfg.ConfigureEventReceiveEndpoint<ProjectAccountDetailsWhenAccountChangedConsumer, DomainEvents.ProfessionalAddressDefined>(registration);
+        cfg.ConfigureEventReceiveEndpoint<ProjectAccountDetailsWhenAccountChangedConsumer, DomainEvents.ProfileUpdated>(registration);
+        cfg.ConfigureEventReceiveEndpoint<ProjectAccountDetailsWhenAccountChangedConsumer, DomainEvents.ResidenceAddressDefined>(registration);
+        cfg.ConfigureEventReceiveEndpoint<ProjectAccountDetailsWhenAccountChangedConsumer, DomainEvents.AccountCreated>(registration);
+        cfg.ConfigureEventReceiveEndpoint<ProjectAccountDetailsWhenAccountChangedConsumer, DomainEvents.AccountDeleted>(registration);
+    }
+
+    private static void ConfigureEventReceiveEndpoint<TConsumer, TEvent>(this IRabbitMqBusFactoryConfigurator bus, IRegistrationContext registration)
+        where TConsumer : class, IConsumer
+        where TEvent : class, IEvent
+        => bus.ReceiveEndpoint(
+            queueName: $"account.{typeof(TConsumer).ToKebabCaseString()}.{typeof(TEvent).ToKebabCaseString()}",
+            configureEndpoint: endpoint =>
+            {
+                endpoint.ConfigureConsumeTopology = false;
+                endpoint.Bind<TEvent>();
+                endpoint.ConfigureConsumer<TConsumer>(registration);
+            });
+}
