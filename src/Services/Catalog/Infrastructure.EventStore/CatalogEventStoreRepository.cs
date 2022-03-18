1c0002261222a908ab6a9b3ac3accedae9931c54
Antônio Falcão <antonio.falcao@xpi.com.br>
2022-03-18 17:15:20 -0300

    Moving capabilities to EventStore project

---
+++ src/Services/Catalog/Infrastructure.EventStore/CatalogEventStoreRepository.cs
@@ -0,0 +1,14 @@
+using System;
+using Application.EventSourcing.EventStore;
+using Application.EventSourcing.EventStore.Events;
+using Domain.Aggregates;
+using Infrastructure.EventStore.Abstractions;
+using Infrastructure.EventStore.Contexts;
+
+namespace Infrastructure.EventStore;
+
+public class CatalogEventStoreRepository : EventStoreRepository<Catalog, CatalogStoreEvent, CatalogSnapshot, Guid>, ICatalogEventStoreRepository
+{
+    public CatalogEventStoreRepository(EventStoreDbContext dbContext) 
+        : base(dbContext) { }
+}
---
+++ src/Services/Catalog/Infrastructure.EventStore/DependencyInjection/Options/SqlServerRetryingOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Infrastructure.EventStore.DependencyInjection.Options;
+
+public class SqlServerRetryOptions
+{
+    [Required, Range(5, 20)]
+    public int MaxRetryCount { get; init; }
+
+    [Required, Timestamp]
+    public TimeSpan MaxRetryDelay { get; init; }
+
+    public int[] ErrorNumbersToAdd { get; init; }
+}
