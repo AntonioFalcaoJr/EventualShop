6757941446320420f39b47829de2639df572dd0f
Antônio Falcão <antonio.falcao@xpi.com.br>
2022-03-18 15:33:53 -0300

    Moving capabilities to EventStore project

---
+++ src/Services/Account/Infrastructure.EventStore/AccountEventStoreRepository.cs
@@ -0,0 +1,14 @@
+using System;
+using Application.EventSourcing.EventStore;
+using Application.EventSourcing.EventStore.Events;
+using Domain.Aggregates;
+using Infrastructure.EventStore.Abstractions;
+using Infrastructure.EventStore.Contexts;
+
+namespace Infrastructure.EventStore;
+
+public class AccountEventStoreRepository : EventStoreRepository<Account, AccountStoreEvent, AccountSnapshot, Guid>, IAccountEventStoreRepository
+{
+    public AccountEventStoreRepository(EventStoreDbContext dbContext)
+        : base(dbContext) { }
+}
--- src/Services/Account/Infrastructure.EventStore/Class1.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace Infrastructure.EventStore;
-
-public class Class1
-{
-}
