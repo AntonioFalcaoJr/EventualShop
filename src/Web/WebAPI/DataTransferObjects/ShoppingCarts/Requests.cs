46ae627dfdd99a395418dd57ea012f230f754cff
Antônio Falcão <antonio.falcao@xpi.com.br>
2022-03-28 10:10:26 -0300

    Naming adjusts

--- src/Web/WebAPI/DataTransferObjects/ShoppingCarts/Requests.cs
+++ src/Web/WebAPI/DataTransferObjects/ShoppingCarts/Requests.cs
@@ -8,12 +8,11 @@ public static class Requests
 {
     public record CreateCart(Guid CustomerId);
 
-    public record AddCartItem(Guid ProductId, string ProductName, decimal UnitPrice, int Quantity, string PictureUrl);
+    public record AddShoppingCartItem(Guid ProductId, string ProductName, decimal UnitPrice, int Quantity, string PictureUrl);
 
-    public record PaymentWithPayPal(string UserName, string Password, decimal Amount);
+    public record AddPayPal(string UserName, string Password, decimal Amount);
 
-    public record PaymentWithCreditCard(string HolderName, string Number, string SecurityNumber,
-        [property: JsonConverter(typeof(ExpirationDateOnlyJsonConverter))] DateOnly Expiration, decimal Amount);
+    public record AddCreditCard(string HolderName, string Number, string SecurityNumber, [property: JsonConverter(typeof(ExpirationDateOnlyJsonConverter))] DateOnly Expiration, decimal Amount);
 
     public record AddAddress(string City, string Country, int? Number, string State, string Street, string ZipCode);
 }
