b65e1cc3137e242453af42fae677a937e526166c
Antônio Falcão <antonio.falcao@xpi.com.br>
2022-03-30 18:48:19 -0300

    Segregating Shopping Cart infrastructure layers

---
+++ src/Services/ShoppingCart/Infrastructure.EventStore/ShoppingCartEventStoreService.cs
@@ -0,0 +1,16 @@
+﻿using Application.Abstractions.Notifications;
+using Application.EventSourcing.EventStore;
+using Application.EventSourcing.EventStore.Events;
+using Domain.Aggregates;
+using Infrastructure.EventStore.Abstractions;
+using Infrastructure.EventStore.DependencyInjection.Options;
+using MassTransit;
+using Microsoft.Extensions.Options;
+
+namespace Infrastructure.EventStore;
+
+public class ShoppingCartEventStoreService : EventStoreService<ShoppingCart, ShoppingCartStoreEvent, ShoppingCartSnapshot, Guid>, IShoppingCartEventStoreService
+{
+    public ShoppingCartEventStoreService(IPublishEndpoint publishEndpoint, IShoppingCartEventStoreRepository repository, INotificationContext notificationContext, IOptionsMonitor<EventStoreOptions> optionsMonitor)
+        : base(publishEndpoint, repository, notificationContext, optionsMonitor) { }
+}
