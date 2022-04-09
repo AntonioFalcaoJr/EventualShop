8a028ae5092a89c86c0b445cf3d6e0e28a4ebad5
Antônio Falcão <antonio.falcao@xpi.com.br>
2022-04-09 14:07:19 -0300

    Adding overload from Response contract to support Projections as Data

--- src/Contracts/ECommerce/Abstractions/Messages/Queries/Responses/Response.cs
+++ src/Contracts/ECommerce/Abstractions/Messages/Queries/Responses/Response.cs
@@ -3,6 +3,11 @@
 namespace ECommerce.Abstractions.Messages.Queries.Responses;
 
 [ExcludeFromTopology]
+public abstract record Response<T> : Message, IResponse
+{
+    public T Data { get; init; }
+}
+
 public abstract record Response : Message, IResponse;
 
 public record NotFound : Response;
