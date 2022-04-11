f56d83cc1ca558dc5212784bf27d8b0577016ca4
Antônio Falcão <antonio.falcao@xpi.com.br>
2022-04-11 19:30:34 -0300

    Adding http clients

---
+++ src/Web/WebAPP/HttpClients/ECommerceHttpClient.cs
@@ -0,0 +1,29 @@
+﻿using ECommerce.Contracts.Catalogs;
+using Microsoft.Extensions.Options;
+using WebAPP.Abstractions.Http;
+using WebAPP.DependencyInjection.Options;
+
+namespace WebAPP.HttpClients;
+
+public class ECommerceHttpClient : ApplicationHttpClient, IECommerceHttpClient
+{
+    private readonly ECommerceHttpClientOptions _options;
+
+    public ECommerceHttpClient(HttpClient client, IOptionsSnapshot<ECommerceHttpClientOptions> optionsSnapshot)
+        : base(client)
+    {
+        _options = optionsSnapshot.Value;
+    }
+
+    public Task<HttpResponse<Responses.Catalogs>> GetCatalogsAsync(int limit, int offset, CancellationToken cancellationToken)
+        => GetAsync<Responses.Catalogs>($"{_options.CatalogEndpoint}/get-catalogs?limit={limit}&offset={offset}", cancellationToken);
+
+    public Task<HttpResponse> CreateCatalogAsync(Requests.CreateCatalog request, CancellationToken cancellationToken)
+        => PostAsync($"{_options.CatalogEndpoint}/create-catalog", request, cancellationToken);
+
+    public Task<HttpResponse> DeleteCatalogAsync(Guid catalogId, CancellationToken cancellationToken)
+        => DeleteAsync($"{_options.CatalogEndpoint}/delete-catalog/{catalogId}", cancellationToken);
+
+    public Task<HttpResponse> EditCatalogAsync(Requests.CreateCatalog request, CancellationToken cancellationToken)
+        => PutAsync($"{_options.CatalogEndpoint}/update-catalog", request, cancellationToken);
+}
---
+++ src/Web/WebAPP/HttpClients/IECommerceHttpClient.cs
@@ -0,0 +1,12 @@
+﻿using ECommerce.Contracts.Catalogs;
+using WebAPP.Abstractions.Http;
+
+namespace WebAPP.HttpClients;
+
+public interface IECommerceHttpClient
+{
+    Task<HttpResponse<Responses.Catalogs>> GetCatalogsAsync(int limit, int offset, CancellationToken cancellationToken);
+    Task<HttpResponse> CreateCatalogAsync(Requests.CreateCatalog request, CancellationToken cancellationToken);
+    Task<HttpResponse> DeleteCatalogAsync(Guid catalogId, CancellationToken cancellationToken);
+    Task<HttpResponse> EditCatalogAsync(Requests.CreateCatalog request, CancellationToken cancellationToken);
+}
