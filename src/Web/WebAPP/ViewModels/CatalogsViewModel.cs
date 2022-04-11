77818482502b709b63c874447fee49f7369d967a
Antônio Falcão <antonio.falcao@xpi.com.br>
2022-04-11 19:27:12 -0300

    Adding CatalogViewModel

---
+++ src/Web/WebAPP/ViewModels/CatalogsViewModel.cs
@@ -0,0 +1,123 @@
+﻿using BlazorStrap;
+using ECommerce.Abstractions.Messages.Queries.Paging;
+using ECommerce.Contracts.Catalogs;
+using WebAPP.Abstractions;
+using WebAPP.HttpClients;
+
+namespace WebAPP.ViewModels;
+
+public class CatalogsViewModel : ViewModel
+{
+    private readonly IECommerceHttpClient _httpClient;
+    private readonly IBlazorStrap _blazorStrap;
+
+    public string Title;
+    public string Description;
+
+    public List<Projections.Catalog> Catalogs = new();
+    public PageInfo PageInfo = new();
+
+    public CatalogsViewModel(IECommerceHttpClient httpClient, IBlazorStrap blazorStrap)
+    {
+        _httpClient = httpClient;
+        _blazorStrap = blazorStrap;
+    }
+
+    public async Task FetchDataAsync(CancellationToken cancellationToken, int limit = 8, int offset = 0)
+    {
+        var response = await _httpClient.GetCatalogsAsync(limit, offset, CancellationToken.None);
+
+        if (response.Success)
+        {
+            Catalogs = response.ActionResult?.Items?.ToList() ?? Catalogs;
+            PageInfo = response.ActionResult?.PageInfo ?? PageInfo;
+        }
+        else Failed(response.Message);
+    }
+
+    public async Task CreateAsync()
+    {
+        try
+        {
+            Requests.CreateCatalog request = new(Guid.NewGuid(), Title, Description);
+            var response = await _httpClient.CreateCatalogAsync(request, CancellationToken.None);
+
+            if (response.Success)
+            {
+                Title = string.Empty;
+                Description = string.Empty;
+                
+                Catalogs.Add(new(request.CatalogId, request.Title, request.Description, default, default));
+                
+                Success();
+            }
+            else Failed(response.Message);
+        }
+        catch (Exception e)
+        {
+            Failed(e.Message);
+        }
+    }
+
+    public async Task DeleteAsync(Guid catalogId)
+    {
+        try
+        {
+            var response = await _httpClient.DeleteCatalogAsync(catalogId, CancellationToken.None);
+
+            if (response.Success)
+            {
+                Catalogs.RemoveAll(catalog => catalog.Id == catalogId);
+                Success();
+            }
+            else Failed(response.Message);
+        }
+        catch (Exception e)
+        {
+            Failed(e.Message);
+        }
+    }
+
+    public async Task EditAsync(Guid catalogId)
+    {
+        try
+        {
+            Requests.CreateCatalog request = new(catalogId, Title, Description);
+            var response = await _httpClient.EditCatalogAsync(request, CancellationToken.None);
+
+            if (response.Success)
+            {
+                var catalog = Catalogs.First(catalog => catalog.Id == catalogId);
+                var index = Catalogs.IndexOf(catalog);
+                Catalogs[index] = new Projections.Catalog(catalogId, Title, Description, catalog.IsActive, catalog.IsDeleted);
+
+                Success();
+            }
+            else Failed(response.Message);
+        }
+        catch (Exception e)
+        {
+            Failed(e.Message);
+        }
+    }
+
+    private void Success()
+    {
+        _blazorStrap.Toaster.Add("Success", "Catalog created", o =>
+        {
+            o.Color = BSColor.Success;
+            o.CloseAfter = 2000;
+            o.Toast = Toast.BottomRight;
+        });
+    }
+
+    private void Failed(string error = default)
+    {
+        _blazorStrap.Toaster.Add("Error", error ?? "It was not possible to create a new Catalog", o =>
+        {
+            o.Color = BSColor.Danger;
+            o.CloseAfter = 2000;
+            o.Toast = Toast.BottomRight;
+        });
+    }
+}
