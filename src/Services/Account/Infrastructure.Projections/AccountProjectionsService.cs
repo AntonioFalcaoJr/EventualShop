edeba312caa8287ec9f5b89c577f6851183e37e9
Antônio Falcão <antonio.falcao@xpi.com.br>
2022-04-12 15:57:25 -0300

    Fixing design

--- src/Services/Account/Infrastructure.Projections/AccountProjectionsService.cs
+++ src/Services/Account/Infrastructure.Projections/AccountProjectionsService.cs
@@ -16,27 +16,19 @@ public class AccountProjectionsService : IAccountProjectionsService
     }
 
     public Task<IPagedResult<AccountDetailsProjection>> GetAccountsAsync(int limit, int offset, CancellationToken cancellationToken)
-    {
-        return _repository.GetAllAsync<AccountDetailsProjection>(
+        => _repository.GetAllAsync<AccountDetailsProjection>(
             new Paging {Limit = limit, Offset = offset},
             projection => projection.IsDeleted == false,
             cancellationToken);
-    }
 
     public Task<AccountDetailsProjection> GetAccountDetailsAsync(Guid accountId, CancellationToken cancellationToken)
-    {
-        return _repository.GetAsync<AccountDetailsProjection, Guid>(accountId, cancellationToken);
-    }
+        => _repository.GetAsync<AccountDetailsProjection, Guid>(accountId, cancellationToken);
 
     public Task ProjectAsync<TProjection>(TProjection projection, CancellationToken cancellationToken)
         where TProjection : IProjection
-    {
-        return _repository.UpsertAsync(projection, cancellationToken);
-    }
+        => _repository.UpsertAsync(projection, cancellationToken);
 
     public Task RemoveAsync<TProjection>(Expression<Func<TProjection, bool>> filter, CancellationToken cancellationToken)
         where TProjection : IProjection
-    {
-        return _repository.DeleteAsync(filter, cancellationToken);
-    }
+        => _repository.DeleteAsync(filter, cancellationToken);
 }
