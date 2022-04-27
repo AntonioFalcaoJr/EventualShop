aa214ece3545667a0c96aa46269f9cd9b03d9cac
Antônio Falcão <antonio.falcao@xpi.com.br>
2022-04-27 16:18:12 -0300

    Ordering

--- src/Contracts/Contracts/Services/Catalog/Command.cs
+++ src/Contracts/Contracts/Services/Catalog/Command.cs
@@ -5,19 +5,19 @@ namespace Contracts.Services.Catalog;
 
 public static class Command
 {
-    public record CreateCatalog(Guid CatalogId, string Title, string Description) : Message(CorrelationId: CatalogId), ICommand;
+    public record AddCatalogItem(Guid CatalogId, Dto.Product Product, int Quantity) : Message(CorrelationId: CatalogId), ICommand;
 
-    public record DeleteCatalog(Guid CatalogId) : Message(CorrelationId: CatalogId), ICommand;
+    public record ActivateCatalog(Guid CatalogId) : Message(CorrelationId: CatalogId), ICommand;
+
+    public record CreateCatalog(Guid CatalogId, string Title, string Description) : Message(CorrelationId: CatalogId), ICommand;
 
     public record ChangeCatalogTitle(Guid CatalogId, string Title) : Message(CorrelationId: CatalogId), ICommand;
 
     public record ChangeCatalogDescription(Guid CatalogId, string Description) : Message(CorrelationId: CatalogId), ICommand;
 
-    public record ActivateCatalog(Guid CatalogId) : Message(CorrelationId: CatalogId), ICommand;
-
     public record DeactivateCatalog(Guid CatalogId) : Message(CorrelationId: CatalogId), ICommand;
 
-    public record DeleteCatalogItem(Guid CatalogId, Guid CatalogItemId) : Message(CorrelationId: CatalogId), ICommand;
+    public record DeleteCatalog(Guid CatalogId) : Message(CorrelationId: CatalogId), ICommand;
 
-    public record AddCatalogItem(Guid CatalogId, Dto.Product Product, int Quantity) : Message(CorrelationId: CatalogId), ICommand;
+    public record DeleteCatalogItem(Guid CatalogId, Guid CatalogItemId) : Message(CorrelationId: CatalogId), ICommand;
 }
