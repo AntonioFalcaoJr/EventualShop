09013c1f168277a807c6f4c4aab7e5de1bb49a58
Antônio Falcão Jr <antonio.falcao@xpi.com.br>
2022-04-30 18:39:13 -0300

    .

--- src/Services/ShoppingCart/Domain/ValueObjects/Addresses/Address.cs
+++ src/Services/ShoppingCart/Domain/ValueObjects/Addresses/Address.cs
@@ -6,7 +6,8 @@ using Domain.Abstractions.ValueObjects;
 
 namespace Domain.ValueObjects.Addresses;
 
-public record Address(string City, string Country, int? Number, string State, string Street, string ZipCode) : ValueObject<AddressValidator>
+public record Address(string City, string Country, int? Number, string State, string Street, string ZipCode) 
+    : ValueObject<AddressValidator>
 {
     public static implicit operator Address(Dto.Address address) 
         => new(address.City, address.Country, address.Number, address.State, address.Street, address.ZipCode);
