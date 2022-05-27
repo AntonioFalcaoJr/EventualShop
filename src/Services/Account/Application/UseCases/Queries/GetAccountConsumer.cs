903a783425d4bd701d3f8d956db95248738ba450
Antônio Falcão <antonio.falcao@xpi.com.br>
2022-05-27 14:45:08 -0300

    Splitting consumers

--- src/Services/Account/Application/UseCases/Queries/GetAccountConsumer.cs
+++ src/Services/Account/Application/UseCases/Queries/GetAccountConsumer.cs
@@ -5,9 +5,7 @@ using MassTransit;
 
 namespace Application.UseCases.Queries;
 
-public class GetAccountConsumer :
-    IConsumer<Query.GetAccount>,
-    IConsumer<Query.GetAccounts>
+public class GetAccountConsumer : IConsumer<Query.GetAccount>
 {
     private readonly IProjectionRepository<Projection.Account> _repository;
 
@@ -21,22 +19,7 @@ public class GetAccountConsumer :
         var account = await _repository.GetAsync(context.Message.AccountId, context.CancellationToken);
 
         await (account is null
-            ? context.RespondAsync<NotFound>(new())
+            ? context.RespondAsync<Reply.NotFound>(new())
             : context.RespondAsync(account));
-    }
-
-    public async Task Consume(ConsumeContext<Query.GetAccounts> context)
-    {
-        var accounts = await _repository.GetAsync(
-            limit: context.Message.Limit,
-            offset: context.Message.Offset,
-            cancellationToken: context.CancellationToken);
-
-        await context.RespondAsync(accounts switch
-        {
-            {PageInfo.Size: > 0} => accounts,
-            {PageInfo.Size: <= 0} => new NoContent(),
-            _ => new NotFound()
-        });
     }
 }
