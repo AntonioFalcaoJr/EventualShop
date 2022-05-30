fa40df07a2b231b0b9700f73ee220834d827b47d
Antônio Falcão <antonio.falcao@xpi.com.br>
2022-05-30 16:45:45 -0300

    Adding equality operators

--- src/Services/Account/Domain/ValueObjects/Addresses/BillingAddress.cs
+++ src/Services/Account/Domain/ValueObjects/Addresses/BillingAddress.cs
@@ -10,4 +10,10 @@ public record BillingAddress(string City, string Country, int? Number, string St
 
     public static implicit operator Dto.Address(BillingAddress address)
         => new(address.City, address.Country, address.Number, address.State, address.Street, address.ZipCode);
+
+    public static bool operator ==(BillingAddress address, Dto.Address dto)
+        => dto == (Dto.Address) address;
+
+    public static bool operator !=(BillingAddress address, Dto.Address dto)
+        => dto != (Dto.Address) address;
 }
