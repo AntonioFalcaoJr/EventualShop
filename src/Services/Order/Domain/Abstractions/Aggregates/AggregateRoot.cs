e8bf29edc382573f76f41d17ab311ee97d5217ed
Antônio Falcão <antonio.falcao@xpi.com.br>
2022-06-18 14:25:09 -0300

    Removing StoreEvent accountability from aggregate

--- src/Services/Order/Domain/Abstractions/Aggregates/AggregateRoot.cs
+++ src/Services/Order/Domain/Abstractions/Aggregates/AggregateRoot.cs
@@ -1,16 +1,13 @@
 ﻿using Domain.Abstractions.Entities;
 using Contracts.Abstractions.Messages;
-using Domain.Abstractions.StoreEvents;
 using FluentValidation;
 using Newtonsoft.Json;
 
 namespace Domain.Abstractions.Aggregates;
 
-public abstract class AggregateRoot<TId, TValidator, TStoreEvent>
-    : Entity<TId, TValidator>, IAggregateRoot<TId, TStoreEvent>
-    where TId : struct
+public abstract class AggregateRoot<TId, TValidator> : Entity<TId, TValidator>, IAggregateRoot<TId>
     where TValidator : IValidator, new()
-    where TStoreEvent : class, IStoreEvent<TId>, new()
+    where TId : struct
 {
     [JsonIgnore]
     private readonly List<IEvent> _events = new();
@@ -18,15 +15,6 @@ public abstract class AggregateRoot<TId, TValidator, TStoreEvent>
     [JsonIgnore]
     public IEnumerable<IEvent> Events
         => _events;
-
-    [JsonIgnore]
-    public IEnumerable<TStoreEvent> StoreEvents
-        => _events.Select(@event => new TStoreEvent
-        {
-            AggregateId = Id,
-            DomainEvent = @event,
-            DomainEventName = @event.GetType().Name
-        });
 
     public void LoadEvents(List<IEvent> events)
         => events.ForEach(ApplyEvent);
