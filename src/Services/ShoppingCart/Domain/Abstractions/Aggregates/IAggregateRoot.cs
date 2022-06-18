7be3158432ea7367fb199bd3885dfa1f633be835
Antônio Falcão <antonio.falcao@xpi.com.br>
2022-06-18 14:25:27 -0300

    Removing StoreEvent accountability from aggregate

--- src/Services/ShoppingCart/Domain/Abstractions/Aggregates/IAggregateRoot.cs
+++ src/Services/ShoppingCart/Domain/Abstractions/Aggregates/IAggregateRoot.cs
@@ -1,14 +1,11 @@
 using Domain.Abstractions.Entities;
 using Contracts.Abstractions.Messages;
-using Domain.Abstractions.StoreEvents;
 
 namespace Domain.Abstractions.Aggregates;
 
-public interface IAggregateRoot<out TId, out TStoreEvent> : IEntity<TId>
+public interface IAggregateRoot<out TId> : IEntity<TId>
     where TId : struct
-    where TStoreEvent : class, IStoreEvent<TId>
 {
     IEnumerable<IEvent> Events { get; }
-    IEnumerable<TStoreEvent> StoreEvents { get; }
     void LoadEvents(List<IEvent> events);
 }
