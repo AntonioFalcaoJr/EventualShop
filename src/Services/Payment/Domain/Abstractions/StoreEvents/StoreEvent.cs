d1a3556a5510d5359a8b2fdbccb4c208e6bbb507
Antônio Falcão <antonio.falcao@xpi.com.br>
2022-06-18 14:32:56 -0300

    Adding required property

--- src/Services/Payment/Domain/Abstractions/StoreEvents/StoreEvent.cs
+++ src/Services/Payment/Domain/Abstractions/StoreEvents/StoreEvent.cs
@@ -3,14 +3,13 @@ using Domain.Abstractions.Aggregates;
 
 namespace Domain.Abstractions.StoreEvents;
 
-public abstract record StoreEvent<TAggregate, TId> : IStoreEvent<TId>
-    where TAggregate : IAggregateRoot<TId, IStoreEvent<TId>>
+public abstract record StoreEvent<TId, TAggregate>
+    where TAggregate : IAggregateRoot<TId>
     where TId : struct
 {
     public long Version { get; }
-    public long Sequence { get; }
-    public TId AggregateId { get; init; }
+    public required TId AggregateId { get; init; }
     public string AggregateName { get; } = typeof(TAggregate).Name;
-    public string DomainEventName { get; init; }
-    public IEvent DomainEvent { get; init; }
+    public required string DomainEventName { get; init; }
+    public required IEvent DomainEvent { get; init; }
 }
