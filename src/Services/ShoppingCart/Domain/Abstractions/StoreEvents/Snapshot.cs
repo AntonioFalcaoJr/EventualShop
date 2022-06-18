69d5848cad613b80d356cbf1324c010283c2d7df
Antônio Falcão <antonio.falcao@xpi.com.br>
2022-06-18 14:33:06 -0300

    Adding required property

--- src/Services/ShoppingCart/Domain/Abstractions/StoreEvents/Snapshot.cs
+++ src/Services/ShoppingCart/Domain/Abstractions/StoreEvents/Snapshot.cs
@@ -2,12 +2,12 @@
 
 namespace Domain.Abstractions.StoreEvents;
 
-public abstract record Snapshot<TAggregate, TId> : ISnapshot<TAggregate, TId>
-    where TAggregate : IAggregateRoot<TId, IStoreEvent<TId>>, new()
+public abstract record Snapshot<TId, TAggregate> 
+    where TAggregate : IAggregateRoot<TId>, new()
     where TId : struct
 {
-    public long AggregateVersion { get; init; }
-    public TId AggregateId { get; init; }
+    public required long AggregateVersion { get; init; }
+    public required TId AggregateId { get; init; }
     public string AggregateName { get; } = typeof(TAggregate).Name;
-    public TAggregate AggregateState { get; init; } = new();
+    public required TAggregate AggregateState { get; init; } = new();
 }
