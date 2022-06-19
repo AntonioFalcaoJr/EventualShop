65c6080b297087b8a90b6f25117af3937d51e441
Antônio Falcão <antonio.falcao@xpi.com.br>
2022-06-19 19:38:54 -0300

    Adding Payment methods projection

--- src/Contracts/Services/Payment/Projection.cs
+++ src/Contracts/Services/Payment/Projection.cs
@@ -6,4 +6,6 @@ namespace Contracts.Services.Payment;
 public static class Projection
 {
     public record Payment(Guid Id, Guid OrderId, decimal Amount, Dto.Address BillingAddress, IEnumerable<Dto.PaymentMethod> PaymentMethods, string Status, bool IsDeleted) : IProjection;
+
+    public record PaymentMethod(Guid Id, Guid PaymentId, decimal Amount, Dto.IPaymentOption Option, string Status, bool IsDeleted) : IProjection;
 }
