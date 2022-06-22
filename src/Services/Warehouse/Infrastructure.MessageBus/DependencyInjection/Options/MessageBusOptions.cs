0c7ea98b1fc905ff8a4564722e31aad793369c40
Antônio Falcão <antonio.falcao@xpi.com.br>
2022-06-22 17:52:35 -0300

    Adding ConnectionString to MessageBusOptions

--- src/Services/Warehouse/Infrastructure.MessageBus/DependencyInjection/Options/MessageBusOptions.cs
+++ src/Services/Warehouse/Infrastructure.MessageBus/DependencyInjection/Options/MessageBusOptions.cs
@@ -4,7 +4,8 @@ namespace Infrastructure.MessageBus.DependencyInjection.Options;
 
 public record MessageBusOptions
 {
-    public List<string> Cluster { get; init; }
+    [Required, MinLength(10)]
+    public string ConnectionString { get; init; }
 
     [Required, MinLength(5)]
     public string SchedulerQueueName { get; init; }
