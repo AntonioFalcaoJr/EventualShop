66ee70a5f5a0fd51cd404d8e5275634d39961744
Antônio Falcão <antonio.falcao@xpi.com.br>
2022-06-22 17:49:59 -0300

    Moving connection settings from options to connection string

--- src/Services/Account/Infrastructure.MessageBus/DependencyInjection/Extensions/ServiceCollectionExtensions.cs
+++ src/Services/Account/Infrastructure.MessageBus/DependencyInjection/Extensions/ServiceCollectionExtensions.cs
@@ -28,13 +28,7 @@ public static class ServiceCollectionExtensions
                 {
                     var options = context.GetRequiredService<IOptionsMonitor<MessageBusOptions>>().CurrentValue;
 
-                    // TODO - What is the best approach to deal with cluster (settings.json)?
-                    // bus.Host(
-                    //     configure: host =>
-                    //     {
-                    //         options.Cluster?.ForEach(node
-                    //             => host.UseCluster(cluster => cluster.Node(node)));
-                    //     });
+                    bus.Host(options.ConnectionString);
 
                     cfg.AddMessageScheduler(new Uri($"queue:{options.SchedulerQueueName}"));
 
@@ -102,13 +96,6 @@ public static class ServiceCollectionExtensions
     public static OptionsBuilder<MassTransitHostOptions> ConfigureMassTransitHostOptions(this IServiceCollection services, IConfigurationSection section)
         => services
             .AddOptions<MassTransitHostOptions>()
-            .Bind(section)
-            .ValidateDataAnnotations()
-            .ValidateOnStart();
-
-    public static OptionsBuilder<RabbitMqTransportOptions> ConfigureRabbitMqTransportOptions(this IServiceCollection services, IConfigurationSection section)
-        => services
-            .AddOptions<RabbitMqTransportOptions>()
             .Bind(section)
             .ValidateDataAnnotations()
             .ValidateOnStart();
