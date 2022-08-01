e3056f466fc7325031d7309ea031fe483a522c30
Antônio Falcão <antonio.falcao@xpi.com.br>
2022-08-01 17:47:19 -0300

    Splitting Command and Query stacks

--- src/Services/Identity/Infrastructure.EventStore/DependencyInjection/Options/EventStoreOptions.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace Infrastructure.EventStore.DependencyInjection.Options;
-
-public class EventStoreOptions
-{
-    [Required, Range(3, 100)] 
-    public int SnapshotInterval { get; init; }
-}
