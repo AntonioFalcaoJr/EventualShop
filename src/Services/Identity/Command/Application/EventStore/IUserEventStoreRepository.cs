16e55dcb6903e1bbf31aadd416ae030d9fcaeb61
Antônio Falcão <antonio.falcao@xpi.com.br>
2022-08-15 19:08:21 -0300

    Removing interface

--- src/Services/Identity/Command/Application/EventStore/IUserEventStoreRepository.cs
+++
@@ -1,7 +0,0 @@
-using Application.Abstractions.EventStore;
-using Domain.Aggregates;
-using Domain.StoreEvents;
-
-namespace Application.EventStore;
-
-public interface IUserEventStoreRepository : IEventStoreRepository<User, UserStoreEvent, UserSnapshot, Guid> { }
