aa07b0ec68af3fa21faf7fadb98e28bffa1a9629
Antônio Falcão <antonio.falcao@xpi.com.br>
2022-08-15 19:11:05 -0300

    Adjusting unit

--- src/Services/Identity/Command/Infrastructure.EventStore/UnitsOfWork/UnitOfWork.cs
+++ src/Services/Identity/Command/Infrastructure.EventStore/UnitsOfWork/UnitOfWork.cs
@@ -1,32 +1,23 @@
-﻿using Infrastructure.EventStore.Contexts;
+﻿using Application.Abstractions;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 
 namespace Infrastructure.EventStore.UnitsOfWork;
 
 public class UnitOfWork : IUnitOfWork
 {
-    private readonly EventStoreDbContext _dbContext;
+    private readonly DatabaseFacade _database;
 
-    public UnitOfWork(EventStoreDbContext dbContext)
-        => _dbContext = dbContext;
+    public UnitOfWork(DbContext dbContext)
+        => _database = dbContext.Database;
 
     public Task ExecuteAsync(Func<CancellationToken, Task> operationAsync, CancellationToken cancellationToken)
-        => CreateExecutionStrategy().ExecuteAsync(ct => ExecuteTransactionAsync(operationAsync, ct), cancellationToken);
+        => _database.CreateExecutionStrategy().ExecuteAsync(ct => ExecuteTransactionAsync(operationAsync, ct), cancellationToken);
 
-    public Task CommitAsync(CancellationToken ct)
-        => _dbContext.SaveChangesAsync(ct);
-
-    private async Task ExecuteTransactionAsync(Func<CancellationToken, Task> operationAsync, CancellationToken ct)
+    private async Task ExecuteTransactionAsync(Func<CancellationToken, Task> operationAsync, CancellationToken cancellationToken)
     {
-        await using var transaction = await BeginTransactionAsync(ct);
-        await operationAsync(ct);
-        await transaction.CommitAsync(ct);
+        await using var transaction = await _database.BeginTransactionAsync(cancellationToken);
+        await operationAsync(cancellationToken);
+        await transaction.CommitAsync(cancellationToken);
     }
-
-    private Task<IDbContextTransaction> BeginTransactionAsync(CancellationToken ct)
-        => _dbContext.Database.BeginTransactionAsync(ct);
-
-    private IExecutionStrategy CreateExecutionStrategy()
-        => _dbContext.Database.CreateExecutionStrategy();
 }
