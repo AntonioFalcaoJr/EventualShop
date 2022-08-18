9721dd3968f5e6b2be96f2eb22ca6ce890dbe2ae
Antônio Falcão <antonio.falcao@xpi.com.br>
2022-08-18 20:46:17 -0300

    Adding todo

--- src/Contracts/Abstractions/Messages/ICommand.cs
+++ src/Contracts/Abstractions/Messages/ICommand.cs
@@ -5,6 +5,7 @@ namespace Contracts.Abstractions.Messages;
 [ExcludeFromTopology]
 public interface ICommand : IMessage { }
 
+// TODO - Remove it after migration
 public interface ICommandWithId : ICommand
 {
     Guid Id { get; }
