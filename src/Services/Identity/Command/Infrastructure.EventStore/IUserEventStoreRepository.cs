fcc7db7ece1a0bd854f7d05fdd0596ea705f4062
Antônio Falcão <antonio.falcao@xpi.com.br>
2022-08-18 20:40:15 -0300

    Removing old approach

--- src/Services/Identity/Command/Infrastructure.EventStore/IUserEventStoreRepository.cs
+++
@@ -1,7 +0,0 @@
-using Domain.Abstractions.EventStore;
-using Domain.Aggregates;
-using Domain.StoreEvents;
-
-namespace Infrastructure.EventStore;
-
-public interface IUserEventStoreRepository : IEventStoreRepository<User, UserStoreEvent, UserSnapshot, Guid> { }
