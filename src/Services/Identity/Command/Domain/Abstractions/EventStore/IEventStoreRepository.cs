14abff03362d4a0222710919b9cadf503841c25f
Antônio Falcão <antonio.falcao@xpi.com.br>
2022-08-18 20:41:17 -0300

    removing generics

--- src/Services/Identity/Command/Domain/Abstractions/EventStore/IEventStoreRepository.cs
+++ src/Services/Identity/Command/Domain/Abstractions/EventStore/IEventStoreRepository.cs
@@ -7,6 +7,6 @@ public interface IEventStoreRepository
     Task AppendEventsAsync(IEnumerable<StoreEvent> events, Func<long, CancellationToken, Task> onEventStored, CancellationToken cancellationToken);
     Task AppendEventAsync(StoreEvent storeEvent, CancellationToken cancellationToken);
     Task AppendSnapshotAsync(Snapshot snapshot, CancellationToken cancellationToken);
-    Task<List<IEvent>> GetStreamAsync<TId>(TId aggregateId, long version, CancellationToken cancellationToken) where TId : struct;
-    Task<Snapshot> GetSnapshotAsync<TId>(TId aggregateId, CancellationToken cancellationToken) where TId : struct;
+    Task<List<IEvent>> GetStreamAsync(Guid aggregateId, long version, CancellationToken cancellationToken);
+    Task<Snapshot> GetSnapshotAsync(Guid aggregateId, CancellationToken cancellationToken);
 }
