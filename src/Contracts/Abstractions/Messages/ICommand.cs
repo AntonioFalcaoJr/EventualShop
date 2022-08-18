15af9d6422337d521b520e0a967b980f78427480
Antônio Falcão <antonio.falcao@xpi.com.br>
2022-08-18 12:12:37 -0300

    Adding command with Id just for tests

--- src/Contracts/Abstractions/Messages/ICommand.cs
+++ src/Contracts/Abstractions/Messages/ICommand.cs
@@ -4,3 +4,8 @@ namespace Contracts.Abstractions.Messages;
 
 [ExcludeFromTopology]
 public interface ICommand : IMessage { }
+
+public interface ICommandWithId : IMessage
+{
+    Guid Id { get; }
+}
--- src/Contracts/Services/Identity/Command.cs
+++ src/Contracts/Services/Identity/Command.cs
@@ -4,9 +4,9 @@ namespace Contracts.Services.Identity;
 
 public static class Command
 {
-    public record RegisterUser(string FirstName, string LastName, string Email, string Password, string PasswordConfirmation) : Message, ICommand;
+    public record RegisterUser(Guid Id, string FirstName, string LastName, string Email, string Password, string PasswordConfirmation) : Message(CorrelationId: Id), ICommandWithId;
 
-    public record ChangePassword(Guid UserId, string NewPassword, string NewPasswordConfirmation) : Message, ICommand;
+    public record ChangePassword(Guid Id, string NewPassword, string NewPasswordConfirmation) : Message(CorrelationId: Id), ICommandWithId;
 
-    public record DeleteUser(Guid UserId) : Message, ICommand;
+    public record DeleteUser(Guid Id) : Message(CorrelationId: Id), ICommandWithId;
 }
