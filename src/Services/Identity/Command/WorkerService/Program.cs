704b3676759389c7fba12d86886a6be32747719a
Antônio Falcão <antonio.falcao@xpi.com.br>
2022-08-20 13:52:48 -0300

    Configuring event Interactors

--- src/Services/Identity/Command/WorkerService/Program.cs
+++ src/Services/Identity/Command/WorkerService/Program.cs
@@ -42,6 +42,7 @@ builder.ConfigureServices((context, services) =>
     services.AddCommandBus();
     services.AddEventBusGateway();
     services.AddCommandInteractors();
+    services.AddEventInteractors();
     services.AddMessageValidators();
 
     services.ConfigureEventStoreOptions(
