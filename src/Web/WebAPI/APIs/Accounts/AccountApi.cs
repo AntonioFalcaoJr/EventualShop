2c7a925bc15e3e2db2f1dbab94c5f59622bc6f1d
Antônio Falcão <antonio.falcao@xpi.com.br>
2022-08-29 16:59:27 -0300

    Removing Command and Query wrapper and applying new command pattern

--- src/Web/WebAPI/APIs/Accounts/AccountApi.cs
+++ src/Web/WebAPI/APIs/Accounts/AccountApi.cs
@@ -7,27 +7,27 @@ public static class AccountApi
 {
     public static void MapAccountApi(this RouteGroupBuilder group)
     {
-        group.MapQuery("/", ([AsParameters] Requests.ListAccounts request)
+        group.MapGet("/", ([AsParameters] Requests.ListAccounts request)
             => ApplicationApi.GetPagedProjectionAsync<Query.ListAccounts, Projection.AccountDetails>(request.Bus, request, request.CancellationToken));
 
-        group.MapCommand(builder => builder.MapPost("/", ([AsParameters] Requests.CreateAccount request)
-            => ApplicationApi.SendCommandAsync<Command.CreateAccount>(request.Bus, request, request.CancellationToken)));
+        group.MapPost("/", ([AsParameters] Requests.CreateAccount request)
+            => ApplicationApi.SendCommandAsync<Command.CreateAccount>(request));
 
-        group.MapQuery("/{AccountId:guid}", ([AsParameters] Requests.GetAccount request)
+        group.MapGet("/{AccountId:guid}", ([AsParameters] Requests.GetAccount request)
             => ApplicationApi.GetProjectionAsync<Query.GetAccount, Projection.AccountDetails>(request.Bus, request, request.CancellationToken));
 
-        group.MapCommand(builder => builder.MapDelete("/{accountId:guid}", ([AsParameters] Requests.DeleteAccount request)
-            => ApplicationApi.SendCommandAsync<Command.DeleteAccount>(request.Bus, request, request.CancellationToken)));
+        group.MapDelete("/{AccountId:guid}", ([AsParameters] Requests.DeleteAccount request)
+            => ApplicationApi.SendCommandAsync<Command.DeleteAccount>(request));
 
-        group.MapQuery("/{AccountId:guid}/profiles/address", ([AsParameters] Requests.ListAddresses request) 
+        group.MapGet("/{AccountId:guid}/profiles/address", ([AsParameters] Requests.ListAddresses request)
             => ApplicationApi.GetPagedProjectionAsync<Query.ListAddresses, Projection.AddressListItem>(request.Bus, request, request.CancellationToken));
 
-        group.MapCommand(builder => builder.MapPut("/{AccountId:guid}/profiles/billing-address", ([AsParameters] Requests.AddBillingAddress request)
-            => ApplicationApi.SendCommandAsync<Command.AddBillingAddress>(request.Bus, request, request.CancellationToken)));
+        group.MapPut("/{AccountId:guid}/profiles/billing-address", ([AsParameters] Requests.AddBillingAddress request)
+            => ApplicationApi.SendCommandAsync<Command.AddBillingAddress>(request));
 
-        group.MapCommand(builder => builder.MapPut("/{AccountId:guid}/profiles/shipping-address", ([AsParameters] Requests.AddShippingAddress request) 
-            => ApplicationApi.SendCommandAsync<Command.AddShippingAddress>(request.Bus, request, request.CancellationToken)));
-        
-        group.WithMetadata(new TagsAttribute("Accounts"));
+        group.MapPut("/{AccountId:guid}/profiles/shipping-address", ([AsParameters] Requests.AddShippingAddress request)
+            => ApplicationApi.SendCommandAsync<Command.AddShippingAddress>(request));
+
+       group.WithMetadata(new TagsAttribute("Accounts"));
     }
 }
