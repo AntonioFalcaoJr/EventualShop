0cde065aa890cad4b48bb43c738bdd598d11259e
Antônio Falcão <antonio.falcao@xpi.com.br>
2022-08-30 12:21:05 -0300

    Improving client delegate

--- src/Web/WebAPI/APIs/Identities/IdentityApi.cs
+++ src/Web/WebAPI/APIs/Identities/IdentityApi.cs
@@ -7,7 +7,7 @@ public static class IdentityApi
     public static void MapIdentityApi(this RouteGroupBuilder group)
     {
         group.MapGet("/login", ([AsParameters] Requests.Login request)
-            => ApplicationApi.QueryAsync(request, client => client.LoginAsync(request)));
+            => ApplicationApi.QueryAsync(request, (client, ct) => client.LoginAsync(request, cancellationToken: ct)));
 
         group.WithMetadata(new TagsAttribute("IdentitiesV2"));
     }
