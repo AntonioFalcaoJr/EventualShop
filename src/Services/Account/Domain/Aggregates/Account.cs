c1dac0ccaecf7b0c6de2b55656ba29d4d7dd808a
Antônio Falcão <antonio.falcao@xpi.com.br>
2022-08-29 21:04:58 -0300

    Adjusting

--- src/Services/Account/Domain/Aggregates/Account.cs
+++ src/Services/Account/Domain/Aggregates/Account.cs
@@ -17,33 +17,26 @@ public class Account : AggregateRoot<Guid, AccountValidator>
     public bool WishToReceiveNews { get; private set; }
     public bool AcceptedPolicies { get; private set; }
     public string Password { get; private set; }
+    public string PasswordConfirmation { get; private set; }
 
     public IEnumerable<Address> Addresses
         => _addresses;
 
     public void Handle(Command.CreateAccount cmd)
-        => RaiseEvent(new DomainEvent.AccountCreated(
-            Guid.NewGuid(),
-            cmd.Profile,
-            cmd.Password,
-            cmd.PasswordConfirmation,
-            cmd.AcceptedPolicies,
-            cmd.WishToReceiveNews));
+        => RaiseEvent(new DomainEvent.AccountCreated(cmd.AccountId, cmd.Password, cmd.PasswordConfirmation, cmd.AcceptedPolicies, cmd.WishToReceiveNews));
 
     public void Handle(Command.DeleteAccount cmd)
         => RaiseEvent(new DomainEvent.AccountDeleted(cmd.AccountId));
 
     public void Handle(Command.AddBillingAddress cmd)
     {
-        if (_addresses.OfType<BillingAddress>().All(address
-                => address != cmd.Address && address.IsDeleted is false))
+        if (_addresses.OfType<BillingAddress>().Where(address => address.IsDeleted is false).All(address => address != cmd.Address))
             RaiseEvent(new DomainEvent.BillingAddressAdded(cmd.AccountId, Guid.NewGuid(), cmd.Address));
     }
 
     public void Handle(Command.AddShippingAddress cmd)
     {
-        if (_addresses.OfType<ShippingAddress>().All(address
-                => address != cmd.Address && address.IsDeleted is false))
+        if (_addresses.OfType<ShippingAddress>().Where(address => address.IsDeleted is false).All(address => address != cmd.Address))
             RaiseEvent(new DomainEvent.ShippingAddressAdded(cmd.AccountId, Guid.NewGuid(), cmd.Address));
     }
 
@@ -61,11 +54,11 @@ public class Account : AggregateRoot<Guid, AccountValidator>
             RaiseEvent(new DomainEvent.ShippingAddressPreferred(cmd.AccountId, cmd.AddressId));
     }
 
-    protected override void ApplyEvent(IEvent domainEvent) 
+    protected override void ApplyEvent(IEvent domainEvent)
         => When(domainEvent as dynamic);
 
     private void When(DomainEvent.AccountCreated @event)
-        => (Id, Profile, Password, _, AcceptedPolicies, WishToReceiveNews) = @event;
+        => (Id, Password, PasswordConfirmation, AcceptedPolicies, WishToReceiveNews) = @event;
 
     private void When(DomainEvent.BillingAddressAdded @event)
         => _addresses.Add(BillingAddress.Create(@event.AddressId, @event.Address));
