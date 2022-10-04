e4b43d4b996e4607d04e42f6f474f8660ca624a8
Antônio Falcão <antonio.falcao@xpi.com.br>
2022-10-04 13:21:29 -0300

    forcing

--- src/Services/Account/Application/UseCases/Queries/GetAccountConsumer.cs
+++ src/Services/Account/Application/UseCases/Queries/GetAccountConsumer.cs
@@ -16,6 +16,7 @@ public class GetAccountConsumer : IConsumer<Query.GetAccount>
     {
         var account = await _repository.GetAsync(context.Message.AccountId, context.CancellationToken);
 
+        
         await (account is null
             ? context.RespondAsync<Reply.NotFound>(new())
             : context.RespondAsync(account));
