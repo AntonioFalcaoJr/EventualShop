ddc84f489b1f44af4a790e288c4e685d32c51e92
Antônio Falcão <antonio.falcao@xpi.com.br>
2022-10-07 13:27:05 -0300

    Moving options to immutable records

---
+++ src/Services/Communication/Command/Infrastructure.SMTP/DependencyInjection/Options/SmtpOptions.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Infrastructure.SMTP.DependencyInjection.Options;
+
+public record SmtpOptions
+{
+    [Required, MinLength(5)] public string Host { get; init; }
+    [Required] public int Port { get; init; }
+    [Required, EmailAddress] public string Username { get; init; }
+    [Required] public string Password { get; init; }
+    [Required] public bool EnableSsl { get; init; }
+}
