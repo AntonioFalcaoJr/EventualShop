8b2b142572c3b2c8fb80b75bc396322f645e4ad0
Antônio Falcão <antonio.falcao@xpi.com.br>
2022-10-07 13:38:20 -0300

    Fixing

--- src/Services/Communication/Command/Infrastructure.MessageBus/EventBusGateway.cs
+++ src/Services/Communication/Command/Infrastructure.MessageBus/EventBusGateway.cs
@@ -7,21 +7,17 @@ namespace Infrastructure.MessageBus;
 public class EventBusGateway : IEventBusGateway
 {
     private readonly IPublishEndpoint _publishEndpoint;
-    // private readonly IMessageScheduler _messageScheduler;
+    private readonly IMessageScheduler _messageScheduler;
 
-    public EventBusGateway(IPublishEndpoint publishEndpoint 
-        // IMessageScheduler messageScheduler
-        )
+    public EventBusGateway(IPublishEndpoint publishEndpoint, IMessageScheduler messageScheduler)
     {
         _publishEndpoint = publishEndpoint;
-        // _messageScheduler = messageScheduler;
+        _messageScheduler = messageScheduler;
     }
 
     public Task PublishAsync(IEnumerable<IEvent> events, CancellationToken cancellationToken)
         => Task.WhenAll(events.Select(@event => _publishEndpoint.Publish(@event, @event.GetType(), cancellationToken)));
 
     public Task SchedulePublishAsync(DateTimeOffset scheduledTime, IEvent @event, CancellationToken cancellationToken)
-        => Task.CompletedTask
-            // _messageScheduler.SchedulePublish(scheduledTime.UtcDateTime, @event, @event.GetType(), cancellationToken)
-            ;
+        => _messageScheduler.SchedulePublish(scheduledTime.UtcDateTime, @event, @event.GetType(), cancellationToken);
 }
