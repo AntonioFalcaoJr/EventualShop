3cda38c020d957fad6a6ca9ba4702827a9cfa6bc
Antônio Falcão <antonio.falcao@xpi.com.br>
2022-10-12 22:03:49 -0300

    removing unnecessary resource

--- src/Services/Communication/Command/Infrastructure.MessageBus/EventBusGateway.cs
+++ src/Services/Communication/Command/Infrastructure.MessageBus/EventBusGateway.cs
@@ -6,18 +6,11 @@ namespace Infrastructure.MessageBus;
 
 public class EventBusGateway : IEventBusGateway
 {
-    private readonly IPublishEndpoint _publishEndpoint;
-    private readonly IMessageScheduler _messageScheduler;
+    private readonly IBus _bus;
 
-    public EventBusGateway(IPublishEndpoint publishEndpoint, IMessageScheduler messageScheduler)
-    {
-        _publishEndpoint = publishEndpoint;
-        _messageScheduler = messageScheduler;
-    }
+    public EventBusGateway(IBus bus)
+        => _bus = bus;
 
     public Task PublishAsync(IEnumerable<IEvent> events, CancellationToken cancellationToken)
-        => Task.WhenAll(events.Select(@event => _publishEndpoint.Publish(@event, @event.GetType(), cancellationToken)));
-
-    public Task SchedulePublishAsync(DateTimeOffset scheduledTime, IEvent @event, CancellationToken cancellationToken)
-        => _messageScheduler.SchedulePublish(scheduledTime.UtcDateTime, @event, @event.GetType(), cancellationToken);
+        => Task.WhenAll(events.Select(@event => _bus.Publish(@event, @event.GetType(), cancellationToken)));
 }
