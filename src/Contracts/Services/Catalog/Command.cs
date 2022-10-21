3d12a4476a5b2998cc0e23241945faee7f0105d3
Kauan Schumacher <kauan.schumacher@xpi.com.br>
2022-10-21 10:45:54 -0300

    Catalog is done!

--- src/Contracts/Services/Catalog/Command.cs
+++ src/Contracts/Services/Catalog/Command.cs
@@ -5,19 +5,19 @@ namespace Contracts.Services.Catalog;
 
 public static class Command
 {
-    public record AddCatalogItem(Guid Id, Guid InventoryId, Dto.Product Product, decimal UnitPrice, string Sku, int Quantity) : Message, ICommand;
+    public record AddCatalogItem(Guid CatalogId, Guid InventoryId, Dto.Product Product, decimal UnitPrice, string Sku, int Quantity) : Message, ICommand;
 
-    public record ActivateCatalog(Guid Id) : Message, ICommand;
+    public record ActivateCatalog(Guid CatalogId) : Message, ICommand;
 
-    public record CreateCatalog(Guid Id, string Title, string Description) : Message, ICommand;
+    public record CreateCatalog(Guid CatalogId, string Title, string Description) : Message, ICommand;
 
-    public record ChangeCatalogTitle(Guid Id, string Title) : Message, ICommand;
+    public record ChangeCatalogTitle(Guid CatalogId, string Title) : Message, ICommand;
 
-    public record ChangeCatalogDescription(Guid Id, string Description) : Message, ICommand;
+    public record ChangeCatalogDescription(Guid CatalogId, string Description) : Message, ICommand;
 
-    public record DeactivateCatalog(Guid Id) : Message, ICommand;
+    public record DeactivateCatalog(Guid CatalogId) : Message, ICommand;
 
-    public record DeleteCatalog(Guid Id) : Message, ICommand;
+    public record DeleteCatalog(Guid CatalogId) : Message, ICommand;
 
-    public record RemoveCatalogItem(Guid Id, Guid ItemId) : Message, ICommand;
+    public record RemoveCatalogItem(Guid CatalogId, Guid ItemId) : Message, ICommand;
 }
