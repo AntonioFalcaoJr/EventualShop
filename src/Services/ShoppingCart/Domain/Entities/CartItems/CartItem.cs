e79cc6265c355a273c29f07bcdbf4a8498236654
Antônio Falcão <antonio.falcao@xpi.com.br>
2022-12-05 21:07:37 -0300

    Adjust

--- src/Services/ShoppingCart/Domain/Entities/CartItems/CartItem.cs
+++ src/Services/ShoppingCart/Domain/Entities/CartItems/CartItem.cs
@@ -6,16 +6,14 @@ namespace Domain.Entities.CartItems;
 
 public class CartItem : Entity<Guid, CartItemValidator>
 {
-    public CartItem(Guid id, Guid catalogId, Product product, ushort quantity, decimal unitPrice)
+    public CartItem(Guid id, Product product, ushort quantity, decimal unitPrice)
     {
         Id = id;
-        CatalogId = catalogId;
         Product = product;
         Quantity = quantity;
         UnitPrice = unitPrice;
     }
 
-    public Guid CatalogId { get; }
     public Product Product { get; }
     public ushort Quantity { get; private set; }
     public decimal UnitPrice { get; }
@@ -27,8 +25,8 @@ public class CartItem : Entity<Guid, CartItemValidator>
         => IsDeleted = true;
 
     public static implicit operator CartItem(Dto.CartItem item)
-        => new(item.Id, item.CatalogId, item.Product, item.Quantity, item.UnitPrice);
+        => new(item.Id, item.Product, item.Quantity, item.UnitPrice);
 
     public static implicit operator Dto.CartItem(CartItem item)
-        => new(item.Id, item.CatalogId, item.Product, item.Quantity, item.UnitPrice);
+        => new(item.Id, item.Product, item.Quantity, item.UnitPrice);
 }
