d5c3962b8d163f72676cc67a49946c5a2b78f044
Antônio Falcão <antonio.falcao@xpi.com.br>
2022-12-07 19:47:52 -0300

    Adjusting contracts

--- src/Contracts/Services/Communication/DomainEvent.cs
+++ src/Contracts/Services/Communication/DomainEvent.cs
@@ -1,8 +1,9 @@
 ﻿using Contracts.Abstractions.Messages;
+using Contracts.DataTransferObjects;
 
 namespace Contracts.Services.Communication;
 
 public static class DomainEvent
 {
-    public record EmailConfirmationRequested(Guid Id, Guid UserId, Guid MethodId, string Email) : Message, IEvent;
+    public record NotificationRequested(Guid NotificationId, IEnumerable<Dto.NotificationMethod> Methods) : Message, IEvent;
 }
