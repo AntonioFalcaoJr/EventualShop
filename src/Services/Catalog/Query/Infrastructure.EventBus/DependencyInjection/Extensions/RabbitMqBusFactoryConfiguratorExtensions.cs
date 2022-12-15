d32de3cdf30fe99920bc8a77e0983eb957d397aa
Antônio Falcão <antonio.falcao@xpi.com.br>
2022-12-15 19:13:01 -0300

    Adding consumers, and adjusting topology

--- src/Services/Catalog/Query/Infrastructure.EventBus/DependencyInjection/Extensions/RabbitMqBusFactoryConfiguratorExtensions.cs
+++ src/Services/Catalog/Query/Infrastructure.EventBus/DependencyInjection/Extensions/RabbitMqBusFactoryConfiguratorExtensions.cs
@@ -9,22 +9,24 @@ internal static class RabbitMqBusFactoryConfiguratorExtensions
 {
     public static void ConfigureEventReceiveEndpoints(this IRabbitMqBusFactoryConfigurator cfg, IRegistrationContext context)
     {
-        cfg.ConfigureEventReceiveEndpoint<ProjectDetailsWhenCatalogCreatedConsumer, DomainEvent.CatalogCreated>(context);
-        cfg.ConfigureEventReceiveEndpoint<ProjectDetailsWhenCatalogDeletedConsumer, DomainEvent.CatalogDeleted>(context);
-        cfg.ConfigureEventReceiveEndpoint<ProjectListItemWhenCatalogDeletedConsumer, DomainEvent.CatalogDeleted>(context);
-        cfg.ConfigureEventReceiveEndpoint<ProjectDetailsWhenCatalogDeactivatedConsumer, DomainEvent.CatalogDeactivated>(context);
-        cfg.ConfigureEventReceiveEndpoint<ProjectDetailsWhenCatalogActivatedConsumer, DomainEvent.CatalogActivated>(context);
-        cfg.ConfigureEventReceiveEndpoint<ProjectDetailsWhenCatalogDescriptionChangedConsumer, DomainEvent.CatalogDescriptionChanged>(context);
-        cfg.ConfigureEventReceiveEndpoint<ProjectDetailsWhenCatalogTitleChangedConsumer, DomainEvent.CatalogTitleChanged>(context);
-        cfg.ConfigureEventReceiveEndpoint<ProjectListItemWhenCatalogItemAddedConsumer, DomainEvent.CatalogItemAdded>(context);
-        cfg.ConfigureEventReceiveEndpoint<ProjectListItemWhenCatalogItemRemovedConsumer, DomainEvent.CatalogItemRemoved>(context);
+        cfg.ConfigureEventReceiveEndpoint<ProjectCatalogGridItemWhenCatalogCreatedConsumer, DomainEvent.CatalogCreated>(context);
+        cfg.ConfigureEventReceiveEndpoint<ProjectCatalogGridItemWhenCatalogDeletedConsumer, DomainEvent.CatalogDeleted>(context);
+        cfg.ConfigureEventReceiveEndpoint<ProjectCatalogItemListItemWhenCatalogDeletedConsumer, DomainEvent.CatalogDeleted>(context);
+        cfg.ConfigureEventReceiveEndpoint<ProjectCatalogGridItemWhenCatalogDeactivatedConsumer, DomainEvent.CatalogDeactivated>(context);
+        cfg.ConfigureEventReceiveEndpoint<ProjectCatalogGridItemWhenCatalogActivatedConsumer, DomainEvent.CatalogActivated>(context);
+        cfg.ConfigureEventReceiveEndpoint<ProjectCatalogGridItemWhenCatalogDescriptionChangedConsumer, DomainEvent.CatalogDescriptionChanged>(context);
+        cfg.ConfigureEventReceiveEndpoint<ProjectCatalogGridItemWhenCatalogTitleChangedConsumer, DomainEvent.CatalogTitleChanged>(context);
+        cfg.ConfigureEventReceiveEndpoint<ProjectCatalogItemListItemWhenCatalogItemAddedConsumer, DomainEvent.CatalogItemAdded>(context);
+        cfg.ConfigureEventReceiveEndpoint<ProjectCatalogItemCardWhenCatalogItemAddedConsumer, DomainEvent.CatalogItemAdded>(context);
+        cfg.ConfigureEventReceiveEndpoint<ProjectCatalogItemDetailsWhenCatalogItemAddedConsumer, DomainEvent.CatalogItemAdded>(context);
+        cfg.ConfigureEventReceiveEndpoint<ProjectCatalogItemListItemWhenCatalogItemRemovedConsumer, DomainEvent.CatalogItemRemoved>(context);
     }
 
     private static void ConfigureEventReceiveEndpoint<TConsumer, TEvent>(this IRabbitMqBusFactoryConfigurator bus, IRegistrationContext context)
         where TConsumer : class, IConsumer
         where TEvent : class, IEvent
         => bus.ReceiveEndpoint(
-            queueName: $"catalog.query.{typeof(TConsumer).ToKebabCaseString()}.{typeof(TEvent).ToKebabCaseString()}",
+            queueName: $"catalog.query.{typeof(TConsumer).ToKebabCaseString()}",
             configureEndpoint: endpoint =>
             {
                 endpoint.ConfigureConsumeTopology = false;
