68043a0b91aaf4d796c5ff0e7008529acbb0e331
Antônio Falcão <antonio.falcao@xpi.com.br>
2022-12-18 21:06:13 -0300

    removing

--- src/Services/Communication/Command/Application/Services/Emails/Http/ICreditCardHttpClient.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Application.Services.Emails.Http;
-
-public interface ICreditCardHttpClient
-{
-    Task<HttpResponse<CreditCardNotificationResult>> AuthorizeAsync(Requests.CreditCardAuthorizePayment request, CancellationToken cancellationToken);
-    Task<HttpResponse<CreditCardNotificationResult>> RefundAsync(Guid transactionId, Requests.CreditCardRefundPayment request, CancellationToken cancellationToken);
-    Task<HttpResponse<CreditCardNotificationResult>> CaptureAsync(Guid transactionId, CancellationToken cancellationToken);
-    Task<HttpResponse<CreditCardNotificationResult>> CancelAsync(Guid transactionId, Requests.CreditCardCancelPayment request, CancellationToken cancellationToken);
-}
--- src/Services/Communication/Command/Application/Services/SMSs/Http/IDebitCardHttpClient.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Application.Services.SMSs.Http;
-
-public interface IDebitCardHttpClient
-{
-    Task<HttpResponse<DebitCardNotificationResult>> AuthorizeAsync(Requests.DebitCardAuthorizePayment request, CancellationToken cancellationToken);
-    Task<HttpResponse<DebitCardNotificationResult>> RefundAsync(Guid transactionId, Requests.DebitCardRefundPayment request, CancellationToken cancellationToken);
-    Task<HttpResponse<DebitCardNotificationResult>> CaptureAsync(Guid transactionId, CancellationToken cancellationToken);
-    Task<HttpResponse<DebitCardNotificationResult>> CancelAsync(Guid transactionId, Requests.DebitCardCancelPayment request, CancellationToken cancellationToken);
-}
