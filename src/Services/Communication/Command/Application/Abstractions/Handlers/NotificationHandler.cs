4a8a4659fd4c884c1790822a4ec2286a76e8f9cc
Antônio Falcão <antonio.falcao@xpi.com.br>
2022-12-19 10:05:18 -0300

    Adjusting

--- src/Services/Communication/Command/Application/Abstractions/Handlers/NotificationHandler.cs
+++ src/Services/Communication/Command/Application/Abstractions/Handlers/NotificationHandler.cs
@@ -5,17 +5,19 @@ namespace Application.Abstractions.Handlers;
 public abstract class NotificationHandler<TOption> : INotificationHandler<TOption>
     where TOption : INotificationOption
 {
-    protected INotificationHandler<INotificationOption> Next;
+    private INotificationHandler<INotificationOption> Next;
 
     public INotificationHandler<INotificationOption> SetNext<T>(INotificationHandler<T> next)
         where T : INotificationOption
-        => Next = next as INotificationHandler<INotificationOption>;
+        => Next = (INotificationHandler<INotificationOption>)next;
 
     public Task<INotificationResult> HandleAsync(
-        Func<INotificationHandler<INotificationOption>, INotificationOption, Task<INotificationResult>> onHandle,
+        Func<INotificationHandler<INotificationOption>, INotificationOption, CancellationToken, Task<INotificationResult>> onHandle,
         INotificationOption option,
         CancellationToken cancellationToken)
-        => option is TOption ? onHandle((INotificationHandler<INotificationOption>)this, option) : Next.HandleAsync(onHandle, option, cancellationToken);
+        => option is TOption
+            ? onHandle((INotificationHandler<INotificationOption>)this, option, cancellationToken)
+            : Next.HandleAsync(onHandle, option, cancellationToken);
 
     public abstract Task<INotificationResult> NotifyAsync(TOption option, CancellationToken cancellationToken);
     public abstract Task<INotificationResult> CancelAsync(TOption option, CancellationToken cancellationToken);
