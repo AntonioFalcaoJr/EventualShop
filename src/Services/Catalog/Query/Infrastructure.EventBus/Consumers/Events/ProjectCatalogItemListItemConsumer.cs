a7feeae71b50d7389f452a7e830783452cf7ae1a
Kauan Schumacher <kauan.schumacher@xpi.com.br>
2022-12-20 15:18:47 -0300

    Create LazyInteractorProvider to resolve multiple dependencies on Consumer

--- src/Services/Catalog/Query/Infrastructure.EventBus/Consumers/Events/ProjectCatalogItemListItemConsumer.cs
+++ src/Services/Catalog/Query/Infrastructure.EventBus/Consumers/Events/ProjectCatalogItemListItemConsumer.cs
@@ -1,5 +1,6 @@
 using Application.Abstractions;
 using Contracts.Services.Catalog;
+using Infrastructure.EventBus.DependencyInjection.Providers;
 using MassTransit;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -10,25 +11,19 @@ public class ProjectCatalogItemListItemConsumer :
     IConsumer<DomainEvent.CatalogItemAdded>,
     IConsumer<DomainEvent.CatalogItemRemoved>
 {
-    private readonly IServiceProvider _serviceProvider;
+    private readonly ILazyInteractorProvider _lazyInteractorProvider;
 
-    public ProjectCatalogItemListItemConsumer(IServiceProvider serviceProvider)
+    public ProjectCatalogItemListItemConsumer(ILazyInteractorProvider lazyInteractorProvider)
     {
-        _serviceProvider = serviceProvider;
+        _lazyInteractorProvider = lazyInteractorProvider;
     }
 
     public Task Consume(ConsumeContext<DomainEvent.CatalogDeleted> context)
-        => _serviceProvider
-            .GetRequiredService<IInteractor<DomainEvent.CatalogDeleted>>()
-            .InteractAsync(context.Message, context.CancellationToken);
+        => _lazyInteractorProvider.InteractAsync(context.Message, context.CancellationToken);
 
     public Task Consume(ConsumeContext<DomainEvent.CatalogItemAdded> context)
-        => _serviceProvider
-            .GetRequiredService<IInteractor<DomainEvent.CatalogItemAdded>>()
-            .InteractAsync(context.Message, context.CancellationToken);
+        => _lazyInteractorProvider.InteractAsync(context.Message, context.CancellationToken);
 
     public Task Consume(ConsumeContext<DomainEvent.CatalogItemRemoved> context)
-        => _serviceProvider
-            .GetRequiredService<IInteractor<DomainEvent.CatalogItemRemoved>>()
-            .InteractAsync(context.Message, context.CancellationToken);
+        => _lazyInteractorProvider.InteractAsync(context.Message, context.CancellationToken);
 }
