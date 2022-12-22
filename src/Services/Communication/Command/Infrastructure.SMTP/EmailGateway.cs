de81950d2ef4574d0d7b39764b6376bbb3fff96d
Antônio Falcão <antonio.falcao@xpi.com.br>
2022-12-22 08:17:45 -0300

    evolving gateway behavior

--- src/Services/Communication/Command/Infrastructure.SMTP/EmailGateway.cs
+++ src/Services/Communication/Command/Infrastructure.SMTP/EmailGateway.cs
@@ -1,35 +1,58 @@
-﻿using System.Net;
-using System.Net.Mail;
+﻿using System.Net.Mail;
 using Application.Abstractions.Gateways;
+using Domain.Enumerations;
 using Domain.ValueObject;
 using Infrastructure.SMTP.DependencyInjection.Options;
 using Microsoft.Extensions.Options;
+using Serilog;
 
 namespace Infrastructure.SMTP;
 
-public class EmailGateway : IEmailGateway
+public class EmailGateway : INotificationOptionGateway<Email>
 {
     private readonly SmtpOptions _options;
     private readonly SmtpClient _smtpClient;
 
-    public EmailGateway(IOptionsSnapshot<SmtpOptions> options)
+    public EmailGateway(IOptionsSnapshot<SmtpOptions> options, SmtpClient smtpClient)
     {
+        _smtpClient = smtpClient;
         _options = options.Value;
-
-        _smtpClient = new()
-        {
-            Host = _options.Host,
-            Port = _options.Port,
-            EnableSsl = _options.EnableSsl,
-            DeliveryMethod = SmtpDeliveryMethod.Network,
-            UseDefaultCredentials = false,
-            Credentials = new NetworkCredential(_options.Username, _options.Password)
-        };
     }
 
-    public Task SendHtmlEmailAsync(Email email, CancellationToken cancellationToken)
-        => _smtpClient.SendMailAsync(new(_options.Username, email.Address, email.Subject, email.Body) { IsBodyHtml = true }, cancellationToken);
+    public Task<NotificationMethodStatus> NotifyAsync(Email email, CancellationToken cancellationToken)
+        => SendAsync(client => client.SendMailAsync(new(_options.Username, email.Address, email.Subject, email.Body) { IsBodyHtml = true }, cancellationToken));
 
-    public Task SendTextEmailAsync(Email email, CancellationToken cancellationToken)
-        => _smtpClient.SendMailAsync(new(_options.Username, email.Address, email.Subject, email.Body), cancellationToken);
+    public Task<NotificationMethodStatus> CancelAsync(Email email, CancellationToken cancellationToken)
+        => SendAsync(client => client.SendMailAsync(_options.Username, email.Address, email.Subject, email.Body, cancellationToken));
+
+    private async Task<NotificationMethodStatus> SendAsync(Func<SmtpClient, Task> sendEmailAsync)
+    {
+        var onSendCompleted = () => NotificationMethodStatus.Pending;
+
+        _smtpClient.SendCompleted += (_, @event) =>
+        {
+            onSendCompleted = @event switch
+            {
+                { Error: { } error } => () =>
+                {
+                    Log.Error(error, "Error sending email");
+                    return NotificationMethodStatus.Failed;
+                },
+                { Cancelled: true } => () =>
+                {
+                    Log.Warning("Email sending cancelled");
+                    return NotificationMethodStatus.Cancelled;
+                },
+                _ => () =>
+                {
+                    Log.Information("Email sent successfully");
+                    return NotificationMethodStatus.Sent;
+                }
+            };
+        };
+
+        await sendEmailAsync(_smtpClient);
+
+        return onSendCompleted();
+    }
 }
