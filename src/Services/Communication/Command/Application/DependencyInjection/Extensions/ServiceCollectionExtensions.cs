c1dd6eb7c44e15c3f7e1ed7db6152c1a5a1d39fc
Antônio Falcão <antonio.falcao@xpi.com.br>
2022-12-22 08:27:30 -0300

    Adding dependencies

--- src/Services/Communication/Command/Application/DependencyInjection/Extensions/ServiceCollectionExtensions.cs
+++ src/Services/Communication/Command/Application/DependencyInjection/Extensions/ServiceCollectionExtensions.cs
@@ -1,8 +1,11 @@
+using Application.Abstractions.Gateways;
 using Application.Abstractions.Interactors;
 using Application.Services;
 using Application.UseCases;
+using Application.UseCases.Events;
 using Microsoft.Extensions.DependencyInjection;
 using Identity = Contracts.Services.Identity;
+using Communication = Contracts.Services.Communication;
 
 namespace Application.DependencyInjection.Extensions;
 
@@ -11,11 +14,17 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         => services
             .AddScoped<IApplicationService, ApplicationService>();
-    
+
     public static IServiceCollection AddCommandInteractors(this IServiceCollection services)
         => services;
 
+    public static IServiceCollection AddNotificationGateway(this IServiceCollection services)
+        => services
+            .AddScoped<INotificationGateway, NotificationGateway>()
+            .AddScoped<NotificationOptionGatewayProvider>();
+
     public static IServiceCollection AddEventInteractors(this IServiceCollection services)
         => services
-            .AddScoped<IInteractor<Identity.DomainEvent.UserRegistered>, UserRegisteredInteractor>();
+            .AddScoped<IInteractor<Identity.DomainEvent.UserRegistered>, UserRegisteredInteractor>()
+            .AddScoped<IInteractor<Communication.DomainEvent.NotificationRequested>, NotificationRequestedInteractor>();
 }
