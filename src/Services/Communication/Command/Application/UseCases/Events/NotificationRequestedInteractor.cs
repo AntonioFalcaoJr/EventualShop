a268aec1f267caea6305936edb4969f0e763b936
Antônio Falcão <antonio.falcao@xpi.com.br>
2022-12-22 08:19:39 -0300

    Adding interactor

---
+++ src/Services/Communication/Command/Application/UseCases/Events/NotificationRequestedInteractor.cs
@@ -0,0 +1,26 @@
+﻿using Application.Abstractions.Gateways;
+using Application.Abstractions.Interactors;
+using Application.Services;
+using Contracts.Services.Communication;
+using Domain.Aggregates;
+
+namespace Application.UseCases.Events;
+
+public class NotificationRequestedInteractor : IInteractor<DomainEvent.NotificationRequested>
+{
+    private readonly IApplicationService _applicationService;
+    private readonly INotificationGateway _notificationGateway;
+
+    public NotificationRequestedInteractor(IApplicationService applicationService, INotificationGateway notificationGateway)
+    {
+        _applicationService = applicationService;
+        _notificationGateway = notificationGateway;
+    }
+    
+    public async Task InteractAsync(DomainEvent.NotificationRequested @event, CancellationToken cancellationToken)
+    {
+        var notification = await _applicationService.LoadAggregateAsync<Notification>(@event.NotificationId, cancellationToken);
+        await _notificationGateway.NotifyAsync(notification as Notification, cancellationToken);
+        await _applicationService.AppendEventsAsync(notification, cancellationToken);
+    }
+}
