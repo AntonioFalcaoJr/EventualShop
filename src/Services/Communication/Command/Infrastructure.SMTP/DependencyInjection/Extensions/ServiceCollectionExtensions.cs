b28707381981ea2fd08c2aff00d27ee1ca01b3f1
Antônio Falcão <antonio.falcao@xpi.com.br>
2022-12-22 08:16:20 -0300

    improving dependency injection

--- src/Services/Communication/Command/Infrastructure.SMTP/DependencyInjection/Extensions/ServiceCollectionExtensions.cs
+++ src/Services/Communication/Command/Infrastructure.SMTP/DependencyInjection/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,7 @@
+using System.Net;
+using System.Net.Mail;
 using Application.Abstractions.Gateways;
+using Domain.ValueObject;
 using Infrastructure.SMTP.DependencyInjection.Options;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -8,8 +11,19 @@ namespace Infrastructure.SMTP.DependencyInjection.Extensions;
 
 public static class ServiceCollectionExtensions
 {
-    public static void AddSmtp(this IServiceCollection services)
-        => services.AddScoped<IEmailGateway, EmailGateway>();
+    public static void AddEmailGateway(this IServiceCollection services)
+        => services.AddScoped<INotificationOptionGateway<Email>, EmailGateway>();
+
+    public static void AddSmtpClient(this IServiceCollection services)
+        => services.AddScoped<SmtpClient>(provider =>
+        {
+            var options = provider.GetRequiredService<IOptions<SmtpOptions>>().Value;
+            return new(options.Host, options.Port)
+            {
+                Credentials = new NetworkCredential(options.Username, options.Password),
+                EnableSsl = options.EnableSsl,
+            };
+        });
 
     public static OptionsBuilder<SmtpOptions> ConfigureSmtpOptions(this IServiceCollection services, IConfigurationSection section)
         => services
