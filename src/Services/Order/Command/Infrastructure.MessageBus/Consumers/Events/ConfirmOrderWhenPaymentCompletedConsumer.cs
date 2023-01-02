80171203c455f984280f333781e43a98823cc690
Antônio Falcão <antonio.falcao@xpi.com.br>
2023-01-02 17:23:19 -0300

    Moving Order to new design

---
+++ src/Services/Order/Command/Infrastructure.MessageBus/Consumers/Events/ConfirmOrderWhenPaymentCompletedConsumer.cs
@@ -0,0 +1,18 @@
+﻿using Application.UseCases.Events;
+using MassTransit;
+using Payment = Contracts.Services.Payment;
+
+namespace Infrastructure.MessageBus.Consumers.Events;
+
+public class ConfirmOrderWhenPaymentCompletedConsumer : IConsumer<Payment.DomainEvent.PaymentCompleted>
+{
+    private readonly IConfirmOrderWhenPaymentCompletedInteractor _interactor;
+
+    public ConfirmOrderWhenPaymentCompletedConsumer(IConfirmOrderWhenPaymentCompletedInteractor interactor)
+    {
+        _interactor = interactor;
+    }
+
+    public Task Consume(ConsumeContext<Payment.DomainEvent.PaymentCompleted> context)
+        => _interactor.InteractAsync(context.Message, context.CancellationToken);
+}
