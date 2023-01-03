3fb317d473edae99a6db8b8b5109a05bc0881e59
Marcus Silva <marcus.silva@xpi.com.br>
2023-01-03 18:27:47 -0300

    feat(query-stack):  rename consumer with base user projection.

--- src/Services/Identity/Query/Infrastructure.EventBus/DependencyInjection/Extensions/RabbitMqBusFactoryConfiguratorExtensions.cs
+++ src/Services/Identity/Query/Infrastructure.EventBus/DependencyInjection/Extensions/RabbitMqBusFactoryConfiguratorExtensions.cs
@@ -9,9 +9,9 @@ internal static class RabbitMqBusFactoryConfiguratorExtensions
 {
     public static void ConfigureEventReceiveEndpoints(this IRabbitMqBusFactoryConfigurator cfg, IRegistrationContext context)
     {
-        cfg.ConfigureEventReceiveEndpoint<UserDeletedConsumer, DomainEvent.UserDeleted>(context);
-        cfg.ConfigureEventReceiveEndpoint<UserRegisteredConsumer, DomainEvent.UserRegistered>(context);
-        cfg.ConfigureEventReceiveEndpoint<UserPasswordChangedConsumer, DomainEvent.UserPasswordChanged>(context);
+        cfg.ConfigureEventReceiveEndpoint<ProjectUserDetailsWhenUserChangedConsumer, DomainEvent.UserDeleted>(context);
+        cfg.ConfigureEventReceiveEndpoint<ProjectUserDetailsWhenUserChangedConsumer, DomainEvent.UserRegistered>(context);
+        cfg.ConfigureEventReceiveEndpoint<ProjectUserDetailsWhenUserChangedConsumer, DomainEvent.UserPasswordChanged>(context);
     }
 
     private static void ConfigureEventReceiveEndpoint<TConsumer, TEvent>(this IRabbitMqBusFactoryConfigurator bus, IRegistrationContext context)
