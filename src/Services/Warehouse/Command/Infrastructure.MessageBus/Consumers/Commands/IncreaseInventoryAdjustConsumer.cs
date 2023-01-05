e18edd523fb81519beda9e370a26a08c8672f32f
MarcusViniciusSS <lowpoc.developer@gmail.com>
2023-01-05 18:16:54 -0300

    feat(command-stack): migrate Infrastructure.MessageBus for new approach.

---
+++ src/Services/Warehouse/Command/Infrastructure.MessageBus/Consumers/Commands/IncreaseInventoryAdjustConsumer.cs
@@ -0,0 +1,11 @@
+﻿using Application.Abstractions;
+using Contracts.Services.Warehouse;
+using Infrastructure.MessageBus.Abstractions;
+
+namespace Infrastructure.MessageBus.Consumers.Commands;
+
+public class IncreaseInventoryAdjustConsumer : Consumer<Command.IncreaseInventoryAdjust>
+{
+    public IncreaseInventoryAdjustConsumer(IInteractor<Command.IncreaseInventoryAdjust> interactor)
+        : base(interactor) { }
+}
---
+++ src/Services/Warehouse/Command/Infrastructure.MessageBus/Consumers/Events/CartItemAddedConsumer.cs
@@ -0,0 +1,11 @@
+﻿using Application.Abstractions;
+using Infrastructure.MessageBus.Abstractions;
+using ShoppingCart = Contracts.Services.ShoppingCart;
+
+namespace Infrastructure.MessageBus.Consumers.Events;
+
+public class CartItemAddedConsumer : Consumer<ShoppingCart.DomainEvent.CartItemAdded>
+{
+    public CartItemAddedConsumer(IInteractor<ShoppingCart.DomainEvent.CartItemAdded> interactor)
+        : base(interactor) { }
+}
