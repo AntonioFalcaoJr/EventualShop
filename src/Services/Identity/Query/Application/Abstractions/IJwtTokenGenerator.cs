b7c85530d6224014f6d80435a9d05623cd53d183
MarcusViniciusSS <lowpoc.developer@gmail.com>
2023-01-05 11:49:47 -0300

    fix(api): fixed dependencies.

--- src/Services/Identity/Query/Application/Abstractions/IJwtTokenGenerator.cs
+++ src/Services/Identity/Query/Application/Abstractions/IJwtTokenGenerator.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Application.Abstractions;
 
 public interface IJwtTokenGenerator
--- src/Services/Identity/Query/Application/Abstractions/IProjectionGateway.cs
+++ src/Services/Identity/Query/Application/Abstractions/IProjectionGateway.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
 using Contracts.Abstractions;
 using Contracts.Abstractions.Paging;
 
