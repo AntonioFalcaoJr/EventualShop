230ed9b576b8824fa9919d99f1838cb7cae48764
Alex Alves <alexalves2501@hotmail.com>
2023-01-06 14:37:07 -0300

    remove unecessary libs

--- src/Services/Payment/Command/Infrastructure.MessageBus/EventBusGateway.cs
+++ src/Services/Payment/Command/Infrastructure.MessageBus/EventBusGateway.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Infrastructure.MessageBus;
 
-namespace Infrastructure.MessageBus
+internal class EventBusGateway
 {
-    internal class EventBusGateway
-    {
-    }
 }
