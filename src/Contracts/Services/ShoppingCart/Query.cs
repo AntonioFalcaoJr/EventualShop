788ed156d634c810dd7c56f60aa15fca3bb23a06
Antônio Falcão <antonio.falcao@xpi.com.br>
2023-01-09 19:36:35 -0300

    Adding implicit operators

--- src/Contracts/Services/ShoppingCart/Query.cs
+++ src/Contracts/Services/ShoppingCart/Query.cs
@@ -5,15 +5,39 @@ namespace Contracts.Services.ShoppingCart;
 
 public static class Query
 {
-    public record GetShoppingCartDetails(Guid CartId) : Message, IQuery;
+    public record GetShoppingCartDetails(Guid CartId) : IQuery
+    {
+        public static implicit operator GetShoppingCartDetails(Protobuf.GetShoppingCartDetailsRequest request)
+            => new(new Guid(request.CartId));
+    }
 
-    public record GetCustomerShoppingCartDetails(Guid CustomerId) : Message, IQuery;
+    public record GetCustomerShoppingCartDetails(Guid CartId, Guid CustomerId) : IQuery
+    {
+        public static implicit operator GetCustomerShoppingCartDetails(Protobuf.GetCustomerShoppingCartDetailsRequest request)
+            => new(new(request.CartId), new(request.CustomerId));
+    }
 
-    public record GetShoppingCartItemDetails(Guid ItemId) : Message, IQuery;
+    public record GetShoppingCartItemDetails(Guid CartId, Guid ItemId) : IQuery
+    {
+        public static implicit operator GetShoppingCartItemDetails(Protobuf.GetShoppingCartItemDetailsRequest request)
+            => new(new(request.CartId), new(request.ItemId));
+    }
 
-    public record GetPaymentMethodDetails(Guid PaymentMethodId) : Message, IQuery;
+    public record GetPaymentMethodDetails(Guid CartId, Guid MethodId) : IQuery
+    {
+        public static implicit operator GetPaymentMethodDetails(Protobuf.GetPaymentMethodDetailsRequest request)
+            => new(new(request.CartId), new(request.MethodId));
+    }
 
-    public record ListShoppingCartItemsListItems(Guid CartId, Paging Paging) : Message, IQuery;
+    public record ListShoppingCartItemsListItems(Guid CartId, Paging Paging) : IQuery
+    {
+        public static implicit operator ListShoppingCartItemsListItems(Protobuf.ListShoppingCartItemsListItemsRequest request)
+            => new(new(request.CartId), request.Paging);
+    }
 
-    public record ListPaymentMethodsListItems(Guid CartId, Paging Paging) : Message, IQuery;
+    public record ListPaymentMethodsListItems(Guid CartId, Paging Paging) : IQuery
+    {
+        public static implicit operator ListPaymentMethodsListItems(Protobuf.ListPaymentMethodsListItemsRequest request)
+            => new(new(request.CartId), request.Paging);
+    }
 }
