8b9df6dbc5303f87335f087e392a69d050bdb085
Antônio Falcão <antonio.falcao@xpi.com.br>
2023-01-11 18:48:30 -0300

    Moving Identity

---
+++ src/Web/WebAPI/APIs/Identities/Commands.cs
@@ -0,0 +1,34 @@
+﻿using Contracts.Services.Identity;
+using MassTransit;
+using WebAPI.Abstractions;
+using WebAPI.APIs.Identities.Validators;
+
+namespace WebAPI.APIs.Identities;
+
+public static class Commands
+{
+    public record SignUp(IBus Bus, Payloads.SignUp Payload, CancellationToken CancellationToken)
+        : Validatable<SignUpValidator>, ICommand<Command.SignUp>
+    {
+        public Command.SignUp Command
+            => new(Guid.NewGuid(), Payload.FirstName, Payload.LastName, Payload.Email, Payload.Password);
+    }
+
+    public record ChangeEmail(IBus Bus, Guid UserId, string Email, CancellationToken CancellationToken)
+        : Validatable<ChangeEmailValidator>, ICommand<Command.ChangeEmail>
+    {
+        public Command.ChangeEmail Command => new(UserId, Email);
+    }
+
+    public record ChangePassword(IBus Bus, Guid UserId, string Password, string PasswordConfirmation, CancellationToken CancellationToken)
+        : Validatable<ChangePasswordValidator>, ICommand<Command.ChangePassword>
+    {
+        public Command.ChangePassword Command => new(UserId, Password, PasswordConfirmation);
+    }
+
+    public record ConfirmEmail(IBus Bus, Guid UserId, string Email, CancellationToken CancellationToken)
+        : Validatable<ConfirmEmailValidator>, ICommand<Command.ConfirmEmail>
+    {
+        public Command.ConfirmEmail Command => new(UserId, Email);
+    }
+}
