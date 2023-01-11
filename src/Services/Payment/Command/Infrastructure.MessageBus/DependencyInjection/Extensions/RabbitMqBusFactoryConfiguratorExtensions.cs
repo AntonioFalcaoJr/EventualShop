4815c5e39b164600f614958bbdddb0581ad6b6e6
MarcusViniciusSS <lowpoc.developer@gmail.com>
2023-01-11 17:34:51 -0300

    feat(command-stack): migrate infrastructure.messagebus layer for new approach.

---
+++ src/Services/Payment/Command/Infrastructure.MessageBus/DependencyInjection/Extensions/RabbitMqBusFactoryConfiguratorExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Contracts.Abstractions.Messages;
+using Infrastructure.MessageBus.Consumers.Events;
+using MassTransit;
+using Order = Contracts.Services.Order;
+using Contracts.Services.Payment;
+
+namespace Infrastructure.MessageBus.DependencyInjection.Extensions;
+
+internal static class RabbitMqBusFactoryConfiguratorExtensions
+{
+    public static void ConfigureEventReceiveEndpoints(this IRabbitMqBusFactoryConfigurator cfg, IRegistrationContext context)
+    {
+        cfg.ConfigureEventReceiveEndpoint<RequestPaymentWhenOrderPlacedConsumer, Order.DomainEvent.OrderPlaced>(context);
+        cfg.ConfigureEventReceiveEndpoint<ProceedWithPaymentWhenRequestedConsumer, DomainEvent.PaymentRequested>(context);
+    }
+
+    private static void ConfigureEventReceiveEndpoint<TConsumer, TEvent>(this IRabbitMqBusFactoryConfigurator bus, IRegistrationContext context)
+        where TConsumer : class, IConsumer
+        where TEvent : class, IEvent
+        => bus.ReceiveEndpoint(
+            queueName: $"payment.command-stack.{typeof(TConsumer).ToKebabCaseString()}.{typeof(TEvent).ToKebabCaseString()}",
+            configureEndpoint: endpoint =>
+            {
+                endpoint.ConfigureConsumeTopology = false;
+                endpoint.Bind<TEvent>();
+                endpoint.ConfigureConsumer<TConsumer>(context);
+            });
+}
