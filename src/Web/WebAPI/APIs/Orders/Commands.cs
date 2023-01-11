7f41959bbd6ce33021d733056a02f6a69327586b
Antônio Falcão <antonio.falcao@xpi.com.br>
2023-01-11 18:48:41 -0300

    Moving Order

---
+++ src/Web/WebAPI/APIs/Orders/Commands.cs
@@ -0,0 +1,15 @@
+﻿using Contracts.Services.Order;
+using MassTransit;
+using WebAPI.Abstractions;
+using WebAPI.APIs.Orders.Validators;
+
+namespace WebAPI.APIs.Orders;
+
+public static class Commands
+{
+    public record CancelOrder(IBus Bus, Guid OrderId, CancellationToken CancellationToken)
+        : Validatable<CancelOrderValidator>, ICommand<Command.CancelOrder>
+    {
+        public Command.CancelOrder Command => new(OrderId);
+    }
+}
