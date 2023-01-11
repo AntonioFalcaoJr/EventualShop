07636b5eb75037714b8deeed13b5c2e803943fe2
MarcusViniciusSS <lowpoc.developer@gmail.com>
2023-01-11 17:48:00 -0300

    feat(command-stack): refactor PaymentMethodStatus

--- src/Services/Payment/Command/Domain/Enumerations/PaymentMethodStatus.cs
+++ src/Services/Payment/Command/Domain/Enumerations/PaymentMethodStatus.cs
@@ -3,19 +3,19 @@ using Newtonsoft.Json;
 
 namespace Domain.Enumerations;
 
-public sealed class PaymentMethodStatus : SmartEnum<PaymentMethodStatus>
+public class PaymentMethodStatus : SmartEnum<PaymentMethodStatus>
 {
     [JsonConstructor]
     private PaymentMethodStatus(string name, int value)
         : base(name, value) { }
 
-    public static readonly PaymentMethodStatus Authorized = new(nameof(Authorized), 1);
-    public static readonly PaymentMethodStatus Canceled = new(nameof(Canceled), 2);
-    public static readonly PaymentMethodStatus CancellationDenied = new(nameof(CancellationDenied), 3);
-    public static readonly PaymentMethodStatus Denied = new(nameof(Denied), 4);
-    public static readonly PaymentMethodStatus Ready = new(nameof(Ready), 5);
-    public static readonly PaymentMethodStatus RefundDenied = new(nameof(RefundDenied), 6);
-    public static readonly PaymentMethodStatus Refunded = new(nameof(Refunded), 7);
+    public static readonly PaymentMethodStatus Authorized = new AuthorizedMethodStatus();
+    public static readonly PaymentMethodStatus Canceled = new CanceledMethodStatus();
+    public static readonly PaymentMethodStatus CancellationDenied = new CancellationDeniedMethodStatus();
+    public static readonly PaymentMethodStatus Denied = new DeniedMethodStatus();
+    public static readonly PaymentMethodStatus Ready = new ReadyMethodStatus();
+    public static readonly PaymentMethodStatus RefundDenied = new RefundDeniedMethodtatus();
+    public static readonly PaymentMethodStatus Refunded = new RefundedMethodtatus();
 
     public static implicit operator PaymentMethodStatus(string name)
         => FromName(name);
@@ -28,4 +28,40 @@ public sealed class PaymentMethodStatus : SmartEnum<PaymentMethodStatus>
 
     public static implicit operator int(PaymentMethodStatus status)
         => status.Value;
+
+
+    public class AuthorizedMethodStatus : PaymentMethodStatus
+    {
+        public AuthorizedMethodStatus() : base(nameof(Authorized), 1) { }
+    }
+    
+    public class CanceledMethodStatus : PaymentMethodStatus
+    {
+        public CanceledMethodStatus() : base(nameof(Canceled), 2) { }
+    }
+    
+    public class CancellationDeniedMethodStatus : PaymentMethodStatus
+    {
+        public CancellationDeniedMethodStatus() : base(nameof(CancellationDenied), 3) { }
+    }
+    
+    public class DeniedMethodStatus : PaymentMethodStatus
+    {
+        public DeniedMethodStatus() : base(nameof(Denied), 4) { }
+    }
+    
+    public class ReadyMethodStatus : PaymentMethodStatus
+    {
+        public ReadyMethodStatus() : base(nameof(Ready), 5) { }
+    }
+    
+    public class RefundDeniedMethodtatus : PaymentMethodStatus
+    {
+        public RefundDeniedMethodtatus() : base(nameof(RefundDenied), 6) { }
+    }
+    
+    public class RefundedMethodtatus : PaymentMethodStatus
+    {
+        public RefundedMethodtatus() : base(nameof(Refunded), 7) { }
+    }
 }
