d1a9399e2d6e45f0722cec0271cd90612d22736d
MarcusViniciusSS <lowpoc.developer@gmail.com>
2023-01-11 17:36:01 -0300

    feat(command-stack): migrate infrastructure.http layer for new approach.

---
+++ src/Services/Payment/Command/Infrastructure.HTTP/DebitCards/DebitCardPaymentService.cs
@@ -0,0 +1,43 @@
+﻿using Application.Abstractions.Services;
+using Application.Services.DebitCards;
+using Application.Services.DebitCards.Http;
+using Domain.Entities.PaymentMethods;
+using Domain.ValueObjects.PaymentOptions.DebitCards;
+
+namespace Infrastructure.HTTP.DebitCards;
+
+public class DebitCardPaymentService : PaymentService, IDebitCardPaymentService
+{
+    private readonly IDebitCardHttpClient _client;
+
+    public DebitCardPaymentService(IDebitCardHttpClient client)
+    {
+        _client = client;
+    }
+
+    public override Task<IPaymentResult> HandleAsync(Func<IPaymentService, PaymentMethod, CancellationToken, Task<IPaymentResult>> behaviorProcessor, PaymentMethod method, CancellationToken cancellationToken)
+        => method.Option is DebitCard
+            ? behaviorProcessor(this, method, cancellationToken)
+            : base.HandleAsync(behaviorProcessor, method, cancellationToken);
+
+    public override async Task<IPaymentResult> AuthorizeAsync(PaymentMethod method, CancellationToken cancellationToken)
+    {
+        Requests.DebitCardAuthorizePayment request = new();
+        var response = await _client.AuthorizeAsync(request, cancellationToken);
+        return response.ActionResult;
+    }
+
+    public override async Task<IPaymentResult> CancelAsync(PaymentMethod method, CancellationToken cancellationToken)
+    {
+        Requests.DebitCardCancelPayment request = new();
+        var response = await _client.CancelAsync(Guid.NewGuid(), request, cancellationToken);
+        return response.ActionResult;
+    }
+
+    public override async Task<IPaymentResult> RefundAsync(PaymentMethod method, CancellationToken cancellationToken)
+    {
+        Requests.DebitCardRefundPayment request = new();
+        var response = await _client.RefundAsync(Guid.NewGuid(), request, cancellationToken);
+        return response.ActionResult;
+    }
+}
