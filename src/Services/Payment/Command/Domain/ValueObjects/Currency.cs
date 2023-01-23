b21af3e54d1da60f907916ddaded9cefad87bf6b
Antônio Falcão <antonio.falcao@xpi.com.br>
2023-01-23 17:05:43 -0300

    Improving currency

--- src/Services/Payment/Command/Domain/ValueObjects/Currency.cs
+++ src/Services/Payment/Command/Domain/ValueObjects/Currency.cs
@@ -1,32 +1,36 @@
-﻿namespace Domain.ValueObjects;
+﻿// ReSharper disable InconsistentNaming
 
-public readonly record struct Currency(string IsoCode, string Symbol, int DecimalPlaces, string Name, string Country)
+namespace Domain.ValueObjects;
+
+public record Currency(string IsoCode, string Symbol, int DecimalPlaces, string Name, string Country, string CultureInfo)
 {
-    public static Currency CAD => new("CAD", "CAN$", 2, "Canadian dollar", "Canada");
-    public static Currency USD => new("USD", "US$", 2, "United States dollar", "United States");
-    public static Currency EUR => new("EUR", "€", 2, "Euro", "European Union");
-    public static Currency GBP => new("GBP", "£", 2, "British pound", "United Kingdom");
-    public static Currency JPY => new("JPY", "JP¥", 0, "Japanese yen", "Japan");
-    public static Currency CHF => new("CHF", "Fr", 2, "Swiss franc", "Switzerland");
-    public static Currency AUD => new("AUD", "AU$", 2, "Australian dollar", "Australia");
-    public static Currency NZD => new("NZD", "$", 2, "New Zealand dollar", "New Zealand");
-    public static Currency SEK => new("SEK", "kr", 2, "Swedish krona", "Sweden");
-    public static Currency NOK => new("NOK", "kr", 2, "Norwegian krone", "Norway");
+    public static Currency BRL => new("BRL", "R$", 2, "Brazilian Real", "Brazil", "pt-BR");
+    public static Currency CAD => new("CAD", "Can$", 2, "Canadian dollar", "Canada", "en-CA");
+    public static Currency USD => new("USD", "US$", 2, "United States dollar", "United States", "en-US");
+    public static Currency EUR => new("EUR", "€", 2, "Euro", "European Union", "en-EU");
+    public static Currency GBP => new("GBP", "£", 2, "British pound", "United Kingdom", "en-GB");
+    public static Currency JPY => new("JPY", "JP¥", 0, "Japanese yen", "Japan", "ja-JP");
+    public static Currency CHF => new("CHF", "CHF", 2, "Swiss franc", "Switzerland", "de-CH");  
+    public static Currency AUD => new("AUD", "A$", 2, "Australian dollar", "Australia", "en-AU");
+    public static Currency CNY => new("CNY", "CNY", 2, "Chinese yuan", "China", "zh-CN");
+    public static Currency INR => new("INR", "INR", 2, "Indian rupee", "India", "hi-IN");
+    public static Currency MXN => new("MXN", "Mex$", 2, "Mexican peso", "Mexico", "es-MX");
 
     public static implicit operator Currency(string currency)
         => currency switch
         {
-            "CAD" or "CAN$" => CAD,
+            "BRL" or "R$" => BRL,
+            "CAD" or "Can$" => CAD,
             "USD" or "US$" => USD,
             "EUR" or "€" => EUR,
             "GBP" or "£" => GBP,
             "JPY" or "JP¥" => JPY,
-            "CHF" or "Fr" => CHF,
-            "AUD" or "AU$" => AUD,
-            "NZD" or "$" => NZD,
-            "SEK" or "kr" => SEK,
-            "NOK" or "kr" => NOK,
-            _ => throw new ArgumentException($"Unknown currency: {currency}", nameof(currency))
+            "CHF" => CHF,
+            "AUD" or "A$" => AUD,
+            "CNY" => CNY,
+            "INR" => INR,
+            "MXN" or "Mex$" => MXN,
+            _ => throw new ArgumentException($"Invalid currency: {currency}")
         };
 
     public static implicit operator string(Currency currency)
