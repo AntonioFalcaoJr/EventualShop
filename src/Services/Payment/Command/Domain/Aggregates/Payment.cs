9d69ce8e88ccf7e66d89cb8ad7d9f332ecce0252
Antônio Falcão <antonio.falcao@xpi.com.br>
2023-01-30 13:13:49 -0300

    Migrating Payment to new style

--- src/Services/Payment/Command/Domain/Aggregates/Payment.cs
+++ src/Services/Payment/Command/Domain/Aggregates/Payment.cs
@@ -35,91 +35,100 @@ public class Payment : AggregateRoot<PaymentValidator>
 
     public void Handle(Command.RequestPayment cmd)
     {
-        RaiseEvent(new DomainEvent.PaymentRequested(Guid.NewGuid(), cmd.OrderId, cmd.AmountDue, cmd.BillingAddress, PaymentStatus.Ready));
+        RaiseEvent<DomainEvent.PaymentRequested>(version
+            => new(Guid.NewGuid(), cmd.OrderId, cmd.AmountDue, cmd.BillingAddress, PaymentStatus.Ready, version));
 
         foreach (var method in cmd.PaymentMethods)
         {
-            RaiseEvent(method.Option switch
+            RaiseEvent(version => method.Option switch
             {
-                Dto.CreditCard creditCard => new DomainEvent.CreditCardAdded(Id, Guid.NewGuid(), method.Amount, creditCard, PaymentMethodStatus.Pending),
-                Dto.DebitCard debitCard => new DomainEvent.DebitCardAdded(Id, Guid.NewGuid(), method.Amount, debitCard, PaymentMethodStatus.Pending),
-                Dto.PayPal payPal => new DomainEvent.PayPalAdded(Id, Guid.NewGuid(), method.Amount, payPal, PaymentMethodStatus.Pending),
+                Dto.CreditCard creditCard => new DomainEvent.CreditCardAdded(Id, Guid.NewGuid(), method.Amount, creditCard, PaymentMethodStatus.Pending, version),
+                Dto.DebitCard debitCard => new DomainEvent.DebitCardAdded(Id, Guid.NewGuid(), method.Amount, debitCard, PaymentMethodStatus.Pending, version),
+                Dto.PayPal payPal => new DomainEvent.PayPalAdded(Id, Guid.NewGuid(), method.Amount, payPal, PaymentMethodStatus.Pending, version),
                 _ => throw new NotImplementedException()
             });
         }
     }
 
     public void Handle(Command.ProceedWithPayment cmd)
-        => RaiseEvent(AmountDue == 0
-            ? new DomainEvent.PaymentCompleted(cmd.PaymentId, cmd.OrderId, PaymentStatus.Completed)
-            : new DomainEvent.PaymentNotCompleted(cmd.PaymentId, cmd.OrderId, PaymentStatus.NotCompleted));
+        => RaiseEvent(version => AmountDue == 0
+            ? new DomainEvent.PaymentCompleted(cmd.PaymentId, cmd.OrderId, PaymentStatus.Completed, version)
+            : new DomainEvent.PaymentNotCompleted(cmd.PaymentId, cmd.OrderId, PaymentStatus.NotCompleted, version));
 
     public void Handle(Command.CancelPayment cmd)
-        => RaiseEvent(new DomainEvent.PaymentCanceled(cmd.PaymentId, cmd.OrderId, PaymentStatus.Cancelled));
+        => RaiseEvent(version => new DomainEvent.PaymentCanceled(cmd.PaymentId, cmd.OrderId, PaymentStatus.Cancelled, version));
 
     public void Handle(Command.AuthorizePaymentMethod cmd)
     {
         if (_methods.Exists(method => method.Id == cmd.PaymentMethodId))
-            RaiseEvent(new DomainEvent.PaymentMethodAuthorized(cmd.PaymentId, cmd.PaymentMethodId, cmd.TransactionId, PaymentMethodStatus.Authorized));
+            RaiseEvent(version => new DomainEvent.PaymentMethodAuthorized(cmd.PaymentId, cmd.PaymentMethodId, cmd.TransactionId, PaymentMethodStatus.Authorized, version));
     }
 
     public void Handle(Command.DenyPaymentMethod cmd)
     {
         if (_methods.Exists(method => method.Id == cmd.PaymentMethodId))
-            RaiseEvent(new DomainEvent.PaymentMethodDenied(cmd.PaymentId, cmd.PaymentMethodId, cmd.TransactionId, PaymentMethodStatus.Denied));
+            RaiseEvent(version => new DomainEvent.PaymentMethodDenied(cmd.PaymentId, cmd.PaymentMethodId, cmd.TransactionId, PaymentMethodStatus.Denied, version));
     }
 
     public void Handle(Command.CancelPaymentMethod cmd)
     {
-        if (_methods.Exists(method => method.Id == cmd.PaymentMethodId))
-            RaiseEvent(new DomainEvent.PaymentMethodCanceled(cmd.PaymentId, cmd.PaymentMethodId, cmd.TransactionId, PaymentMethodStatus.Cancelled));
+        if (_methods.Exists(method => method.Id == cmd.PaymentMethodId) is false) return;
+
+        RaiseEvent<DomainEvent.PaymentMethodCanceled>(version
+            => new(cmd.PaymentId, cmd.PaymentMethodId, cmd.TransactionId, PaymentMethodStatus.Cancelled, version));
     }
 
     public void Handle(Command.DenyPaymentMethodCancellation cmd)
     {
-        if (_methods.Exists(method => method.Id == cmd.PaymentMethodId))
-            RaiseEvent(new DomainEvent.PaymentMethodCancellationDenied(cmd.PaymentId, cmd.PaymentMethodId, cmd.TransactionId, PaymentMethodStatus.CancellationDenied));
+        if (_methods.Exists(method => method.Id == cmd.PaymentMethodId) is false) return;
+
+        RaiseEvent<DomainEvent.PaymentMethodCancellationDenied>(version
+            => new(cmd.PaymentId, cmd.PaymentMethodId, cmd.TransactionId, PaymentMethodStatus.CancellationDenied, version));
     }
 
     public void Handle(Command.RefundPaymentMethod cmd)
     {
-        if (_methods.Exists(method => method.Id == cmd.PaymentMethodId))
-            RaiseEvent(new DomainEvent.PaymentMethodRefunded(cmd.PaymentId, cmd.PaymentMethodId, cmd.TransactionId, PaymentMethodStatus.Refunded));
+        if (_methods.Exists(method => method.Id == cmd.PaymentMethodId) is false) return;
+
+        RaiseEvent<DomainEvent.PaymentMethodRefunded>(version
+            => new(cmd.PaymentId, cmd.PaymentMethodId, cmd.TransactionId, PaymentMethodStatus.Refunded, version));
     }
 
     public void Handle(Command.DenyPaymentMethodRefund cmd)
     {
-        if (_methods.Exists(method => method.Id == cmd.PaymentMethodId))
-            RaiseEvent(new DomainEvent.PaymentMethodRefundDenied(cmd.PaymentId, cmd.PaymentMethodId, cmd.TransactionId, PaymentMethodStatus.RefundDenied));
+        if (_methods.Exists(method => method.Id == cmd.PaymentMethodId) is false) return;
+
+        RaiseEvent<DomainEvent.PaymentMethodRefundDenied>(version
+            => new(cmd.PaymentId, cmd.PaymentMethodId, cmd.TransactionId, PaymentMethodStatus.RefundDenied, version));
     }
 
-    protected override void Apply(IEvent? @event)
-        => Apply(@event as dynamic);
+    protected override void Apply(IDomainEvent @event)
+        => When(@event as dynamic);
 
-    private void Apply(DomainEvent.PaymentRequested @event)
-        => (Id, OrderId, Amount, BillingAddress, Status) = @event;
+    private void When(DomainEvent.PaymentRequested @event)
+        => (Id, OrderId, Amount, BillingAddress, Status, _) = @event;
 
-    private void Apply(DomainEvent.PaymentMethodAuthorized @event)
+    private void When(DomainEvent.PaymentMethodAuthorized @event)
         => _methods.Single(method => method.Id == @event.PaymentMethodId).Authorize();
 
-    private void Apply(DomainEvent.PaymentMethodDenied @event)
+    private void When(DomainEvent.PaymentMethodDenied @event)
         => _methods.Single(method => method.Id == @event.PaymentMethodId).Deny();
 
-    private void Apply(DomainEvent.PaymentMethodCanceled @event)
+    private void When(DomainEvent.PaymentMethodCanceled @event)
         => _methods.Single(method => method.Id == @event.PaymentMethodId).Cancel();
 
-    private void Apply(DomainEvent.PaymentMethodCancellationDenied @event)
+    private void When(DomainEvent.PaymentMethodCancellationDenied @event)
         => _methods.Single(method => method.Id == @event.PaymentMethodId).DenyCancellation();
 
-    private void Apply(DomainEvent.PaymentMethodRefunded @event)
+    private void When(DomainEvent.PaymentMethodRefunded @event)
         => _methods.Single(method => method.Id == @event.PaymentMethodId).Refund();
 
-    private void Apply(DomainEvent.PaymentMethodRefundDenied @event)
+    private void When(DomainEvent.PaymentMethodRefundDenied @event)
         => _methods.Single(method => method.Id == @event.PaymentMethodId).DenyRefund();
 
-    private void Apply(DomainEvent.PaymentCompleted @event)
+    private void When(DomainEvent.PaymentCompleted @event)
         => Status = @event.Status;
 
-    private void Apply(DomainEvent.PaymentNotCompleted @event)
+    private void When(DomainEvent.PaymentNotCompleted @event)
         => Status = @event.Status;
 }
--- src/Services/Payment/Command/Infrastructure.EventStore/DependencyInjection/Extensions/ServiceCollectionExtensions.cs
+++ src/Services/Payment/Command/Infrastructure.EventStore/DependencyInjection/Extensions/ServiceCollectionExtensions.cs
@@ -1,7 +1,6 @@
 using Application.Abstractions;
 using Application.Abstractions.Gateways;
 using Domain.Abstractions.EventStore;
-using Infrastructure.EventStore.Abstractions;
 using Infrastructure.EventStore.Contexts;
 using Infrastructure.EventStore.DependencyInjection.Options;
 using Infrastructure.EventStore.UnitsOfWork;
