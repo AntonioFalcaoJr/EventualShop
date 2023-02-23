2d5d2babcdb5213e9f87cc00519222531f304db0
Antônio Falcão Jr <arfj@edu.univali.br>
2023-02-22 21:17:25 -0300

    Improving immutability of PagedResult

--- src/Services/Order/Query/Infrastructure.Projections/Pagination/PagedResult.cs
+++ src/Services/Order/Query/Infrastructure.Projections/Pagination/PagedResult.cs
@@ -1,38 +1,40 @@
+using Contracts.Abstractions;
 using Contracts.Abstractions.Paging;
 using MongoDB.Driver;
 using MongoDB.Driver.Linq;
 
 namespace Infrastructure.Projections.Pagination;
 
-public class PagedResult<T> : IPagedResult<T>
+public class PagedResult<TProjection> : IPagedResult<TProjection>
+    where TProjection : IProjection
 {
-    private readonly List<T> _items;
+    private readonly IReadOnlyList<TProjection> _items;
     private readonly Paging _paging;
 
-    public PagedResult(List<T> items, Paging paging)
+    public PagedResult(IReadOnlyList<TProjection> items, Paging paging)
     {
         _items = items;
         _paging = paging;
     }
 
-    public IEnumerable<T> Items
-        => _items.Take(_paging.Limit);
+    public IReadOnlyList<TProjection> Items
+        => _items.Take(_paging.Limit).ToList().AsReadOnly();
 
     public Page Page
         => new()
         {
             Current = _paging.Offset + 1,
-            Size = Items.Count(),
+            Size = Items.Count,
             HasNext = _items.Count > _paging.Limit,
             HasPrevious = _paging.Offset > 0
         };
 
-    public static async Task<IPagedResult<T>> CreateAsync(Paging paging, IQueryable<T> source, CancellationToken cancellationToken)
+    public static async ValueTask<IPagedResult<TProjection>> CreateAsync(Paging paging, IQueryable<TProjection> source, CancellationToken cancellationToken)
     {
         var items = await ApplyPagination(paging, source).ToListAsync(cancellationToken);
-        return new PagedResult<T>(items, paging);
+        return new PagedResult<TProjection>(items, paging);
     }
 
-    private static IMongoQueryable<T>? ApplyPagination(Paging paging, IQueryable<T> source)
-        => source.Skip(paging.Limit * paging.Offset).Take(paging.Limit) as IMongoQueryable<T>;
+    private static IMongoQueryable<TProjection> ApplyPagination(Paging paging, IQueryable<TProjection> source)
+        => (IMongoQueryable<TProjection>)source.Skip(paging.Limit * paging.Offset).Take(paging.Limit);
 }
--- src/Web/WebAPI/Abstractions/PagedResult.cs
+++ src/Web/WebAPI/Abstractions/PagedResult.cs
@@ -7,13 +7,13 @@ namespace WebAPI.Abstractions;
 public record PagedResult<TProjection> : IPagedResult<TProjection>
     where TProjection : IMessage, new()
 {
-    public IEnumerable<TProjection> Items { get; init; } = new List<TProjection>();
+    public IReadOnlyList<TProjection> Items { get; init; } = new List<TProjection>();
     public Page Page { get; init; } = new();
 
     public static implicit operator PagedResult<TProjection>(PagedResult result)
         => new()
         {
-            Items = result.Projections.Select(proto => proto.Unpack<TProjection>()),
+            Items = result.Projections.Select(proto => proto.Unpack<TProjection>()).ToList(),
             Page = result.Page
         };
 }
