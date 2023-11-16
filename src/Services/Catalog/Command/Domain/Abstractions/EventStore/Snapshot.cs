b2c12513553cf259a01692a4d4110e23d0238337
Antonio Falcão Jr <arfj@edu.univali.br>
2023-11-16 12:10:13 -0600

    Remove Catalog Command application and related domain functionality
    This commit eliminates the Catalog Command application, including all underlying service abstractions, gateways, interactors, and related code. This major removal is reflected in the deletion of numerous command interactors, application services, gateways, and other associated interfaces and classes. Necessary changes were made in the domain layer to comply with this removal.

--- src/Services/Catalog/Command/Domain/Abstractions/EventStore/Snapshot.cs
+++
@@ -1,9 +0,0 @@
-﻿using Domain.Abstractions.Aggregates;
-
-namespace Domain.Abstractions.EventStore;
-
-public record Snapshot(Guid AggregateId, string AggregateType, IAggregateRoot Aggregate, long Version, DateTimeOffset Timestamp)
-{
-    public static Snapshot Create(IAggregateRoot aggregate, StoreEvent @event)
-        => new(aggregate.Id, aggregate.GetType().Name, aggregate, @event.Version, @event.Timestamp);
-}
