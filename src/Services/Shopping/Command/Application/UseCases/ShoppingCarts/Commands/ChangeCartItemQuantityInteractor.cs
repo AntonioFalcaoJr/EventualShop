f97f9122d85615f44edd1978415eed2ed6f74a33
Antônio Falcão Jr <arfj@edu.univali.br>
2026-02-08 13:28:49 -0300

    Improve ChangeCartItemQuantityInteractor use case in Shopping Command

--- src/Services/Shopping/Command/Application/UseCases/ShoppingCarts/Commands/ChangeCartItemQuantityInteractor.cs
+++ src/Services/Shopping/Command/Application/UseCases/ShoppingCarts/Commands/ChangeCartItemQuantityInteractor.cs
@@ -1,22 +1,24 @@
-﻿using Application.Abstractions;
-using Application.Services;
-using Contracts.Boundaries.Shopping.ShoppingCart;
+﻿using Application.Services;
 using Domain.Aggregates.Products;
 using Domain.Aggregates.ShoppingCarts;
 using Domain.ValueObjects;
+using MediatR;
 
 namespace Application.UseCases.ShoppingCarts.Commands;
 
-public class ChangeCartItemQuantityInteractor(IApplicationService service) : IInteractor<Command.ChangeCartItemQuantity>
+public record ChangeCartItemQuantity(CartId CartId, ProductId ProductId, Quantity NewQuantity) : IRequest;
+
+public class ChangeCartItemQuantityInteractor(IApplicationService service) : IRequestHandler<ChangeCartItemQuantity>
 {
-    public async Task InteractAsync(Command.ChangeCartItemQuantity cmd, CancellationToken cancellationToken)
+    public async Task Handle(ChangeCartItemQuantity cmd, CancellationToken token)
     {
-        var cart = await service.LoadAggregateAsync<ShoppingCart, CartId>((CartId)cmd.CartId, cancellationToken);
-        
-        cart.ChangeItemQuantity(
-            (ProductId)cmd.ProductId, 
-            (Quantity)cmd.NewQuantity);
-        
-        await service.AppendEventsAsync<ShoppingCart, CartId>(cart, cancellationToken);
+        var cart = await service.LoadAggregateAsync<ShoppingCart, CartId>(cmd.CartId, token);
+        var product = await service.LoadAggregateAsync<Product, ProductId>(cmd.ProductId, token);
+
+        product.ReserveStock(cmd.CartId, cmd.NewQuantity);
+        cart.ChangeItemQuantity(cmd.ProductId, cmd.NewQuantity);
+
+        await service.AppendEventsAsync<Product, ProductId>(product, token);
+        await service.AppendEventsAsync<ShoppingCart, CartId>(cart, token);
     }
 }
