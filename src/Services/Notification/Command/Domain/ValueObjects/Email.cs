f54517072f33db791c61ca480ae7b6af3b078067
Antônio Falcão Jr <arfj@edu.univali.br>
2026-02-08 13:29:44 -0300

    Add Email value object to Notification Command Domain

---
+++ src/Services/Notification/Command/Domain/ValueObjects/Email.cs
@@ -0,0 +1,9 @@
+﻿using Contracts.DataTransferObjects;
+
+namespace Domain.ValueObjects;
+
+public record Email(string Address, string Subject, string Body) : INotificationOption
+{
+    public static implicit operator Email(Dto.Email email)
+        => new(email.Address, email.Subject, email.Body);
+}
