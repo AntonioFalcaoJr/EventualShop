a8dae1c39146550e05443efb5d3b0d09f2374edc
Antônio Falcão Jr <arfj@edu.univali.br>
2026-02-08 13:29:46 -0300

    Add PushWeb value object to Notification Command Domain

---
+++ src/Services/Notification/Command/Domain/ValueObjects/PushWeb.cs
@@ -0,0 +1,12 @@
+﻿using Contracts.DataTransferObjects;
+
+namespace Domain.ValueObjects;
+
+public record PushWeb(Guid UserId) : INotificationOption
+{
+    public static implicit operator PushWeb(Guid userId)
+        => new(userId);
+
+    public static implicit operator PushWeb(Dto.PushWeb push)
+        => new(push.UserId);
+}
