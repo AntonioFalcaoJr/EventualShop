0f481c504086b00997ec3a6a24e50e1feb500fcd
Antônio Falcão Jr <arfj@edu.univali.br>
2026-02-08 13:28:51 -0300

    Improve CatalogId aggregate in Shopping Command

--- src/Services/Shopping/Command/Domain/Aggregates/ShoppingCarts/CatalogId.cs
+++ src/Services/Shopping/Command/Domain/Aggregates/ShoppingCarts/CatalogId.cs
@@ -6,9 +6,10 @@ public record CatalogId : GuidIdentifier
 {
     public CatalogId() { }
     public CatalogId(string value) : base(value) { }
+    public CatalogId(Guid value) : base(value) { }
 
     public static CatalogId New => new();
-    public static readonly CatalogId Undefined = new() { Value = Guid.Empty };
+    public static readonly CatalogId Undefined = new(Guid.Empty);
 
     public static explicit operator CatalogId(string value) => new(value);
     public override string ToString() => base.ToString();
