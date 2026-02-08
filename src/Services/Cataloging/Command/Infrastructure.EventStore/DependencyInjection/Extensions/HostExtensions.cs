85cd6d77d0e7925472eac9559692330d1b9b28f7
Antônio Falcão Jr <arfj@edu.univali.br>
2026-02-08 13:28:35 -0300

    Update HostExtensions DI configuration in Cataloging Command EventStore

--- src/Services/Cataloging/Command/Infrastructure.EventStore/DependencyInjection/Extensions/HostExtensions.cs
+++ src/Services/Cataloging/Command/Infrastructure.EventStore/DependencyInjection/Extensions/HostExtensions.cs
@@ -1,17 +1,28 @@
-﻿using Infrastructure.EventStore.Contexts;
+﻿using Application.DependencyInjection;
+using Infrastructure.EventStore.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace Infrastructure.EventStore.DependencyInjection.Extensions;
 
 public static class HostExtensions
 {
-    public static async Task MigrateEventStoreAsync(this IHost host)
+    public static async Task MigrateEventStoreAsync(this IHost host, CancellationToken token)
     {
+        Log.Information("Migrating EventStore");
         await using var scope = host.Services.CreateAsyncScope();
         await using var dbContext = scope.ServiceProvider.GetRequiredService<EventStoreDbContext>();
-        await dbContext.Database.MigrateAsync();
-        await dbContext.Database.EnsureCreatedAsync();
+        await dbContext.Database.MigrateAsync(token);
+        await dbContext.Database.EnsureCreatedAsync(token);
+    }
+
+    public static async Task SeedDataAsync(this IHost host, CancellationToken token)
+    {
+        Log.Information("Seeding Data");
+        await using var scope = host.Services.CreateAsyncScope();
+        var seeder = scope.ServiceProvider.GetRequiredService<ISeeder>();
+        await seeder.SeedDataAsync(token);
     }
 }
