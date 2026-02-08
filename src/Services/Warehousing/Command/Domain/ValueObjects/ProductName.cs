e6aeccf459876668fe8bc03f484e3562b87f89bd
Antônio Falcão Jr <arfj@edu.univali.br>
2026-02-08 13:30:09 -0300

    Add ProductName value object to Warehousing Command Domain

---
+++ src/Services/Warehousing/Command/Domain/ValueObjects/ProductName.cs
@@ -0,0 +1,21 @@
+﻿namespace Domain.ValueObjects;
+
+public record ProductName
+{
+    private readonly string _value;
+
+    public ProductName(string productName)
+    {
+        productName = productName.Trim();
+        ArgumentException.ThrowIfNullOrEmpty(productName);
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(productName.Length, 50);
+
+        _value = productName;
+    }
+
+    public static explicit operator ProductName(string productName) => new(productName);
+    public static implicit operator string(ProductName productName) => productName._value;
+    public static ProductName Undefined => new("Undefined");
+
+    public override string ToString() => _value;
+}
