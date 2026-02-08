6c4102bae951ac9ba6b7448d6f1fe16fe8599d13
Antônio Falcão Jr <arfj@edu.univali.br>
2026-02-08 13:29:48 -0300

    Add CatalogItemId to Shopping Command Domain

---
+++ src/Services/Shopping/Command/Domain/Aggregates/CatalogItemId.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Abstractions.Identities;
+
+namespace Domain.Aggregates;
+
+        public record CatalogItemId : GuidIdentifier
+        {
+            public CatalogItemId() { }
+            public CatalogItemId(string value) : base(value) { }
+            public CatalogItemId(Guid value) : base(value) { }
+
+            public static CatalogItemId New => new();
+            public static readonly CatalogItemId Undefined = new(Guid.Empty);
+
+            public static explicit operator CatalogItemId(string value) => new(value);
+            public override string ToString() => base.ToString();
+        }
