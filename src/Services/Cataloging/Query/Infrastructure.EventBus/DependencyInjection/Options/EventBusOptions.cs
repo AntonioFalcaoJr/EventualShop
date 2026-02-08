4611775968fee31363af765d2f75d751d829c73e
Antônio Falcão Jr <arfj@edu.univali.br>
2026-02-08 13:28:40 -0300

    Update EventBusOptions DI configuration in Cataloging Query EventBus

--- src/Services/Cataloging/Query/Infrastructure.EventBus/DependencyInjection/Options/EventBusOptions.cs
+++ src/Services/Cataloging/Query/Infrastructure.EventBus/DependencyInjection/Options/EventBusOptions.cs
@@ -5,7 +5,6 @@ namespace Infrastructure.EventBus.DependencyInjection.Options;
 public record EventBusOptions
 {
     [Required] public required string ConnectionName { get; init; }
-    [Required] public required Uri ConnectionString { get; init; }
     [Required, Range(1, 10)] public int RetryLimit { get; init; }
     [Required, Range(typeof(TimeSpan), "00:00:01", "00:00:30")] public TimeSpan InitialInterval { get; init; }
     [Required, Range(typeof(TimeSpan), "00:00:01", "00:00:30")] public TimeSpan IntervalIncrement { get; init; }
