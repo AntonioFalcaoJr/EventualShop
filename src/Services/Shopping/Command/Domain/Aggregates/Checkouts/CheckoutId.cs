14bd2a959ef4b242564ca911a99f29769e20ad5b
Antônio Falcão Jr <arfj@edu.univali.br>
2026-02-08 13:28:50 -0300

    Improve CheckoutId aggregate in Shopping Command

--- src/Services/Shopping/Command/Domain/Aggregates/Checkouts/CheckoutId.cs
+++ src/Services/Shopping/Command/Domain/Aggregates/Checkouts/CheckoutId.cs
@@ -6,10 +6,11 @@ public record CheckoutId : GuidIdentifier
 {
     public CheckoutId() { }
     public CheckoutId(string value) : base(value) { }
-    
+    public CheckoutId(Guid value) : base(value) { }
+
     public static CheckoutId New => new();
-    public static readonly CheckoutId Undefined = new() { Value = Guid.Empty };
-    
+    public static readonly CheckoutId Undefined = new(Guid.Empty);
+
     public static explicit operator CheckoutId(string value) => new(value);
     public override string ToString() => base.ToString();
 }
