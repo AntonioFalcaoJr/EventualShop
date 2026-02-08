05317c0eefbf82daa64539bef1d5ec31a4d873b3
Antônio Falcão Jr <arfj@edu.univali.br>
2026-02-08 13:28:33 -0300

    Improve domain exceptions in Cataloging Command

--- src/Services/Cataloging/Command/Domain/Exceptions.cs
+++ src/Services/Cataloging/Command/Domain/Exceptions.cs
@@ -11,4 +11,7 @@ public static class Exceptions
     public class InvalidQuantity() : DomainException<InvalidQuantity>("Invalid quantity.");
     public class VersionMustBePositive() : DomainException<VersionMustBePositive>("Version must be a positive number.");
     public class VersionFormatException() : DomainException<VersionFormatException>("Version must be a positive number.");
+    public class CatalogAlreadyCreated() : DomainException<CatalogAlreadyCreated>("Catalog is already created.");
+    public class CatalogItemAlreadyAssociated() : DomainException<CatalogItemAlreadyAssociated>("Catalog item already associated.");
+    public class ProductAlreadyRegistered() : DomainException<ProductAlreadyRegistered>("Product already registered.");
 }
