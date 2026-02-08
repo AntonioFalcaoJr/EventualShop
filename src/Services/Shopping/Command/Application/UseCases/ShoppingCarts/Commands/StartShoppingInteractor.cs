f02f1ee5f488d63d83916639b65c7877e93c4943
Antônio Falcão Jr <arfj@edu.univali.br>
2026-02-08 13:28:49 -0300

    Improve StartShoppingInteractor use case in Shopping Command

--- src/Services/Shopping/Command/Application/UseCases/ShoppingCarts/Commands/StartShoppingInteractor.cs
+++ src/Services/Shopping/Command/Application/UseCases/ShoppingCarts/Commands/StartShoppingInteractor.cs
@@ -1,6 +1,5 @@
 ﻿using Application.Services;
 using Domain.Aggregates.ShoppingCarts;
-using Domain.Enumerations;
 using MediatR;
 
 namespace Application.UseCases.ShoppingCarts.Commands;
@@ -9,13 +8,11 @@ public record StartShopping(CustomerId CustomerId) : IRequest<CartId>;
 
 public class StartShoppingInteractor(IApplicationService service) : IRequestHandler<StartShopping, CartId>
 {
-    public async Task<CartId> Handle(StartShopping cmd, CancellationToken cancellationToken)
+    public async Task<CartId> Handle(StartShopping cmd, CancellationToken cancellation)
     {
-        var cart = await service.LoadAggregateAsync<ShoppingCart, CartId>(cart => cart.CustomerId == cmd.CustomerId, cancellationToken);
-        if (cart.Status is CartStatusOpen or CartStatusAbandoned) return cart.Id;
-
-        cart = ShoppingCart.StartShopping(cmd.CustomerId);
-        await service.AppendEventsAsync<ShoppingCart, CartId>(cart, cancellationToken);
+        ShoppingCart cart = new();
+        cart.StartShopping(cmd.CustomerId);
+        await service.AppendEventsAsync<ShoppingCart, CartId>(cart, cancellation);
         return cart.Id;
     }
 }
