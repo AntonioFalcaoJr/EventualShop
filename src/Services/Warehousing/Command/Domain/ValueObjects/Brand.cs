2bff31e93e6e2fdbce02f0e98147aa9276c77f64
Antônio Falcão Jr <arfj@edu.univali.br>
2026-02-08 13:29:56 -0300

    Add Brand value object to Warehousing Command Domain

---
+++ src/Services/Warehousing/Command/Domain/ValueObjects/Brand.cs
@@ -0,0 +1,28 @@
+﻿namespace Domain.ValueObjects;
+
+public record Brand
+{
+    private readonly string _name;
+    private readonly string _prefix;
+    
+     public static Brand Undefined => new(string.Empty, string.Empty);
+
+    public Brand(string name, string prefix)
+    {
+        name = name.Trim();
+        prefix = prefix.Trim();
+        
+        ArgumentException.ThrowIfNullOrEmpty(name);
+        ArgumentException.ThrowIfNullOrEmpty(prefix);
+        
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(name.Length, 30);
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(prefix.Length, 5);
+      
+        _name = name;
+        _prefix = prefix;
+    }
+
+    public static explicit operator Brand((string name, string prefix) tuple) => new(tuple.name, tuple.prefix);
+    public static implicit operator string(Brand brand) => brand._prefix;
+    public override string ToString() => _name;
+}
