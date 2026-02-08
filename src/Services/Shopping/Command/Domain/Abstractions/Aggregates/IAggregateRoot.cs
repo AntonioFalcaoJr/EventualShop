0b1f6230cbd5eba86ba47156a198b096a035b517
Antônio Falcão Jr <arfj@edu.univali.br>
2026-02-08 13:28:50 -0300

    Improve IAggregateRoot aggregate in Shopping Command

--- src/Services/Shopping/Command/Domain/Abstractions/Aggregates/IAggregateRoot.cs
+++ src/Services/Shopping/Command/Domain/Abstractions/Aggregates/IAggregateRoot.cs
@@ -1,14 +1,12 @@
 using Contracts.Abstractions.Messages;
 using Domain.Abstractions.Entities;
 using Domain.Abstractions.Identities;
-using Version = Domain.ValueObjects.Version;
 
 namespace Domain.Abstractions.Aggregates;
 
 public interface IAggregateRoot<out TId> : IEntity<TId>
     where TId : IIdentifier, new()
 {
-    Version Version { get; }
     void LoadFromStream(List<IDomainEvent> events);
     bool TryDequeueEvent(out IDomainEvent @event);
 }
