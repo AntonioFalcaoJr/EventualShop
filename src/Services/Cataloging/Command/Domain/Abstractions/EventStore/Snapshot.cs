6eebfec2ab6fb99906f28d09d1c6a5cd1809d9d4
Antônio Falcão Jr <arfj@edu.univali.br>
2026-02-08 13:28:32 -0300

    Improve Snapshot abstraction in Cataloging Command

--- src/Services/Cataloging/Command/Domain/Abstractions/EventStore/Snapshot.cs
+++ src/Services/Cataloging/Command/Domain/Abstractions/EventStore/Snapshot.cs
@@ -4,10 +4,15 @@ using Version = Domain.ValueObjects.Version;
 
 namespace Domain.Abstractions.EventStore;
 
-public record Snapshot<TAggregate, TId>(TId AggregateId, TAggregate Aggregate, Version Version, DateTimeOffset Timestamp)
+public record Snapshot<TAggregate, TId>(
+    TId AggregateId, 
+    TAggregate Aggregate, 
+    Version Version, 
+    DateTimeOffset Timestamp,
+    string? ReferenceId)
     where TAggregate : IAggregateRoot<TId>
     where TId : IIdentifier, new()
 {
-    public static Snapshot<TAggregate, TId> Create(TAggregate aggregate, StoreEvent<TAggregate, TId> @event)
-        => new(aggregate.Id, aggregate, @event.Version, @event.Timestamp);
+    public static Snapshot<TAggregate, TId> Create(TAggregate aggregate, StoreEvent<TAggregate, TId> @event, string? referenceId = null)
+        => new(aggregate.Id, aggregate, @event.Version, @event.Timestamp, referenceId);
 }
