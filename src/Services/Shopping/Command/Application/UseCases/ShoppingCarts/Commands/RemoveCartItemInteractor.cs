da26abe0497f4943db388addf4efa0472bea3fa4
Antônio Falcão Jr <arfj@edu.univali.br>
2026-02-08 13:28:49 -0300

    Improve RemoveCartItemInteractor use case in Shopping Command

--- src/Services/Shopping/Command/Application/UseCases/ShoppingCarts/Commands/RemoveCartItemInteractor.cs
+++ src/Services/Shopping/Command/Application/UseCases/ShoppingCarts/Commands/RemoveCartItemInteractor.cs
@@ -1,17 +1,25 @@
-﻿using Application.Abstractions;
-using Application.Services;
-using Contracts.Boundaries.Shopping.ShoppingCart;
+﻿using Application.Services;
 using Domain.Aggregates.Products;
 using Domain.Aggregates.ShoppingCarts;
+using MediatR;
 
 namespace Application.UseCases.ShoppingCarts.Commands;
 
-public class RemoveCartItemInteractor(IApplicationService service) : IInteractor<Command.RemoveCartItem>
+public record RemoveCartItem(CartId CartId, ProductId ProductId) : IRequest;
+
+public class RemoveCartItemInteractor(IApplicationService service) : IRequestHandler<RemoveCartItem>
 {
-    public async Task InteractAsync(Command.RemoveCartItem cmd, CancellationToken cancellationToken)
+    public async Task Handle(RemoveCartItem cmd, CancellationToken cancellationToken)
     {
-        var shoppingCart = await service.LoadAggregateAsync<ShoppingCart, CartId>((CartId)cmd.CartId, cancellationToken);
-        shoppingCart.RemoveItem((ProductId)cmd.ProductId);
-        await service.AppendEventsAsync<ShoppingCart, CartId>(shoppingCart, cancellationToken);
+        var cart = await service.LoadAggregateAsync<ShoppingCart, CartId>(cmd.CartId, cancellationToken);
+        var product = await service.LoadAggregateAsync<Product, ProductId>(cmd.ProductId, cancellationToken);
+
+        var quantity = cart.Items[cmd.ProductId].Quantity;
+
+        cart.RemoveItem(cmd.ProductId);
+        product.Restock(quantity);
+
+        await service.AppendEventsAsync<ShoppingCart, CartId>(cart, cancellationToken);
+        await service.AppendEventsAsync<Product, ProductId>(product, cancellationToken);
     }
 }
