809eef768e2376d0382ad9c2c9fb1872d3a42b25
Antônio Falcão Jr <arfj@edu.univali.br>
2026-02-08 13:30:00 -0300

    Add Category value object to Warehousing Command Domain

---
+++ src/Services/Warehousing/Command/Domain/ValueObjects/Category.cs
@@ -0,0 +1,21 @@
+﻿namespace Domain.ValueObjects;
+
+public record Category
+{
+    private readonly string _value;
+
+    public static Category Undefined => new("Undefined");
+
+    public Category(string category)
+    {
+        category = category.Trim();
+        ArgumentException.ThrowIfNullOrEmpty(category);
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(category.Length, 50);
+
+        _value = category;
+    }
+
+    public static explicit operator Category(string category) => new(category);
+    public static implicit operator string(Category category) => category._value;
+    public override string ToString() => _value;
+}
