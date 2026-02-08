a06eca2c2fe8a1153e7aad76cb467d2ba7222020
Antônio Falcão Jr <arfj@edu.univali.br>
2026-02-08 13:29:47 -0300

    Add Sms value object to Notification Command Domain

---
+++ src/Services/Notification/Command/Domain/ValueObjects/Sms.cs
@@ -0,0 +1,12 @@
+﻿using Contracts.DataTransferObjects;
+
+namespace Domain.ValueObjects;
+
+public record Sms(string Number) : INotificationOption
+{
+    public static implicit operator Sms(string number)
+        => new(number);
+
+    public static implicit operator Sms(Dto.Sms sms)
+        => new(sms.Number);
+}
