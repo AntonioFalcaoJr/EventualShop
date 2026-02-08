b7420392a2a547fb53e0b7a75cf512938d811daa
Antônio Falcão Jr <arfj@edu.univali.br>
2026-02-08 13:29:37 -0300

    Add RegisterCatalogInteractor use case to Cataloging Command

---
+++ src/Services/Cataloging/Command/Application/UseCases/Catalogs/Commands/RegisterCatalogInteractor.cs
@@ -0,0 +1,20 @@
+﻿using Application.Services;
+using Domain.Aggregates;
+using Domain.Aggregates.Catalogs;
+using Domain.ValueObjects;
+using MediatR;
+
+namespace Application.UseCases.Catalogs.Commands;
+
+public record RegisterCatalog(AppId AppId, Title Title, Description Description) : IRequest<CatalogId>;
+
+public class RegisterCatalogInteractor(IApplicationService service) : IRequestHandler<RegisterCatalog, CatalogId>
+{
+    public async Task<CatalogId> Handle(RegisterCatalog cmd, CancellationToken cancellationToken)
+    {
+        Catalog catalog = new();
+        catalog.Register(cmd.AppId, cmd.Title, cmd.Description);
+        await service.AppendEventsAsync<Catalog, CatalogId>(catalog, cancellationToken);
+        return catalog.Id;
+    }
+}
