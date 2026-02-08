5e2b797924af61b8d1235fd300b3adbcfe5700ad
Antônio Falcão Jr <arfj@edu.univali.br>
2026-02-08 13:28:33 -0300

    Improve Brand value object in Cataloging Command

--- src/Services/Cataloging/Command/Domain/ValueObjects/Brand.cs
+++ src/Services/Cataloging/Command/Domain/ValueObjects/Brand.cs
@@ -9,18 +9,20 @@ public record Brand
     {
         name = name.Trim();
         prefix = prefix.Trim();
-        
+
         ArgumentException.ThrowIfNullOrEmpty(name);
         ArgumentException.ThrowIfNullOrEmpty(prefix);
-        
+
         ArgumentOutOfRangeException.ThrowIfGreaterThan(name.Length, 30);
         ArgumentOutOfRangeException.ThrowIfGreaterThan(prefix.Length, 5);
-      
+
         _name = name;
         _prefix = prefix;
     }
 
     public static explicit operator Brand((string name, string prefix) tuple) => new(tuple.name, tuple.prefix);
     public static implicit operator string(Brand brand) => brand._prefix;
+    public static Brand Undefined => new("Undefined", "None");
+
     public override string ToString() => _name;
 }
