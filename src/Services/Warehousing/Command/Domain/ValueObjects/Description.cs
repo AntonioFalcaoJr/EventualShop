3f0e6fcecf61e00964cfbd65fd2ddd3374297434
Antônio Falcão Jr <arfj@edu.univali.br>
2026-02-08 13:30:02 -0300

    Add Description value object to Warehousing Command Domain

---
+++ src/Services/Warehousing/Command/Domain/ValueObjects/Description.cs
@@ -0,0 +1,21 @@
+﻿namespace Domain.ValueObjects;
+
+public record Description
+{
+    private readonly string _value;
+
+    public static Description Undefined => new(string.Empty);
+    
+    private Description(string description)
+    {
+        description = description.Trim();
+        ArgumentException.ThrowIfNullOrEmpty(description);
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(description.Length, 500);
+
+        _value = description;
+    }
+
+    public static implicit operator string(Description description) => description._value;
+    public static explicit operator Description(string description) => new(description);
+    public override string ToString() => _value;
+}
