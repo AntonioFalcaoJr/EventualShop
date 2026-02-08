8365614bee127687809a5af0a9cf89bb40f5adfd
Antônio Falcão Jr <arfj@edu.univali.br>
2026-02-08 13:28:56 -0300

    Update HostExtensions DI configuration in Shopping Command EventStore

--- src/Services/Shopping/Command/Infrastructure.EventStore/DependencyInjection/Extensions/HostExtensions.cs
+++ src/Services/Shopping/Command/Infrastructure.EventStore/DependencyInjection/Extensions/HostExtensions.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace Infrastructure.EventStore.DependencyInjection.Extensions;
 
@@ -9,6 +10,7 @@ public static class HostExtensions
 {
     public static async Task MigrateEventStoreAsync(this IHost host)
     {
+        Log.Information("Checking EventStore for pending migrations");
         await using var scope = host.Services.CreateAsyncScope();
         await using var dbContext = scope.ServiceProvider.GetRequiredService<EventStoreDbContext>();
         await dbContext.Database.MigrateAsync();
