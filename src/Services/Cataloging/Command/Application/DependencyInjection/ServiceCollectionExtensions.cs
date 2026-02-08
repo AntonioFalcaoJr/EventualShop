1c828514267097c98868330f4521d55f641249b6
Antônio Falcão Jr <arfj@edu.univali.br>
2026-02-08 13:28:30 -0300

    Improve ServiceCollectionExtensions in Cataloging Command

--- src/Services/Cataloging/Command/Application/DependencyInjection/ServiceCollectionExtensions.cs
+++ src/Services/Cataloging/Command/Application/DependencyInjection/ServiceCollectionExtensions.cs
@@ -8,6 +8,7 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
         => services
+            .AddScoped<ISeeder, Seeder>()
             .AddScoped<IApplicationService, ApplicationService>()
             .AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
 }
