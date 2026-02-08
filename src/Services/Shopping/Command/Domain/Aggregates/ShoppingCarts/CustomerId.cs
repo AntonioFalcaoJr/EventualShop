620854ee5e7c773fa2bacb5771cb885d9a9aa055
Antônio Falcão Jr <arfj@edu.univali.br>
2026-02-08 13:28:51 -0300

    Improve CustomerId aggregate in Shopping Command

--- src/Services/Shopping/Command/Domain/Aggregates/ShoppingCarts/CustomerId.cs
+++ src/Services/Shopping/Command/Domain/Aggregates/ShoppingCarts/CustomerId.cs
@@ -6,9 +6,10 @@ public record CustomerId : GuidIdentifier
 {
     public CustomerId() { }
     public CustomerId(string value) : base(value) { }
+    public CustomerId(Guid value) : base(value) { }
 
     public static CustomerId New => new();
-    public static readonly CustomerId Undefined = new() { Value = Guid.Empty };
+    public static readonly CustomerId Undefined = new(Guid.Empty);
 
     public static explicit operator CustomerId(string value) => new(value);
     public override string ToString() => base.ToString();
