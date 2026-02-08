13305e59fa0c87af184ba32b4a4a76cf9db3adb4
Antônio Falcão Jr <arfj@edu.univali.br>
2026-02-08 13:28:28 -0300

    Update DomainEvent contract in Shopping

--- src/Contracts/Boundaries/Shopping/Product/DomainEvent.cs
+++ src/Contracts/Boundaries/Shopping/Product/DomainEvent.cs
@@ -4,12 +4,24 @@ namespace Contracts.Boundaries.Shopping.Product;
 
 public static class DomainEvent
 {
-    public record ProductRegistered(string ProductId, string CatalogItemId, string Name, IDictionary<string, string> Prices, string Inventory, ulong Version) 
+    public record ProductReleased(string ProductId, string CatalogItemId, string Name, IDictionary<string, string> Prices, string Availability, ulong Version) 
         : Message, IDomainEvent;
 
-    public record ProductInventoryTaken(string ProductId, string Quantity, string NewInventory, ulong Version) 
+    public record ProductTaken(string ProductId, string Quantity, string NewInventory, ulong Version) 
         : Message, IDomainEvent;
 
-    public record ProductInventoryReturned(string ProductId, string Quantity, string NewInventory, ulong Version) 
+    public record ProductRestocked(string ProductId, string Quantity, string NewAvailability, ulong Version) 
+        : Message, IDomainEvent;
+
+    public record ProductReserved(string ProductId, string CartId, string Quantity, string NewReservation, string NewTotalReserved, ulong Version)
+        : Message, IDomainEvent;
+    
+    public record ReservationReleased(string ProductId, string CartId, string NewReservation, string NewTotalReserved, ulong Version)
+        : Message, IDomainEvent;
+    
+    public record ReservationDecreased(string ProductId, string CartId, string NewReservation, string NewTotalReserved, ulong Version)
+        : Message, IDomainEvent;
+    
+    public record ReservationIncreased(string ProductId, string CartId, string NewReservation, string NewTotalReserved, ulong Version)
         : Message, IDomainEvent;
 }
