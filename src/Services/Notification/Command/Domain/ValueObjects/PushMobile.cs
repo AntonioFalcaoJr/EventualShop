61adc37c578e2c9125cae0197a74c5ed216a1b81
Antônio Falcão Jr <arfj@edu.univali.br>
2026-02-08 13:29:45 -0300

    Add PushMobile value object to Notification Command Domain

---
+++ src/Services/Notification/Command/Domain/ValueObjects/PushMobile.cs
@@ -0,0 +1,12 @@
+﻿using Contracts.DataTransferObjects;
+
+namespace Domain.ValueObjects;
+
+public record PushMobile(Guid DeviceId) : INotificationOption
+{
+    public static implicit operator PushMobile(Guid deviceId)
+        => new(deviceId);
+
+    public static implicit operator PushMobile(Dto.PushMobile push)
+        => new(push.DeviceId);
+}
