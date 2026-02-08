a2478b8196173f0c6a09b1a2f45e25ef05a4c798
Antônio Falcão Jr <arfj@edu.univali.br>
2026-02-08 13:28:32 -0300

    Improve StoreEvent abstraction in Cataloging Command

--- src/Services/Cataloging/Command/Domain/Abstractions/EventStore/StoreEvent.cs
+++ src/Services/Cataloging/Command/Domain/Abstractions/EventStore/StoreEvent.cs
@@ -5,10 +5,16 @@ using Version = Domain.ValueObjects.Version;
 
 namespace Domain.Abstractions.EventStore;
 
-public record StoreEvent<TAggregate, TId>(TId AggregateId, string EventType, IDomainEvent Event, Version Version, DateTimeOffset Timestamp)
+public record StoreEvent<TAggregate, TId>(
+    TId AggregateId, 
+    string EventType, 
+    IDomainEvent Event, 
+    Version Version, 
+    DateTimeOffset Timestamp, 
+    string? ReferenceId)
     where TAggregate : IAggregateRoot<TId>
     where TId : IIdentifier, new()
 {
-    public static StoreEvent<TAggregate, TId> Create(TAggregate aggregate, IDomainEvent @event)
-        => new(aggregate.Id, @event.GetType().Name, @event, (Version)@event.Version, @event.Timestamp);
+    public static StoreEvent<TAggregate, TId> Create(TAggregate aggregate, IDomainEvent @event, string? referenceId = null)
+        => new(aggregate.Id, @event.GetType().Name, @event, (Version)@event.Version, @event.Timestamp, referenceId);
 }
